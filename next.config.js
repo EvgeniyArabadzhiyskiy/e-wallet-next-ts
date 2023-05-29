267d4716c0bf98860dc55e3507e5cf46b52b8817
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-05-29 23:31:50 +0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+
+// const withImages = require('next-images');
+
+const nextConfig = {
+ // reactStrictMode: true,
+
+  // compiler: {
+  //   styledComponents: true,
+  // },
+
+  // experimental: {
+  //   appDir: true,
+  // },
+};
+
+module.exports = nextConfig;
+// module.exports = withImages(nextConfig);
