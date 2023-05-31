0634ca0660e347ee1c754c43594dac042ca7c275
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-05-31 20:25:40 +0300

    Create GlobalStyle.ts

---
+++ styles/theme/GlobalStyle.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+// import 'modern-normalize';
+// import 'react-toastify/dist/ReactToastify.css';
+
+export const GlobalStyle = createGlobalStyle`
+    /* h1,
+    h2,
+    h3,
+    h4,
+    h5,
+    h6,
+    p {
+        margin: 0;
+    } */
+  
+`;
