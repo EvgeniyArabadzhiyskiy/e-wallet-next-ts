91dc97418371c0a050142c711290b084e1ea6c0f
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-06-05 21:17:38 +0300

    Create MobileOnly.styled.tsx

---
+++ components/MobileOnly/MobileOnly.styled.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import styled from "styled-components";
+
+// export const Mobile = styled.div`
+//   @media screen and (max-width: 767px) {
+//     display: block;
+//   }
+
+//   @media screen and (min-width: 768px) {
+//     display: none;
+//   }
+
+//   @media screen and (min-width: 1280px) {
+//     display: none;
+//   }
+// `;
+
+export const Mobile = styled.div`
+  display: block;
+
+  @media screen and (min-width: 768px) {
+    display: none;
+  }
+`;
