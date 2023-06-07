7818483e73d5ebfbd39da67816b8fdd15deb8d1d
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-06-07 21:53:40 +0300

    Create media.tsx

---
+++ lib/media.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { createMedia } from "@artsy/fresnel";
+import { breakpointsValue } from "@/styles/theme/breakpoints";
+
+const MyAppMedia = createMedia({
+  breakpoints: breakpointsValue,
+});
+
+export const mediaStyles = MyAppMedia.createMediaStyle();
+
+export const { Media, MediaContextProvider } = MyAppMedia;
