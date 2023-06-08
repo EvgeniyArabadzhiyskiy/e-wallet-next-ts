95409f641c90da773e77f909e195b484477c118d
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-06-08 21:53:48 +0300

    Update Providers.tsx

--- components/Providers/Providers.tsx
+++ components/Providers/Providers.tsx
@@ -1,23 +1,29 @@
 "use client";
 
+import React from "react";
 import { MediaContextProvider } from "@/lib/media";
 import { GlobalStyle } from "@/styles/theme/GlobalStyle";
 import { dayTheme } from "@/styles/theme/theme";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "styled-components";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 interface IProps {
   children?: React.ReactNode;
 }
 
 export const Providers = ({ children }: IProps) => {
+  const [queryClient] = React.useState(() => new QueryClient());
+
   return (
     <>
-      <MediaContextProvider disableDynamicMediaQueries>
-        <ThemeProvider theme={dayTheme}>
-          <SessionProvider>{children}</SessionProvider>
-        </ThemeProvider>
-      </MediaContextProvider>
+      <ThemeProvider theme={dayTheme}>
+        <MediaContextProvider disableDynamicMediaQueries>
+          <QueryClientProvider client={queryClient}>
+            <SessionProvider>{children}</SessionProvider>
+          </QueryClientProvider>
+        </MediaContextProvider>
+      </ThemeProvider>
       {/* <GlobalStyle /> */}
     </>
   );
