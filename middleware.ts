9628959b0fe758649aa223870590f1d24479d677
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-06-08 21:53:36 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,40 +1,45 @@
-export { default } from "next-auth/middleware";
+// export { default } from "next-auth/middleware";
 
-export const config = {
-  matcher: ["/about"],
-};
+// export const config = {
+//   matcher: ["/about"],
+// };
 
 // =================================================================
 
-// import { NextResponse } from "next/server";
-// import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+import { withAuth } from "next-auth/middleware";
 
-// export default withAuth(
-//   function middleware(req) {
-//     // const isLoggedIn = !!req.nextauth.token;
-//     // console.log("========================", isLoggedIn);
+export default withAuth(
+  function middleware(req) {
+    const isLoggedIn = req.nextauth;
+    // console.log("========================", isLoggedIn);
 
-//     // if (!isLoggedIn && req.nextUrl.pathname.startsWith("/about")) {
-//     //   return NextResponse.redirect(new URL("/login", req.url));
-//     // }
+  
 
-//     // if (isLoggedIn && req.nextUrl.pathname.startsWith("/login")) {
-//     //   return NextResponse.redirect(new URL("/home", req.url));
-//     // }
-//   },
+    // const isLoggedIn = !!req.nextauth.token;
+    // console.log("========================", isLoggedIn);
 
-//   {
-//     callbacks: {
-//       authorized: ({ token, req }) => {
-//         return !!token
+    // if (!isLoggedIn && req.nextUrl.pathname.startsWith("/about")) {
+    //   return NextResponse.redirect(new URL("/login", req.url));
+    // }
 
-//         return false;
-//       },
-//     },
-//   }
-// );
+    // if (isLoggedIn && req.nextUrl.pathname.startsWith("/login")) {
+    //   return NextResponse.redirect(new URL("/home", req.url));
+    // }
+  },
 
-// export const config = { matcher: [
-//   "/about",
-// //  "/login"
-// ] };
+  {
+    callbacks: {
+      authorized: ({ token, req }) => {
+        // return !!token
+
+        return true;
+      },
+    },
+  }
+);
+
+export const config = { matcher: [
+  "/about",
+//  "/login"
+] };
