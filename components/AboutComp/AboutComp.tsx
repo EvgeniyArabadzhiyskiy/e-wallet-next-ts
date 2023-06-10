2b2de99e8db6b5fd0f240c33fb01c770ce8a5fff
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-06-10 14:16:38 +0300

    Create AboutComp.tsx

---
+++ components/AboutComp/AboutComp.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+// import Image from "next/image";
+import Link from "next/link";
+
+const AboutComp = () => {
+  const session = useSession()
+  console.log("AboutComp  session:", session.data);
+
+  return (
+    <div>
+      <Link href="/">HOME</Link>
+      
+      {/* <div style={{ position: "fixed", width: "300px", height: "200px", overflowX: "hidden", 
+       zIndex: -1
+      }} >
+      <Image
+        src="/images/ewallet-2.webp"
+        alt="phone"
+        fill={true}
+        style={{objectFit: 'cover'}}
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw "
+        priority
+       />
+      </div> */}
+      <h1 style={{  textAlign: "center", fontSize: '64px'}} >Current pathname</h1>
+      
+    </div>
+  );
+};
+
+export default AboutComp;
