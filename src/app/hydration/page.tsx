2a4e63bd202e2dad925541f64ba4aa097d52163e
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-06-14 13:16:30 +0300

    Create page.tsx

---
+++ src/app/hydration/page.tsx
@@ -0,0 +1,25 @@
+import getQueryClient from "@/src/lib/getQueryClient";
+import { dehydrate, Hydrate } from "@tanstack/react-query";
+import HydratedPokemon from "@/src/components/HydratedPokemon/HydratedPokemon";
+
+const getPokemonInfo = async (): Promise<any> => {
+  const req = await fetch(
+    `https://pokeapi.co/api/v2/pokemon?offset=0&limit=10`,
+    {cache: 'no-store'}
+  );
+  const data = (await req.json()) as any;
+
+  return data.results;
+};
+
+export default async function HydratedPage() {
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery(["pokemon"], getPokemonInfo);
+  const dehydratedState = dehydrate(queryClient);
+
+  return (
+    <Hydrate state={dehydratedState}>
+      <HydratedPokemon />
+    </Hydrate>
+  );
+}
