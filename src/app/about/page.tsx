dc8f51c8f5a57e064c8b7f4ff34983102184b77d
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-06-19 13:07:30 +0300

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,10 @@
 import AboutComp from "@/src/components/AboutComp/AboutComp";
+import { authOptions } from "@/src/lib/auth";
+import { getServerSession } from "next-auth";
 
-export default  function AboutPage() {
+export default async  function AboutPage() {
+  const session = await getServerSession(authOptions)
+  console.log("AboutPage  session:", session);
   
 
   return (
