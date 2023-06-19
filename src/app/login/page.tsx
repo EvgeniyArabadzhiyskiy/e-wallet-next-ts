069f52e225ec2879ba9f8f86c43537d5805416a8
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-06-19 12:24:24 +0300

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,23 +1,61 @@
-"use client";
+// "use client";
 
 import Link from "next/link";
 import LoginForm from "@/src/components/LoginForm/LoginForm";
 import { useSession } from "next-auth/react";
-import { redirect } from "next/navigation";
-import {
-  Section,
-  Text,
-  Wrapper,
-  Container,
-} from "../_components/LoginLayout.styled";
+import { redirect, useRouter } from "next/navigation";
+
 import Statistic from "@/src/components/Statistic/Statistic";
 // import { Container } from "@/components/Container/Container.styled";
 import WrapperLogin from "@/src/components/WrapperLogin";
 import Logo from "@/src/components/Logo/Logo";
 import { Media, MediaContextProvider } from "@/src/lib/media";
 import LoginPageLayout from "@/src/components/LoginPageLayout/LoginPageLayout";
-
+import { getUser } from "@/src/helpers/getUser";
+import { cookies } from "next/headers";
+import { AuthRequiredError } from "@/src/lib/authError";
+import { parseCookies } from "nookies";
 // import { useEffect } from "react";
+
+export default async function LoginPage() {
+  // const router = useRouter();
+  // const { authToken } = parseCookies();
+  // console.log("LoginPage  authToken:", authToken);
+
+  const cookieStore = cookies();
+  const authToken = cookieStore.get("authToken")?.value;
+  // const  authToken  = undefined;
+  // throw new AuthRequiredError("Something Error")
+
+  // const queryUserData = authToken && await getUser(authToken);
+
+  // const isLoggedIn = !!queryUserData?.email;
+
+  if (authToken) {
+    redirect("/home");
+  }
+
+  // useEffect(() => {
+  //   if (authToken) {
+  //     router.push("/home");
+  //   }
+  // }, [authToken, router]);
+
+  //======================================================================
+  // const session = useSession();
+  // console.log("LoginPage session:", session.status);
+
+  // if (session.status === "loading") {
+  //   return <h1>Session loading ...</h1>;
+  // }
+
+  // if (session.status === "authenticated") {
+  //   // redirect("/home");
+  // }
+
+  //======================================================================
+  return <LoginForm />;
+}
 
 // export default  function LoginPage() {
 //   //   const res = await fetch(
@@ -53,42 +91,3 @@ import LoginPageLayout from "@/src/components/LoginPageLayout/LoginPageLayout";
 
 //   // return  <WrapperLogin pokemons={results} />
 // }
-
-const wait = async (time: number) => {
-  return new Promise((res) => {
-    setTimeout(() => {
-      res("WORLD");
-    }, time);
-  });
-};
-
-// export default  function LoginPage() {
-//   // await wait(5000)
-//   const res = await fetch(
-//     "https://pokeapi.co/api/v2/pokemon?offset=0&limit=10"
-//   );
-//   const { results } = await res.json();
-
-//   return (
-//     <>
-//         <Logo  />
-//         <Link href="/">HOME</Link>
-//         {/* <Statistic pokemons={results} /> */}
-//     </>
-//   );
-// }
-
-export default function LoginPage() {
-  const session = useSession();
-  // console.log("LoginPage session:", session.status);
-
-  // if (session.status === "loading") {
-  //   return <h1>Session loading ...</h1>;
-  // }
-
-  // if (session.status === "authenticated") {
-  //   // redirect("/home");
-  // }
-
-  return <LoginForm />;
-}
