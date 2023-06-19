fe2dc5f8391387411df80b4ec290c5597c315e85
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-06-19 12:24:31 +0300

    Update LoginForm.tsx

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { signIn, useSession } from "next-auth/react";
+// import { signIn, useSession } from "next-auth/react";
 import { useState } from "react";
 import { FormWrap } from "./LoginForm.styled";
 import { useScaleForm } from "@/src/hooks/useScaleForm";
@@ -16,98 +16,137 @@ import { FormikHelpers, FormikProps } from "formik";
 import { ILoginValues } from "@/src/types/loginValues";
 import Link from "next/link";
 import Spinner from "../Spinner/Spinner";
-import {Title} from "../Title/Title.styled";
+import { Title } from "../Title/Title.styled";
 import { IRegisterValues } from "@/src/types/registerValues";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { BASE_URL, USER_LOGIN } from "@/src/constants/apiPath";
 
-// const login = async (credentials: any) => {
-//   const BASE_URL = "https://wallet-backend-xmk0.onrender.com/api";
-//   const USER_LOGIN = "/users/login";
+const login = async (credentials: ILoginValues) => {
+  // const BASE_URL = "https://wallet-backend-xmk0.onrender.com/api";
+  // const USER_LOGIN = "/users/login";
 
-//   const options = {
-//     method: "POST",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify(credentials),
-//   };
+  // const options = {
+  //   method: "POST",
+  //   headers: { "Content-Type": "application/json" },
+  //   body: JSON.stringify(credentials),
+  // };
 
-//   const resFetch = await fetch(`${BASE_URL}${USER_LOGIN}`, options);
-//   const user = await resFetch.json();
-//   console.log("login  user:", user);
+  // const resFetch = await fetch(`${BASE_URL}${USER_LOGIN}`, options);
+  // const user = await resFetch.json();
+  // console.log("login  user:", user);
 
-//   // const { data: user } = await axios.post(
-//   //   `${BASE_URL}${USER_LOGIN}`,
-//   //   credentials
-//   // );
+  try {
+    const { data } = await axios.post(`${BASE_URL}${USER_LOGIN}`, credentials);
+    return data;
+    
+  } catch (error) {
+    throw error;
+  }
+};
 
-//   return user;
-// };
-
-
-
-
-export default function LoginForm () {
+export default function LoginForm() {
+  const router = useRouter();
   const isScale = useScaleForm();
-  const [isLoading, setIsLoading] = useState(false);
-
-  
-  
-  
-  // const session = useSession()
-  // console.log("LoginForm  session:", session.status);
-  // const isLoading = session.status === "loading"
-  // console.log("LoginForm  isLoading:", isLoading);
-
-  // if (session.status === "loading") {
-  //   return <h1>Session loading ...</h1>;
-  // }
-  // console.log("LoginPage session:", session.data?.user.token);
 
   const initialValues: ILoginValues = {
     email: "",
     password: "",
   };
 
-  const handleSubmit = async (values: ILoginValues, 
-    { resetForm }: FormikHelpers<ILoginValues>
-    ) => {
-    
-    setIsLoading(true);
+  const { mutate: signInUser, isLoading } = useMutation({
+    mutationFn: login,
 
-    try {
-      const user = await signIn('credentials', {
-        email: values.email,
-        password: values.password,
-        redirect: false,
+    onSuccess: (data) => {
+      // console.log("LoginForm  data:", data);
+      const { token, ...rest } = data;
+      // queryClient.setQueryData(["currentUser"], rest);
+
+      setCookie(null, "authToken", `${token}`, {
+        maxAge: 30 * 24 * 60 * 60,
+        path: "/",
       });
-  
-      // console.log('handleSubmit user:', user);
-  
-      resetForm({ values: { email: '', password: '' } });
-    } catch (error) {
-      // Обработка ошибок
-    } finally {
-      setIsLoading(false);
-    }
+
+      router.push("/home");
+      // const ddd = queryClient.getQueryData(['currentUser'])
+      // console.log("LoginForm  ddd:", ddd);
+    },
+  });
+
+  const handleSubmit = async (
+    values: ILoginValues,
+    { resetForm }: FormikHelpers<ILoginValues>
+  ) => {
+    signInUser({ email: values.email, password: values.password });
+
+    resetForm({ values: { email: "", password: "" } });
   };
 
   return (
     <FormWrap $isScale={isScale}>
       {isLoading && <h1>Loading...</h1>}
-      <Title as='h2' mb={5} color='expense' fontSize={["ml"]}  >Login Page</Title>
+      <Title as="h2" mb={5} color="expense" fontSize={["ml"]}>
+        Login Page
+      </Title>
       <Logo />
 
-      <FormContainer 
+      <FormContainer
         onSubmit={handleSubmit}
         initialValues={initialValues}
         validationSchema={schema.login}
-        render={(formik: FormikProps<ILoginValues>) => <LoginFormFields formik={formik} />}
+        render={(formik: FormikProps<ILoginValues>) => (
+          <LoginFormFields formik={formik} />
+        )}
       />
-
     </FormWrap>
   );
 }
 
+// export default function LoginForm () {
+//   const isScale = useScaleForm();
+//   const [isLoading, setIsLoading] = useState(false);
 
+//   const initialValues: ILoginValues = {
+//     email: "",
+//     password: "",
+//   };
 
+//   const handleSubmit = async (values: ILoginValues,
+//     { resetForm }: FormikHelpers<ILoginValues>
+//     ) => {
+
+//     setIsLoading(true);
+
+//     try {
+//       const user = await signIn('credentials', {
+//         email: values.email,
+//         password: values.password,
+//         redirect: false,
+//       });
+
+//       resetForm({ values: { email: '', password: '' } });
+//     } catch (error) {
+//     } finally {
+//       setIsLoading(false);
+//     }
+//   };
+
+//   return (
+//     <FormWrap $isScale={isScale}>
+//       {isLoading && <h1>Loading...</h1>}
+//       <Title as='h2' mb={5} color='expense' fontSize={["ml"]}  >Login Page</Title>
+//       <Logo />
+
+//       <FormContainer
+//         onSubmit={handleSubmit}
+//         initialValues={initialValues}
+//         validationSchema={schema.login}
+//         render={(formik: FormikProps<ILoginValues>) => <LoginFormFields formik={formik} />}
+//       />
+
+//     </FormWrap>
+//   );
+// }
 
 // function LoginForm() {
 //   const [email, setEmail] = useState("");
