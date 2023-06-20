bda937db59ae360ca0541667d66665b587d5ef76
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-06-20 12:39:27 +0300

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,13 +1,24 @@
 import AboutComp from "@/src/components/AboutComp/AboutComp";
+import TestCookies from "@/src/components/TestCookies";
 import { authOptions } from "@/src/lib/auth";
 import { getServerSession } from "next-auth";
+import { cookies } from "next/headers";
+import Link from "next/link";
 
 export default async function AboutPage() {
-  const session = await getServerSession(authOptions);
+  // const session = await getServerSession(authOptions);
 
+  const cookieStore = cookies();
+  const authToken = cookieStore.get("authToken")?.value;
+  // console.log("AboutPage  authToken:", authToken);
+
+  
   return (
     <main>
-      <AboutComp session={session} />
+      <Link href="/">HOME</Link>
+      <h1>Cookies{authToken}</h1>
+      <TestCookies />
+      {/* <AboutComp session={session} /> */}
     </main>
   );
 }
