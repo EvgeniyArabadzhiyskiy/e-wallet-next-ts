e5c8c7cc7f87c0595b5e35565461763f2da4446a
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-06-21 21:56:59 +0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,17 +12,13 @@ import { withAuth } from "next-auth/middleware";
 export default withAuth(
   function middleware(req) {
     const isLoggedIn = !!req.nextauth.token;
-    const cookies = req.cookies;
-    const authToken = cookies.get("authToken")?.value;
-    console.log("middleware  authToken:", authToken);
-    
     // console.log("========================", isLoggedIn);
 
     if (!isLoggedIn && req.nextUrl.pathname.startsWith("/home")) {
       return NextResponse.redirect(new URL("/login", req.url));
     }
 
-    if (authToken && req.nextUrl.pathname.startsWith("/login")) {
+    if (isLoggedIn && req.nextUrl.pathname.startsWith("/login")) {
       return NextResponse.redirect(new URL("/home", req.url));
     }
   },
