4be386d50cbe9dc7f6d226bc51e0baffce8214b1
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-06-22 21:41:19 +0300

    Create ModalWindow.styled.tsx

---
+++ src/components/ModalWindow/ModalWindow.styled.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import styled from "styled-components";
+
+export const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: #2a2c2d67;
+`;
+
+export const Modal = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 300px;
+  height: 400px;
+  background-color: #ffffff;
+`;
