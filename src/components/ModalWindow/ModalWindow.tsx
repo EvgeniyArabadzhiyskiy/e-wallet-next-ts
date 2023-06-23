abc8c6de49ae30a2645cdb61db1cf2108c2a9738
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-06-23 21:56:13 +0300

    Update ModalWindow.tsx

--- src/components/ModalWindow/ModalWindow.tsx
+++ src/components/ModalWindow/ModalWindow.tsx
@@ -1,104 +1,54 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { createPortal } from "react-dom";
 import { Modal, Overlay } from "./ModalWindow.styled";
+import { ModalState } from "../GlobalProvider/GlobalProvider";
 
 interface IProps {
   children: React.ReactNode;
+  modalName: string;
   setIsModalOpen: any;
 }
 
+export default function ModalWindow({ children, modalName, setIsModalOpen }: IProps) {
+  const modalRoot = document.querySelector("#modal-root");
 
-export default function ModalWindow({ children, setIsModalOpen }: IProps) {
-    const modalRoot = document.querySelector('#modal-root');
-
-    const onCloseModal = () => {
-      setIsModalOpen(false);
-    };
-  
-    const onBackdropClick = (e: any) => {
-      if (e.target === e.currentTarget) {
-        //   setIsModalOpen(false);
-        onCloseModal();
-      }
-    };
-  
-    const onEscPress = (e: any) => {
-      if (e.code === "Escape") {
-        //   setIsModalOpen(false);
-        onCloseModal();
-      }
-    };
-  
-    useEffect(() => {
-      window.addEventListener("keydown", onEscPress);
-      document.body.style.overflow = "hidden";
-  
-      return () => {
-        window.removeEventListener("keydown", onEscPress);
-        document.body.style.overflow = "visible";
+  const onToggleModal = () => {
+    setIsModalOpen((prev: ModalState) => {
+      return {
+        ...prev,
+        [modalName]: !prev[modalName],
       };
     });
-  
-    return createPortal(
-      <Overlay onClick={onBackdropClick}>
-        <Modal>
-          
-          {children}
-          {/* <button type="button" onClick={onCloseModal}>
-            Close
-          </button> */}
-        </Modal>
-      </Overlay>,
-      modalRoot as HTMLElement
-    );
-  }
+  };
 
-// export default function ModalWindow({ children, setIsOpen }: IProps) {
-//   const onCloseModal = () => {
-//     setIsOpen(false);
-//   };
+  const onBackdropClick = (e: any) => {
+    if (e.target === e.currentTarget) {
+      onToggleModal();
+    }
+  };
 
-//   const onBackdropClick = (e: any) => {
-//     if (e.target === e.currentTarget) {
-//       //   setIsOpen(false);
-//       onCloseModal();
-//     }
-//   };
+  const onEscPress = (e: any) => {
+    if (e.code === "Escape") {
+      onToggleModal();
+    }
+  };
 
-//   const onEscPress = (e: any) => {
-//     if (e.code === "Escape") {
-//       //   setIsOpen(false);
-//       onCloseModal();
-//     }
-//   };
+  useEffect(() => {
+    window.addEventListener("keydown", onEscPress);
+    document.body.style.overflow = "hidden";
 
-//   useEffect(() => {
-//     window.addEventListener("keydown", onEscPress);
-//     document.body.style.overflow = "hidden";
+    return () => {
+      window.removeEventListener("keydown", onEscPress);
+      document.body.style.overflow = "visible";
+    };
+  });
 
-//     return () => {
-//       window.removeEventListener("keydown", onEscPress);
-//       document.body.style.overflow = "visible";
-//     };
-//   });
-
-//   return createPortal(
-//     <Overlay onClick={onBackdropClick}>
-//       <Modal>
-//         {React.Children.map(children, (child) => {
-//           return React.cloneElement(child as React.ReactElement<any>, {
-//             color: "red",
-//             onCloseModal: onCloseModal,
-//           });
-//         })}
-//         {/* {children} */}
-//         {/* <button type="button" onClick={onCloseModal}>
-//           Close
-//         </button> */}
-//       </Modal>
-//     </Overlay>,
-//     document.body as HTMLElement
-//   );
-// }
+  return createPortal(
+    <Overlay onClick={onBackdropClick}>
+      <Modal>{children}</Modal>
+    </Overlay>,
+    modalRoot as HTMLElement
+  );
+}
