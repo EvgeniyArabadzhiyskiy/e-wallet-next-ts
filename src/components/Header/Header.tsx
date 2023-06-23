b5b49828f51d2a1c63faa4c5168ebeeabe8b1592
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-06-23 21:56:22 +0300

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,9 +2,17 @@
 
 import { useContext } from "react";
 import AuthMenu from "../AuthMenu/AuthMenu";
-import { StyledHeader, TextName } from "./Header.styled";
+import {
+  HeaderWrapper,
+  StyledHeader,
+  TextName,
+  UserBox,
+} from "./Header.styled";
 import { useSession } from "next-auth/react";
 import { GlobalContext } from "../GlobalProvider/GlobalProvider";
+import LogoutBtn from "../Buttons/LogoutBtn/LogoutBtn";
+import Container from "../Container/Container";
+import Logo from "../Logo/Logo";
 
 interface UserData {
   email: string;
@@ -12,18 +20,35 @@ interface UserData {
   balance: number;
 }
 
-
 export default function Header({ currentUser }: { currentUser?: any }) {
   const user = useSession();
   const { isModalOpen, setIsModalOpen } = useContext(GlobalContext);
 
+  console.log("Header re-re");
+
   return (
     <StyledHeader>
-      <AuthMenu />
-      <TextName>{user.data?.user.user.firstName}</TextName>
-      {/* <TextName>SERVER USER {currentUser?.user.user.firstName}</TextName> */}
-      {/* <h2>Balance: {currentUser?.user.user.balance}</h2> */}
-      <button type="button" onClick={() => setIsModalOpen(true)}>Open</button>
+      <Container>
+        <HeaderWrapper>
+          <Logo />
+          <UserBox>
+            <TextName>SERVER USER {currentUser?.user.user.firstName}</TextName>
+            <LogoutBtn modalName="logout" type="exit" />
+          </UserBox>
+          {/* <AuthMenu /> */}
+          {/* <TextName>{user.data?.user.user.firstName}</TextName> */}
+          {/* <h2>Balance: {currentUser?.user.user.balance}</h2> */}
+          {/* <button type="button" onClick={() => setIsModalOpen(true)}>Open</button> */}
+          {/* <button
+        type="button"
+        onClick={() =>
+          setIsModalOpen((prev: any) => ({ ...prev, logout: true }))
+        }
+      >
+        Open
+      </button> */}
+        </HeaderWrapper>
+      </Container>
     </StyledHeader>
   );
 }
