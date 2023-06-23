65f425b2721901bd22ccf875fa817114a6e8a211
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-06-23 21:56:25 +0300

    Update Header.styled.tsx

--- src/components/Header/Header.styled.tsx
+++ src/components/Header/Header.styled.tsx
@@ -1,11 +1,24 @@
-'use client'
+"use client";
 
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const StyledHeader = styled.header`
   background-color: #74a887;
   /* height: 100px; */
 `;
+
+export const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const UserBox = styled.div`
+  display: flex;
+  align-items: center;
+  color: ${p => p.theme.colors.secondaryText};
+`;
+
 
 export const TextName = styled.div`
   font-size: 24px;
