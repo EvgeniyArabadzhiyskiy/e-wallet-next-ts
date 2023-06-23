878d7f92e8f53ce0c8252f4102d845a879134162
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-06-23 21:56:18 +0300

    Update ModalLogOut.tsx

--- src/components/ModalLogOut/ModalLogOut.tsx
+++ src/components/ModalLogOut/ModalLogOut.tsx
@@ -1,40 +1,14 @@
 "use client";
 
-import { useContext } from "react";
 import { LogoutButton } from "../AuthButtons/AuthButtons";
-import { GlobalContext } from "../GlobalProvider/GlobalProvider";
-// import { ModalContext } from "../ModalBox";
+import LogoutBtn from "../Buttons/LogoutBtn/LogoutBtn";
 
 export default function ModalLogOut() {
-  const { isModalOpen, setIsModalOpen } = useContext(GlobalContext);
-
   return (
     <>
       <h1>LodOut Modal</h1>
       <LogoutButton />
-      <button type="button" onClick={() => setIsModalOpen(false)}>
-        Close
-      </button>
+      <LogoutBtn modalName="logout" type="cancel" />
     </>
   );
 }
-
-//   interface IProps {
-//     color?: string;
-//     onCloseModal?: () => void;
-//   }
-
-// export default function ModalLogOut({ color, onCloseModal }: IProps) {
-//   // const onCloseModal = () => {
-//   //     setIsOpen(false)
-//   // }
-//   return (
-//     <>
-//       <h1>LodOut Modal</h1>
-//       <LogoutButton />
-//       <button type="button" onClick={onCloseModal}>
-//         Close
-//       </button>
-//     </>
-//   );
-// }
