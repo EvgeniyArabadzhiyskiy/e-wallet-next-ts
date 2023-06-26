fc90758cd3ad1846ead9a4616c848a2156d32074
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-06-26 21:06:21 +0300

    Update fonts.ts

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,8 +1,17 @@
-import { Poppins } from "next/font/google";
+import { Inter, Poppins, Fira_Code } from "next/font/google";
 
 export const poppins = Poppins({
   subsets: ["latin"],
   weight: ["400", "700"],
   style: "normal",
+  variable: "--font-poppins",
+  display: "swap",
+});
+
+export const inter = Inter({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  style: "normal",
+  variable: "--font-inter",
   display: "swap",
 });
