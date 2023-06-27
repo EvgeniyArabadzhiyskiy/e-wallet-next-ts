dec1970216681552ba35b97b40e93e2ed3aee6cb
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-06-27 22:37:39 +0300

    Update LoginPageLayout.tsx

--- src/components/LoginPageLayout/LoginPageLayout.tsx
+++ src/components/LoginPageLayout/LoginPageLayout.tsx
@@ -4,27 +4,14 @@ import stl from "./LoginPageLayout.module.scss";
 export default function LoginPageLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-    <div className={stl.background}>
-      <h1 className={stl.background__title}>Wallet App</h1>
-    </div>
-    <div className={stl.section}>
-      <Container>
-        <div className={stl.wrapper}>
-          {children}
-        </div>
-      </Container>
-    </div>
+      <div className={stl.background}>
+        <h1 className={stl.background__title}>Wallet App</h1>
+      </div>
+      <div className={stl.section}>
+        <Container>
+          <div className={stl.wrapper}>{children}</div>
+        </Container>
+      </div>
     </>
   );
-
-
-  // return (
-  //   <div className="section" >
-  //     <Container>
-  //       <div className="wrapper" >
-  //         {children}
-  //       </div>
-  //     </Container>
-  //   </div>
-  // );
 }
