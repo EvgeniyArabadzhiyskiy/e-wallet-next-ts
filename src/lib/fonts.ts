4cd153da65d327fc549d3c6de9da786656b97d23
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-06-27 14:25:52 +0300

    Update fonts.ts

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,4 +1,4 @@
-import { Inter, Poppins, Fira_Code } from "next/font/google";
+import { Inter, Poppins } from "next/font/google";
 
 export const poppins = Poppins({
   subsets: ["latin"],
@@ -7,6 +7,7 @@ export const poppins = Poppins({
   variable: "--font-poppins",
   display: "swap",
 });
+console.log("poppins:", poppins.variable);
 
 export const inter = Inter({
   subsets: ["latin"],
@@ -15,3 +16,23 @@ export const inter = Inter({
   variable: "--font-inter",
   display: "swap",
 });
+
+// const poppin = localFont({
+//   src: [
+//     {
+//       path: "../../../public/fonts/Poppins-Regular.ttf",
+//       weight: "400",
+//     },
+//     {
+//       path: "../../../public/fonts/Poppins-Bold.ttf",
+//       weight: "700",
+//     },
+//   ],
+//   display: "swap",
+// });
+
+// const myFont = localFont({
+//   src: "../../../public/fonts/Poppins-Regular.ttf",
+//   weight: "400",
+//   display: "swap",
+// });
