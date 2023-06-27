47b9b8fd8c622e9efe51fa9bcff137c85c60110e
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-06-27 22:37:47 +0300

    Update Providers.tsx

--- src/components/Providers/Providers.tsx
+++ src/components/Providers/Providers.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MediaContextProvider } from "@/src/lib/media";
 import { GlobalStyle } from "@/src/styles/theme/GlobalStyle";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "styled-components";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useGlobalState } from "../GlobalProvider/GlobalProvider";
+import { darkTheme, lightTheme } from "@/src/styles/theme/theme";
 
 interface IProps {
   children?: React.ReactNode;
@@ -15,10 +16,10 @@ interface IProps {
 export const Providers = ({ children }: IProps) => {
   const [queryClient] = useState(() => new QueryClient());
   const { theme } = useGlobalState();
-
+ 
   return (
     <>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
         <MediaContextProvider disableDynamicMediaQueries>
           <QueryClientProvider client={queryClient}>
             <SessionProvider>{children}</SessionProvider>
