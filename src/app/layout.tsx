3b2bde0ba85e8deddf3ddffb1822393dd7418d76
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-06-28 20:10:47 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import { Metadata } from "next";
 import StyledComponentsRegistry from "../lib/registry";
-// import "modern-normalize";
 import "../styles/globals.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
@@ -8,13 +7,10 @@ config.autoAddCss = false;
 
 import { Providers } from "../components/Providers/Providers";
 import AuthMenu from "../components/AuthMenu/AuthMenu";
-
 import { mediaStyles } from "../lib/media";
 import AuthProvider from "../components/AuthProvider/AuthProvider";
 import GlobalProvider from "../components/GlobalProvider/GlobalProvider";
-
 import { poppins } from "../lib/fonts";
-import { Inter } from "next/font/google";
 import Fonts from "../lib/FontsFamily";
 
 export const metadata: Metadata = {
@@ -26,18 +22,9 @@ interface IProps {
   children: React.ReactNode;
 }
 
-const inter = Inter({
-  subsets: ["latin"],
-  weight: "400",
-  preload: true,
-});
-
 async function RootLayout({ children }: IProps) {
   return (
-    <html
-      lang="en"
-      //  className={poppins.variable}
-    >
+    <html lang="en">
       <head>
         <link rel="icon" href="/vercel.svg" />
         {/* <style type="text/css" dangerouslySetInnerHTML={{ __html: mediaStyles }}/> */}
