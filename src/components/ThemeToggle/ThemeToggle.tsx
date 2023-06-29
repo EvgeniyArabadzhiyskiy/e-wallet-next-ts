9f9cfe4dd148e23307833b345a4cda5707e0f957
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-06-29 18:33:03 +0300

    Update ThemeToggle.tsx

--- src/components/ThemeToggle/ThemeToggle.tsx
+++ src/components/ThemeToggle/ThemeToggle.tsx
@@ -1,59 +1,12 @@
 "use client";
 
-import { parseCookies, setCookie } from "nookies";
 import { useGlobalState } from "../GlobalProvider/GlobalProvider";
-import { useEffect, useState } from "react";
-
-export const getStorageTheme = (key: string, initialValue: string) => {
-  if (typeof window === "undefined") {
-    return initialValue;
-  }
-
-  const userTheme = window.localStorage.getItem(key);
-
-  return userTheme || initialValue;
-};
-
-export const useLocalStorage = (key: string, initialValue: string) => {
-  const {theme} = parseCookies()
-  const [storageItem, setStorageItem] = useState(getStorageTheme(key, initialValue));
-
-  useEffect(() => {
-    window.localStorage.setItem(key, storageItem);
-  }, [key, storageItem]);
-
-  return {storageItem, setStorageItem};
-};
-
-export const useCookies = () => {
-  const { theme } = parseCookies()
-  const [storageItem, setStorageItem] = useState(theme);
-
-  useEffect(() => {
-    setCookie(null, "theme", `${storageItem}`, {
-      maxAge: 30 * 24 * 60 * 60,
-      path: "/",
-    });
-  }, [storageItem]);
-
-  return {storageItem, setStorageItem};
-};
 
 export default function ThemeToggle() {
   const { theme, setTheme } = useGlobalState();
 
   const onToggleTheme = () => {
-    setTheme(theme === "light" ? "dark" : "light")
-
-    // if (theme === "light") {
-    //   // setStorageItem("dark");
-    //   setTheme("dark");
-    // }
-
-    // if (theme === "dark") {
-    //   // setStorageItem("light");
-    //   setTheme("light");
-    // }
+    setTheme((prev) => (prev === "light" ? "dark" : "light"));
   };
 
   return (
@@ -64,5 +17,3 @@ export default function ThemeToggle() {
     </>
   );
 }
-
-
