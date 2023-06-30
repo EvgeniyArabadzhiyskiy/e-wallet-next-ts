bcbce96b12edc6ac7e94fedc90bde7cbeb0b2ddf
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-06-30 21:24:37 +0300

    Update DashBoardLayout.tsx

--- src/components/DashBoardLayout/DashBoardLayout.tsx
+++ src/components/DashBoardLayout/DashBoardLayout.tsx
@@ -2,6 +2,7 @@
 
 import Container from "../Container/Container";
 import Header from "../Header/Header";
+import Navigation from "../Navigation/Navigation";
 import stl from "./DashBoardLayout.module.scss";
 
 export default function DashBoardLayout({ children }: { children: React.ReactNode }) {
@@ -11,7 +12,9 @@ export default function DashBoardLayout({ children }: { children: React.ReactNod
       <div className={stl.section}>
         <Container>
           <div className={stl.wrapper}>
-             <div className={stl.sidebar}></div>
+             <div className={stl.sidebar}>
+              <Navigation />
+             </div>
             
             {children}
             </div>
