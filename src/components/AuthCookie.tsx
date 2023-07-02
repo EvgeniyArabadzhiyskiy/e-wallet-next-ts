85dbd5afcd7aba1c2b52b14353ca118134af7164
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-02 21:49:19 +0300

    Update AuthCookie.tsx

--- src/components/AuthCookie.tsx
+++ src/components/AuthCookie.tsx
@@ -1,18 +1,76 @@
-// "use client"
+// "use client";
 
 import { getServerSession } from "next-auth";
 import { cookies } from "next/headers";
 import { use } from "react";
 import { authOptions } from "../lib/auth";
+import axios, { AxiosError } from "axios";
+import { useQuery } from "@tanstack/react-query";
 
+// export const dynamic = 'force-dynamic'
+
+// "https://pokeapi.co/api/v2/pokemon?offset=0&limit=10"
 const getPokemons = async () => {
-  const res = await fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=10")
-  const {results} = await res.json()
-  // console.log("============================================results", results);
-  return results 
-}
+  // console.log("getPokemons  response:", response);
 
-export default async function AuthCookie  () {
+  // if (!response.ok) {
+  //   throw new Error("Not Found");
+  // }
+
+  try {
+    const response = await fetch(
+      "https://pokeapi.co/api/v2/pokemon?offset=0&limit=10"
+    );
+    // console.log("getPokemons  response:", response.status);
+
+    if (!response.ok) {
+      throw new Error("Not Found");
+    }
+
+    const data = await response.json();
+
+    return data;
+  } catch (error) {
+    throw new Error("Network Error");
+  }
+};
+
+const getPokemonsAxios = async () => {
+  // try {
+  const response = await axios(
+    "https://pokeapi.co/api/v2/pokemon?offset=0&limit=10"
+  );
+  // console.log("getPokemons  response:", response);
+
+  return response.data;
+  // } catch (error) {
+  //   throw error as AxiosError;
+  //   // throw new Error("Network Error");
+  // }
+};
+
+// getPokemons()
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err.message));
+
+// getPokemonsAxios()
+//   .then((data) => console.log(data))
+//   .catch((err: AxiosError) => console.log(err));
+// .catch((err: AxiosError) => console.log(err.message));
+// .catch((err) => console.log(err.response.data))
+
+export default async function AuthCookie() {
+  // const { data, isError, error } = useQuery({
+  //   queryKey: ["Pokemon"],
+  //   queryFn: getPokemons,
+  //   staleTime: Infinity,
+  //   refetchOnWindowFocus: false,
+  //   retry: 0,
+  // });
+
+  // console.log("AuthCookie  error:", error);
+  // console.log("AuthCookie  data:", data);
+
   // const session = await getServerSession(authOptions);
   // console.log("AuthCookie  session:", session);
 
@@ -22,15 +80,24 @@ export default async function AuthCookie  () {
   // const session = use(getServerSession(authOptions))
   // console.log("AuthCookie  session:", session);
 
-  const data = await getPokemons()
-  console.log("AuthCookie  data:", data);
- 
+  const data = await getPokemonsAxios();
+  console.log("AuthCookie  data:+++++++++++++++++++++++++", data.results[0]);
+
+  // if (isError) {
+  //   return (
+  //     <>
+  //       {/* <h1>{(error as AxiosError).message}</h1> */}
+  //       <h1>{(error as Error).message}</h1>
+  //     </>
+  //   );
+  // }
+
   return (
     <>
-      <h1>{JSON.stringify(data)}</h1>
+      {/* <><pre>{JSON.stringify(data.results[0], null, 2)}</pre></> */}
       {/* <h1>Auth Token: {authToken}</h1> */}
     </>
   );
-};
+}
 
- AuthCookie;
+AuthCookie;
