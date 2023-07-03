b05e74620651e3f29d7371a621ff1b7e8286825d
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-03 21:37:52 +0300

    Create ErrorBoundary.tsx

---
+++ src/components/ErrorBoundary.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import React, { Component, ErrorInfo, ReactNode } from 'react';
+
+interface ErrorBoundaryProps {
+  children: ReactNode;
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error: Error, info: ErrorInfo) {
+    this.setState({ hasError: true });
+    console.error(error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Что-то пошло не так.</h1>;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
