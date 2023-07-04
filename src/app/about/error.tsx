4068854019feeefc3a2dec27493370e1709e917a
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-04 19:24:37 +0300

    Create error.tsx

---
+++ src/app/about/error.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error;
+  reset: () => void;
+}) {
+  console.log("error:", error.message);
+
+  // useEffect(() => {
+  //   // Log the error to an error reporting service
+  //   console.error(error)
+  // }, [error])
+  
+  return (
+    <>
+      <h1>{error.message}</h1>
+      <button type="button" onClick={reset}>
+        Try Again
+      </button>
+    </>
+  );
+}
