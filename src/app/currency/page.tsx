97eb7ebde0ffa3d8f3525d105cdae75c9060a55c
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-04 19:24:45 +0300

    Update page.tsx

--- src/app/currency/page.tsx
+++ src/app/currency/page.tsx
@@ -1,26 +1,32 @@
-"use client";
+// "use client";
 
+import ErrorBoundary from "@/src/components/ErrorBoundary";
+import TestError from "@/src/components/TestError";
 import Link from "next/link";
 import { redirect } from "next/navigation";
 import { useMedia } from "react-use";
 
 export default function PageCurrency() {
-  const isDesctop = useMedia("(min-width: 768px)", false);
+  // const isDesctop = useMedia("(min-width: 768px)", false);
 
-  if (isDesctop) {
-      redirect("/")
-    
-  }
+  // if (isDesctop) {
+  //     redirect("/")
 
-//   console.log("PageCurrency  isDesctop:=======================", isDesctop);
+  // }
 
-//   if (!isDesctop) {
-//     return <h1>Loading Title..</h1>;
-//   }
+  //   console.log("PageCurrency  isDesctop:=======================", isDesctop);
+
+  //   if (!isDesctop) {
+  //     return <h1>Loading Title..</h1>;
+  //   }
   return (
     <>
       <Link href="/">HOME</Link>
-      {isDesctop && <h1>Page Currency</h1>}
+      <ErrorBoundary>
+        <TestError />
+      </ErrorBoundary>
+
+      {/* {isDesctop && <h1>Page Currency</h1>} */}
     </>
   );
 }
