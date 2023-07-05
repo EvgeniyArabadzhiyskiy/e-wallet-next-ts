3b48d3af1be25198d4429d9f83cd3c2a22d2aa11
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-05 18:24:33 +0300

    Update AuthCookie.tsx

--- src/components/AuthCookie.tsx
+++ src/components/AuthCookie.tsx
@@ -1,4 +1,4 @@
-"use client";
+// "use client";
 
 import { getServerSession } from "next-auth";
 import { cookies } from "next/headers";
@@ -10,6 +10,82 @@ import { useQuery } from "@tanstack/react-query";
 // export const dynamic = 'force-dynamic'
 
 // "https://pokeapi.co/api/v2/pokemon?offset=0&limit=10"
+
+type ApiResponse<T> = {
+  data: T;
+  // error: string | null;
+};
+
+interface IPokemon {
+  name: string;
+  url: string;
+}
+
+interface IPokemons {
+  results: IPokemon[];
+}
+
+async function fetchData<T>(url: string): Promise<T> {
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Request failed with status ${response.status}`);
+    }
+
+    const data: T = await response.json();
+
+    return data;
+  } catch (error) {
+    console.log("getPok  error:", (error as Error).message);
+    throw error;
+    // throw new Error("Not Found");
+  }
+}
+
+// const httpFetcher = async (url: string) => {
+//   const response = await fetch(url);
+
+//   const data = await response.json();
+
+//   if (!response.ok) {
+//     throw new Error("Not Found");
+//   }
+
+//   return data
+// }
+
+interface HttpResponse<T> extends Response {
+  parseBody?: T;
+}
+
+const httpFetcher = async <T,>(url: string): Promise<HttpResponse<T>> => {
+  const response: HttpResponse<T> = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error("Not Found");
+  }
+
+  response.parseBody = await response.json();
+
+  return response;
+};
+
+// async function httpFetcher<T>(url: string): Promise<HttpResponse<T>> {
+//   const response: HttpResponse<T> = await fetch(url);
+
+//   response.parseBody = await response.json();
+
+//   return response;
+// }
+
+const getPok = async (url: string) => {
+  return await fetchData<IPokemons>(url);
+};
+
+// getPok()
+//   .then((data) => console.log(data))
+//   .catch((err) => console.log(err.message));
 
 export const getPokemons = async () => {
   try {
@@ -56,27 +132,36 @@ const getPokemonsAxios = async () => {
 // .catch((err: AxiosError) => console.log(err.message));
 // .catch((err) => console.log(err.response.data))
 
-export default   function AuthCookie() {
-  const [counter, setCounter] = useState(0);
+export default async function AuthCookie({url}: {url:string}) {
+  // const [counter, setCounter] = useState(0);
+  // const [data, setData] = useState<IPokemons>();
+  // const [error, setError] = useState<any>(null);
 
   // const { data, isError, error } = useQuery({
   //   queryKey: ["Pokemon"],
-  //   queryFn: getPokemons,
+  //   queryFn: getPok,
   //   staleTime: Infinity,
   //   refetchOnWindowFocus: false,
   //   retry: 0,
   // });
 
   // useEffect(() => {
-  //   throw new Error("Custom Error");
+  //   (async () => {
+  //     try {
+  //       const pokemon = await getPok();
+  //       setData(pokemon);
+  //     } catch (error) {
+  //       console.log("getPok  error:", (error as Error).message);
+  //       // throw error;
+
+  //       setError(error)
+  //     }
+  //   })();
   // }, []);
 
   // getPokemons()
   // .then((data) => console.log(data))
   // .catch((err) => console.log(err.message));
-
-  // console.log("AuthCookie  error:", error);
-  // console.log("AuthCookie  data:", data);
 
   // const session = await getServerSession(authOptions);
   // console.log("AuthCookie  session:", session);
@@ -87,10 +172,11 @@ export default   function AuthCookie() {
   // const session = use(getServerSession(authOptions))
   // console.log("AuthCookie  session:", session);
 
-  // const data = await getPokemons();
-  // console.log("AuthCookie  data:+++++++++++++++++++++++++", data.results[0]);
+  const data = await getPok(url);
+  console.log("AuthCookie  data:+++++++++++++++++++++++++", data.results[0]);
 
-  // if (isError) {
+  // if (error) {
+  //   console.log("AuthCookie  error:", (error as Error).message);
   //   return (
   //     <>
   //       {/* <h1>{(error as AxiosError).message}</h1> */}
@@ -99,21 +185,28 @@ export default   function AuthCookie() {
   //   );
   // }
 
-  const handleClick = () => {
-    setCounter((prev) => prev + 1);
-  };
+  // const handleClick = () => {
+  //   setCounter((prev) => prev + 1);
+  // };
 
-  if (counter === 3) {
-    throw new Error("I crashed!");
-  }
+  // if (counter === 3) {
+  //   throw new Error("I crashed!");
+  // }
 
   return (
     <>
-      <h1>{counter}</h1>
+      <h1>About</h1>
+      {/* <h1>{counter}</h1>
       <button type="button" onClick={handleClick}>
         Click
-      </button>
-      {/* {data && <><pre>{JSON.stringify(data.results[0], null, 2)}</pre></>} */}
+      </button> */}
+
+      {data && (
+        <>
+          <pre>{JSON.stringify(data.results[0], null, 2)}</pre>
+        </>
+      )}
+
       {/* <h1>Auth Token: {authToken}</h1> */}
     </>
   );
