de998780fa1e3985ba61c2e9a643d0967ddf5eba
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-05 21:29:28 +0300

    Update Providers.tsx

--- src/components/Providers/Providers.tsx
+++ src/components/Providers/Providers.tsx
@@ -18,11 +18,12 @@ export const Providers = ({ children }: IProps) => {
   const [queryClient] = useState(() => new QueryClient());
   const { isLoading, theme } = useGlobalState();
 
-  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYTM0ZGFhMTQyNGVhZDExNWVhNTJhNSIsImlhdCI6MTY4ODQwNTUyNiwiZXhwIjoxNjg5NjE1MTI2fQ.nTUHoyF8mdoMniLDqUw5ZphOVBqWFWx4thg-DM3dVhg'
-        setCookie(null, "authToken", `${token}`, {
-        maxAge: 30 * 24 * 60 * 60,
-        path: "/",
-      });
+  // const token =
+  //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYTM0ZGFhMTQyNGVhZDExNWVhNTJhNSIsImlhdCI6MTY4ODQwNTUyNiwiZXhwIjoxNjg5NjE1MTI2fQ.nTUHoyF8mdoMniLDqUw5ZphOVBqWFWx4thg-DM3dVhg";
+  // setCookie(null, "authToken", `${token}`, {
+  //   maxAge: 30 * 24 * 60 * 60,
+  //   path: "/",
+  // });
 
   // if (isLoading) {
   //   return <h1>Loading Theme..</h1>;
