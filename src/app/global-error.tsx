c67d74d49351c3ab2222549920cb542ff3abc74b
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-06 12:00:02 +0300

    Update global-error.tsx

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -10,6 +10,7 @@ export default function GlobalError({
   return (
     <html>
       <body>
+        <h1>Global Error</h1>
         <h2>{error.message}</h2>
         <button onClick={() => reset()}>Try again</button>
       </body>
