78d9e21b2ab29157799b1b168b56b3151b38b598
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-08 20:48:40 +0300

    Create page.tsx

---
+++ src/app/home/transactions/page.tsx
@@ -0,0 +1,61 @@
+import { authOptions } from "@/src/lib/auth";
+import { getServerSession } from "next-auth";
+import Link from "next/link";
+
+import { Hydrate, dehydrate, useQuery } from "@tanstack/react-query";
+import { apiWallet } from "@/src/apiWallet/apiWallet";
+import getQueryClient from "@/src/lib/getQueryClient";
+import TransactionList from "@/src/components/TransactionList/TransactionList";
+import ModalBox from "@/src/components/ModalBox";
+import ModalLogOut from "@/src/components/ModalLogOut/ModalLogOut";
+
+// const getAllTransactions = async (authToken: string | undefined, pageNum: number) => {
+//   const options: RequestInit = {
+//     method: "GET",
+//     headers: {
+//       Authorization: `Bearer ${authToken}`
+//     },
+//     cache: "no-store",
+//   };
+
+//   try {
+//     const data = await fetcher<any>(`${BASE_URL}${TRANSACTIONS}?page=${pageNum}&limit=10`, options);
+    
+//     if (!isITransactions(data)) {
+//       throw new Error("Invalid data format");
+//     }
+
+//     const transactions: ITransactions = data;
+
+//     return transactions;
+
+//   } catch (error) {
+//     console.log("getTrans  Error:", (error as Error).message);
+//     throw error
+//   }
+// }
+
+export default async function PageTransactions() {
+  const session = await getServerSession(authOptions);
+  const authToken = session?.user.token;
+  // console.log("HomePage  authToken>>>>>>>>>>>>>>>>>>>>>>>>>>>>", authToken);
+
+  const queryClient = getQueryClient();
+  await queryClient.prefetchQuery(["Transactions", 1], () =>
+    apiWallet.getAllTransactions(authToken, 1)
+  );
+  const dehydratedState = dehydrate(queryClient);
+  return (
+    <Hydrate state={dehydratedState}>
+      <Link href="/">HOME</Link>
+      <h1>Page Transactions</h1>
+      <div style={{ height: 250, backgroundColor: "red" }}></div>
+
+      <TransactionList authToken={authToken} />
+
+      <ModalBox modalName="logout">
+        <ModalLogOut />
+      </ModalBox>
+    </Hydrate>
+  );
+}
