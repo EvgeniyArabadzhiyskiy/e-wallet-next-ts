392d663711b8d6421f566a63c2c32556140b4cdb
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-08 20:48:37 +0300

    Create page.tsx

---
+++ src/app/home/statistic/page.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function PageStatistic() {
+  return (
+    <>
+      <Link href="/">HOME</Link>
+      <h1>Page Statistic</h1>
+      <div style={{height:250, backgroundColor: "green"}} ></div>
+    </>
+  );
+}
