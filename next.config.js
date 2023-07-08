53c198a78964f172ea90a1109748d7fd0e8edea8
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-08 15:28:59 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
+const path = require("path");
 
 // const withImages = require('next-images');
 
 const nextConfig = {
- // reactStrictMode: true,
+  // reactStrictMode: true,
 
   // experimental: {
   //   appDir: true,
@@ -19,10 +19,19 @@ const nextConfig = {
   //   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET
   // },
 
-
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
   },
+
+  
+  // ====== SVG =========
+  // webpack: (config) => {
+  //   config.module.rules.push({
+  //     test: /\.svg$/i,
+  //     use: ['@svgr/webpack'],
+  //   });
+  //   return config;
+  // },
 };
 
 module.exports = nextConfig;
