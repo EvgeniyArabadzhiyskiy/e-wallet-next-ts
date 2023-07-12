8d3e106bcb6c0a6cdf4ec35008ae1ca12c7e4d6e
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-12 17:37:45 +0300

    Update page.tsx

--- src/app/home/statistic/page.tsx
+++ src/app/home/statistic/page.tsx
@@ -1,11 +1,47 @@
+"use client"
+import { apiWallet } from "@/src/apiWallet/apiWallet";
+import { useQuery } from "@tanstack/react-query";
+import { useSession } from "next-auth/react";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function PageStatistic() {
+  const [pageNum, setPageNum] = useState(1);
+  // console.log("PageStatistic  pageNum:", pageNum);
+
+  const session = useSession();
+  const userToken = session.data?.user.token;
+  console.log("PageStatistic  userToken:", userToken);
+
+    const { data, isError, error, isFetching, refetch } = useQuery({
+    queryKey: ["Transactions", 1],
+    queryFn: () => apiWallet.getAllTransactions(userToken, 1), // ИЛИ authToken
+    staleTime: Infinity,
+    refetchOnWindowFocus: false,
+    retry: 0,
+    enabled: !!userToken, // При authToken  Удалить
+  });
+    console.log("PageStatistic  data:", data);
+
+
+  useEffect(() => {
+    console.log("useEffect");
+    
+
+    return () => {
+      console.log("Unmount");
+    };
+  },);
+
+  console.log("Rerender");
   return (
     <>
       <Link href="/">HOME</Link>
-      <h1>Page Statistic</h1>
+      <h1>Page Statistic </h1>
       <div style={{height:250, backgroundColor: "green"}} ></div>
+      <button type="button" onClick={() => setPageNum((p) => p + 1)}>
+        Click
+      </button>
     </>
   );
 }
