b484cfecbed4ed81d92a1ad41939aeb0d56834bc
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-12 17:58:09 +0300

    Update TransactionList.tsx

--- src/components/TransactionList/TransactionList.tsx
+++ src/components/TransactionList/TransactionList.tsx
@@ -57,126 +57,17 @@ const getAllTransactions = async (authToken: any, pageNum: number) => {
 };
 
 const TransactionList = ({ authToken }: { authToken?: string | undefined }) => {
-  // const { authToken } = parseCookies();
-  // const queryClient = useQueryClient();
 
-  // const { data, isFetching, listElem, observerElem } = useLazyTransactions()
-
-  const listElem = useRef<HTMLUListElement>(null);
-  const observerElem = useRef<HTMLDivElement>(null);
-
-  const [pageNum, setPageNum] = useState(1);
-  const session = useSession();
-  const userToken = session.data?.user.token;
-
-
-  // const { data, isError, error, isFetching, refetch } = useQuery({
-  //   queryKey: ["Transactions", pageNum],
-  //   queryFn: () => apiWallet.getAllTransactions(userToken, pageNum), // ИЛИ authToken
-  //   staleTime: Infinity,
-  //   refetchOnWindowFocus: false,
-  //   retry: 0,
-  //   enabled: !!userToken, // При authToken  Удалить
-  // });
-
-  const { data: { pages } = {}, isFetching, fetchNextPage, hasNextPage } = useInfiniteQuery({
-    queryKey: ["TransactionsList"],
-    queryFn: ({ pageParam = 1 }) =>
-      apiWallet.getAllTransactions(userToken, pageParam),
-    getNextPageParam: (lastPage, allPages) => {
-      const nextPage = allPages.length + 1;
-
-      return lastPage.transactions.length !== 0 ? nextPage : undefined;
-    },
-
-    staleTime: Infinity,
-    enabled: !!userToken,
-    select: (data) => {
-      const transactions = data.pages.map(({ transactions }) => transactions).flat();
-      return {
-        ...data,
-        pages: transactions
-      }
-    }
-  });
-
-
-  // console.log("TransactionList  data:", allTransactions?.pages);
-  // console.log("TransactionList  data:", allTransactions);
-
-  // const allTransactions = data?.pages.map(({transactions}) => transactions).flat();
-  // console.log("TransactionList  allTransactions:", allTransactions);
-
-  // const allTransactions = useMemo(() => {
-  //   return data?.pages.map(({ transactions }) => transactions).flat();
-  // }, [data?.pages]);
-
-  //   const queryUserData = queryClient.getQueriesData<any>(["Transactions"]);
-  //   console.log("Header  queryUserData:", queryUserData);
-
-  // const handleObserver = useCallback(
-  //   (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
-  //     const { isIntersecting, target } = entries[0];
-
-  //     if (isIntersecting && hasNextPage) {
-  //       fetchNextPage();
-  //       // console.log("Target is Intersect");
-  //       // setPageNum((p) => p + 1)
-  //     }
-  //   },
-  //   [fetchNextPage, hasNextPage]
-  // );
-
-  useEffect(() => {
-    const target = observerElem.current;
-
-    const observer = new IntersectionObserver((entries) => {
-      const { isIntersecting } = entries[0];
-
-      if (isIntersecting && hasNextPage) {
-        fetchNextPage();
-      }
-    }, 
-    { root: listElem.current, rootMargin: "10px" });
-
-    if (target) {
-      observer.observe(target);
-    }
-
-    return () => {
-      if (target) {
-        observer.unobserve(target);
-      }
-    };
-  }, [fetchNextPage, hasNextPage]);
-
-  // if (isError) {
-  //   return (
-  //     <>
-  //       <h1>{(error as Error).message}</h1>
-  //       <button onClick={() => refetch()}>Retry</button>
-  //     </>
-  //   );
-  // }
-
-  console.log("Rerender");
+  const { data: allTransactions = [], isFetching, listElem, observerElem } = useLazyTransactions()
 
   return (
     <>
       <h1>HOME PAGE</h1>
       <Title>Title</Title>
       <Link href="/">HOME</Link>
-      {/* <button type="button" onClick={() => fetchNextPage()}>
-        Next Page
-      </button> */}
-
-      <button type="button" onClick={() => setPageNum((p) => p + 1)}>
-        Click
-      </button>
-
+      
       <ul ref={listElem} style={{ height: 240, overflowY: "scroll" }}>
-        {pages &&
-          pages.map((item) => {
+        { allTransactions.map((item) => {
             return (
               <div key={item._id}>
                 {isFetching ? (
@@ -197,6 +88,149 @@ const TransactionList = ({ authToken }: { authToken?: string | undefined }) => {
 };
 
 export default TransactionList;
+
+
+//=================================================================================
+// const TransactionList = ({ authToken }: { authToken?: string | undefined }) => {
+//   // const { authToken } = parseCookies();
+//   // const queryClient = useQueryClient();
+
+//   const { data: allTransactions = [], isFetching, listElem, observerElem } = useLazyTransactions()
+
+//   // const listElem = useRef<HTMLUListElement>(null);
+//   // const observerElem = useRef<HTMLDivElement>(null);
+
+//   const [pageNum, setPageNum] = useState(1);
+//   const session = useSession();
+//   const userToken = session.data?.user.token;
+
+
+//   // const { data, isError, error, isFetching, refetch } = useQuery({
+//   //   queryKey: ["Transactions", pageNum],
+//   //   queryFn: () => apiWallet.getAllTransactions(userToken, pageNum), // ИЛИ authToken
+//   //   staleTime: Infinity,
+//   //   refetchOnWindowFocus: false,
+//   //   retry: 0,
+//   //   enabled: !!userToken, // При authToken  Удалить
+//   // });
+
+//   // const { data: { pages } = {}, isFetching, fetchNextPage, hasNextPage } = useInfiniteQuery({
+//   //   queryKey: ["TransactionsList"],
+//   //   queryFn: ({ pageParam = 1 }) =>
+//   //     apiWallet.getAllTransactions(userToken, pageParam),
+//   //   getNextPageParam: (lastPage, allPages) => {
+//   //     const nextPage = allPages.length + 1;
+
+//   //     return lastPage.transactions.length !== 0 ? nextPage : undefined;
+//   //   },
+
+//   //   staleTime: Infinity,
+//   //   enabled: !!userToken,
+//   //   select: (data) => {
+//   //     const transactions = data.pages.map(({ transactions }) => transactions).flat();
+//   //     return {
+//   //       ...data,
+//   //       pages: transactions
+//   //     }
+//   //   }
+//   // });
+
+
+//   // console.log("TransactionList  data:", allTransactions?.pages);
+//   // console.log("TransactionList  data:", allTransactions);
+
+//   // const allTransactions = data?.pages.map(({transactions}) => transactions).flat();
+//   // console.log("TransactionList  allTransactions:", allTransactions);
+
+//   // const allTransactions = useMemo(() => {
+//   //   return data?.pages.map(({ transactions }) => transactions).flat();
+//   // }, [data?.pages]);
+
+//   //   const queryUserData = queryClient.getQueriesData<any>(["Transactions"]);
+//   //   console.log("Header  queryUserData:", queryUserData);
+
+//   // const handleObserver = useCallback(
+//   //   (entries: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+//   //     const { isIntersecting, target } = entries[0];
+
+//   //     if (isIntersecting && hasNextPage) {
+//   //       fetchNextPage();
+//   //       // console.log("Target is Intersect");
+//   //       // setPageNum((p) => p + 1)
+//   //     }
+//   //   },
+//   //   [fetchNextPage, hasNextPage]
+//   // );
+
+//   // useEffect(() => {
+//   //   const target = observerElem.current;
+
+//   //   const observer = new IntersectionObserver((entries) => {
+//   //     const { isIntersecting } = entries[0];
+
+//   //     if (isIntersecting && hasNextPage) {
+//   //       fetchNextPage();
+//   //     }
+//   //   }, 
+//   //   { root: listElem.current, rootMargin: "10px" });
+
+//   //   if (target) {
+//   //     observer.observe(target);
+//   //   }
+
+//   //   return () => {
+//   //     if (target) {
+//   //       observer.unobserve(target);
+//   //     }
+//   //   };
+//   // }, [fetchNextPage, hasNextPage]);
+
+//   // if (isError) {
+//   //   return (
+//   //     <>
+//   //       <h1>{(error as Error).message}</h1>
+//   //       <button onClick={() => refetch()}>Retry</button>
+//   //     </>
+//   //   );
+//   // }
+
+//   console.log("Rerender");
+
+//   return (
+//     <>
+//       <h1>HOME PAGE</h1>
+//       <Title>Title</Title>
+//       <Link href="/">HOME</Link>
+//       {/* <button type="button" onClick={() => fetchNextPage()}>
+//         Next Page
+//       </button> */}
+
+//       <button type="button" onClick={() => setPageNum((p) => p + 1)}>
+//         Click
+//       </button>
+
+//       <ul ref={listElem} style={{ height: 240, overflowY: "scroll" }}>
+//         { allTransactions.map((item) => {
+//             return (
+//               <div key={item._id}>
+//                 {isFetching ? (
+//                   <div style={{ height: 50 }}>Loading Transactions...</div>
+//                 ) : (
+//                   <div style={{ height: 50 }}>{item.category}</div>
+//                 )}
+//               </div>
+//             );
+//           })}
+
+//         <div ref={observerElem} style={{ height: 50, background: "tomato" }}>
+//           Observer Target
+//         </div>
+//       </ul>
+//     </>
+//   );
+// };
+
+// export default TransactionList;
 
 // const TransactionList = ({ authToken }: { authToken?: string | undefined }) => {
 //   const observerElem = useRef<any>(null);
