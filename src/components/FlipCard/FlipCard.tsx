de277b162204f83a86b0ee467c1951bf766438e8
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-13 21:22:38 +0300

    Create FlipCard.tsx

---
+++ src/components/FlipCard/FlipCard.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { useState } from "react";
+import { Card, CardBack, CardFront, InnerCard } from "./FlipCard.styled";
+import TransactionForm from "../TransactionForm/TransactionForm";
+
+
+export default function FlipCard ()  {
+    const [isIncome, setIsIncome] = useState(false);
+  
+    return (
+      <Card>
+        <InnerCard isFlipped={isIncome}>
+          <CardFront>
+            <TransactionForm setIsIncome={setIsIncome} isIncome={isIncome} />
+          </CardFront>
+  
+          <CardBack>
+            <TransactionForm setIsIncome={setIsIncome} isIncome={isIncome} />
+          </CardBack>
+        </InnerCard>
+      </Card>
+    );
+  };
+  
+   
