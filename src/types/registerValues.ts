188a4cc49556760139753b0f0f58aaad741c2f24
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-14 21:57:35 +0300

    Update registerValues.ts

--- src/types/registerValues.ts
+++ src/types/registerValues.ts
@@ -1,6 +1,12 @@
 export interface IRegisterValues {
   email: string;
   password: string;
-  confirmPassword?: string;
-  name?: string;
+  confirmPassword: string;
+  name: string;
+}
+
+export interface ICredentials {
+  email: string;
+  password: string;
+  firstName: string;
 }
