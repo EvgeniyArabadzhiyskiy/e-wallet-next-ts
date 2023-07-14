c62b8e07d0144bc748ca2a008a6f4f9bf8fb49b0
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-14 21:57:25 +0300

    Create selectOptions.ts

---
+++ src/constants/selectOptions.ts
@@ -0,0 +1,12 @@
+export const optionsExpense = [
+  { value: "main", label: "Main" },
+  { value: "food", label: "Food" },
+  { value: "auto", label: "Auto" },
+  { value: "development", label: "Development" },
+  { value: "children", label: "Children" },
+  { value: "house", label: "House" },
+  { value: "education", label: "Education" },
+  { value: "other", label: "Other expenses" },
+];
+
+export const optionsIncome = [{ value: "regular", label: "Regular Income" }];
