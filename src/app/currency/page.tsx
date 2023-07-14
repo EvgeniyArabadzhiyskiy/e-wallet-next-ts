3143ea7034feaa7d1e5b158abe24ab33b6a876f5
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-14 21:57:08 +0300

    Update page.tsx

--- src/app/currency/page.tsx
+++ src/app/currency/page.tsx
@@ -5,22 +5,77 @@ import { redirect } from "next/navigation";
 import { useMedia } from "react-use";
 
 export default function PageCurrency() {
-  const isDesctop = useMedia("(min-width: 768px)", false);
+  // const isDesctop = useMedia("(min-width: 768px)", false);
 
-  if (isDesctop) {
-      redirect("/")
+  // if (isDesctop) {
+  //     redirect("/")
     
-  }
+  // }
 
-//   console.log("PageCurrency  isDesctop:=======================", isDesctop);
-
-//   if (!isDesctop) {
-//     return <h1>Loading Title..</h1>;
-//   }
   return (
     <>
       <Link href="/">HOME</Link>
-      {isDesctop && <h1>Page Currency</h1>}
+      {/* {isDesctop && <h1>Page Currency</h1>} */}
     </>
   );
 }
+
+// interface IUser<T> {
+//   user: T
+// }
+
+// interface Admin extends Person {
+//   role: string;
+// }
+
+
+// interface Person {
+//   name: string;
+//   age: number;
+// }
+
+// interface User  extends Person {
+//   email: string;
+// }
+
+
+// const user: User = {
+//   email: "poly@mail.com",
+//   name: "Poly",
+//   age: 35,
+// }
+
+// function generic<T extends Person>(params: T ): T   {
+  
+//   const result: T =  params
+//  return result
+  
+// }
+
+// const res = generic<User>(user)
+
+
+interface Animal {
+  name: string;
+  age: number;
+}
+
+interface Cat extends Animal {
+  meow(): void;
+}
+
+function processAnimal<T extends Animal & Cat>(animal: T): void {
+  const ddd = animal.meow()
+  console.log(`Name: ${animal.name}`);
+  console.log(`Age: ${animal.age}`);
+}
+
+const cat: Cat = {
+  name: "Whiskers",
+  age: 5,
+  meow() {
+    console.log("Meow!");
+  },
+};
+
+processAnimal(cat);
