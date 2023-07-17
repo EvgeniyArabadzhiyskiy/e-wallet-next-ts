81dc6ffe2a3d07930dc2111d4a8eb3d5ebe2251e
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-17 21:06:01 +0300

    Update TransactionForm.tsx

--- src/components/TransactionForm/TransactionForm.tsx
+++ src/components/TransactionForm/TransactionForm.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { ITransactionValue } from "@/src/types/transactionValue";
 import FormContainer from "../FormContainer/FormContainer";
 import { Title } from "./TransactionForm.styled";
@@ -5,10 +7,32 @@ import { getTypeOperation } from "@/src/helpers/getTypeOperation";
 import TransactionFormFields from "../TransactionFormFields/TransactionFormFields";
 import { Form, Formik, FormikProps, FormikHelpers } from "formik";
 import { ILoginValues } from "@/src/types/loginValues";
+import { createTransaction } from "@/src/apiWallet/createTransaction";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import schema from "@/src/helpers/formValidation";
+import axios from "axios";
+import { BASE_URL, TRANSACTIONS } from "@/src/constants/apiPath";
 
+const transData = {
+  amount: 500,
+  category: "Next Js",
+  typeOperation: "expense",
+  comment: "Fruits",
+  // date: "Sun Apr 09 2023 16:49:02 GMT+0300 (Восточная Европа, летнее время)",
+  // date: "Wed Apr 05 2023 21:41:36 GMT+0300 (Восточная Европа, летнее время)",
+  date: new Date().toString(),
+};
 
+// export const create = async (transaction: any) => {
+//   const token =
+//     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYTM0ZGFhMTQyNGVhZDExNWVhNTJhNSIsImlhdCI6MTY4OTE0NjM0NSwiZXhwIjoxNjkwMzU1OTQ1fQ.GuTYaKcbkLR4odPZ3iJBd6ORIDPHZaAXwVKRfmSpoco";
 
+//   axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
+//   const { data } = await axios.post(`${BASE_URL}${TRANSACTIONS}`, transaction);
+//   console.log("SecondPage  data:", data);
+//   return data;
+// };
 
 interface IProps {
   setIsIncome: any;
@@ -23,6 +47,14 @@ export default function TransactionForm({ isIncome, setIsIncome }: IProps) {
     date: new Date().toString(),
   };
 
+  const mutation = useMutation({
+    mutationFn: createTransaction,
+    
+    onSuccess: (data) => {
+      console.log("onSuccess  data:", data);
+    },
+  });
+
   const handleSubmit = async (
     values: ITransactionValue,
     formikHelpers: FormikHelpers<ITransactionValue>
@@ -34,6 +66,8 @@ export default function TransactionForm({ isIncome, setIsIncome }: IProps) {
       amount: Number(values.amount),
       typeOperation,
     };
+
+    mutation.mutate(transaction);
   };
 
   return (
@@ -42,9 +76,8 @@ export default function TransactionForm({ isIncome, setIsIncome }: IProps) {
 
       <FormContainer<ITransactionValue>
         initialValues={initialValues}
-        validationSchema={{}}
+        validationSchema={schema.transactionShema}
         onSubmit={handleSubmit}
-        
         render={(formik) => (
           <TransactionFormFields
             formik={formik}
