5f14bbe0ec4fe4a500add00247c2034daf72f347
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-18 19:34:28 +0300

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -12,6 +12,8 @@ import LogoutBtn from "../Buttons/LogoutBtn/LogoutBtn";
 import Container from "../Container/Container";
 import Logo from "../Logo/Logo";
 import ThemeToggle from "../ThemeToggle/ThemeToggle";
+import ModalBox from "../ModalWindow/ModalBox";
+import ModalLogOut from "../ModalLogOut/ModalLogOut";
 
 interface UserData {
   email: string;
@@ -26,22 +28,27 @@ export default function Header({ currentUser }: { currentUser?: any }) {
   // console.log("Header re-re");
 
   return (
-    <StyledHeader>
-      <Container>
-        <HeaderWrapper>
-          <Logo />
-          <UserBox>
-            <ThemeToggle />
-            <TextName>
-              {isLading ? <>Load</> : user.data?.user.user.firstName}
-            </TextName>
-            {/* <TextName>SERVER USER {currentUser?.user.user.firstName}</TextName> */}
-            <LogoutBtn modalName="logout" type="exit" />
-          </UserBox>
-          {/* <h2>Balance: {currentUser?.user.user.balance}</h2> */}
-         
-        </HeaderWrapper>
-      </Container>
-    </StyledHeader>
+    <>
+      <StyledHeader>
+        <Container>
+          <HeaderWrapper>
+            <Logo />
+            <UserBox>
+              <ThemeToggle />
+              <TextName>
+                {isLading ? <>Load</> : user.data?.user.user.firstName}
+              </TextName>
+              {/* <TextName>SERVER USER {currentUser?.user.user.firstName}</TextName> */}
+              <LogoutBtn modalName="logout" type="exit" />
+            </UserBox>
+            {/* <h2>Balance: {currentUser?.user.user.balance}</h2> */}
+          </HeaderWrapper>
+        </Container>
+      </StyledHeader>
+
+      <ModalBox modalName="logout">
+        <ModalLogOut />
+      </ModalBox>
+    </>
   );
 }
