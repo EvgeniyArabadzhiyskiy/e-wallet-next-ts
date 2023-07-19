49d0593b02736e415e1fba59bd3129f4644a64dd
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-19 21:12:58 +0300

    Update createTransaction.ts

--- src/apiWallet/createTransaction.ts
+++ src/apiWallet/createTransaction.ts
@@ -1,9 +1,8 @@
 import { fetcher } from "../helpers/fetcher";
 import { BASE_URL, TRANSACTIONS } from "../constants/apiPath";
+import { ITransactionData } from "../types/transactionValue";
 
-export const createTransaction = async (transaction: any) => {
-  const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYTM0ZGFhMTQyNGVhZDExNWVhNTJhNSIsImlhdCI6MTY4OTE0NjM0NSwiZXhwIjoxNjkwMzU1OTQ1fQ.GuTYaKcbkLR4odPZ3iJBd6ORIDPHZaAXwVKRfmSpoco";
+export const createTransaction = async (transaction: ITransactionData, token: string | undefined) => {
 
   const options: RequestInit = {
     method: "POST",
@@ -13,6 +12,7 @@ export const createTransaction = async (transaction: any) => {
     },
     body: JSON.stringify(transaction),
   };
+  
   const data = await fetcher(`${BASE_URL}${TRANSACTIONS}`, options);
 
   return data;
