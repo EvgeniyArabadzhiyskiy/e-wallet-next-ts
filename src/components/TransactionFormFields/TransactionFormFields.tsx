dc72800ee7b2b5f9e90ffd9d03cff939981cce01
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-22 19:49:27 +0300

    Update TransactionFormFields.tsx

--- src/components/TransactionFormFields/TransactionFormFields.tsx
+++ src/components/TransactionFormFields/TransactionFormFields.tsx
@@ -1,8 +1,8 @@
 import "../../styles/rdt-styles.css";
 import moment from "moment";
 import Datetime from "react-datetime";
-import  { GroupBase,  OptionProps } from "react-select";
-import Select from "react-select/dist/declarations/src/Select";
+// import ReactSelect, { GroupBase, OptionProps } from "react-select";
+// import Select from "react-select/dist/declarations/src/Select";
 
 import { Box } from "../Box/Box";
 import { ChangeEvent, useRef, useState } from "react";
@@ -21,16 +21,18 @@ import { verifyFutureDate } from "@/src/helpers/verifyFutureDate";
 import { optionsExpense, optionsIncome } from "@/src/constants/selectOptions";
 import { ITransactionValue } from "@/src/types/transactionValue";
 import { useGlobalState } from "../GlobalProvider/GlobalProvider";
-import CustomSelect from "../SelectComponent/SelectComponent";
-import { IOptionData } from "@/src/types/optionData";
+import CustomSelect from "../CustomSelect/CustomSelect";
+import { OptionType } from "@/src/types/optionType";
+
+
+
+
 
 interface IProps {
   isIncome: boolean;
   setIsIncome: any;
   formik: FormikProps<ITransactionValue>;
 }
-
-interface OptionType {value: string; label: string;}
 
 export default function TransactionFormFields({
   formik,
@@ -39,55 +41,48 @@ export default function TransactionFormFields({
 }: IProps) {
   const { setModalToggle } = useGlobalState();
 
-  const { setFieldValue, isValid, dirty, isSubmitting, values } = formik;
+  const { setFieldValue, isValid, dirty, isSubmitting } = formik;
   const isDisabled = !(isValid && dirty) || isSubmitting;
 
-  
-  const selectInputRef =
-    useRef<Select<unknown, boolean, GroupBase<unknown>>>(null);
+  // const selectInputRef =
+  //   useRef<Select<OptionType, false, GroupBase<OptionType>>>(null);
 
   const currentDate = moment().format("DD.MM.YYYY");
+  const [value, setValue] = useState<OptionType | null>(null);
 
-  
 
   const onChangeSwitch = (e: ChangeEvent<HTMLInputElement>) => {
     setIsIncome(e.target.checked);
+    setValue(null)
     // selectInputRef.current?.clearValue();
   };
 
-  const onSelectChange = (data: any) => {
-    
-    setFieldValue("category", data?.label);
-  };
-
-
   
-
   return (
     <>
       <SwithChecbox isIncome={isIncome} onChangeSwitch={onChangeSwitch} />
 
       <Box mb={5} position="relative">
-        {/* <ReactSelect
+        {/* <ReactSelect<OptionType>
           ref={selectInputRef}
           name="category"
           options={isIncome ? optionsIncome : optionsExpense}
           placeholder="Select a category"
-          onChange={(data: any) => {
-            // setFieldValue("category", data?.label);
-          }}
           styles={selectStyles}
+          onChange={(data) => {
+            console.log("data:", data?.value);
+            
+            setFieldValue("category", data?.label);
+          }}
         /> */}
-        
-        <CustomSelect<OptionType, false, GroupBase<OptionType>>
-          isIncome={isIncome}
-          setFieldValue={setFieldValue}
-          // selectRef={selectInputRef}
 
-          // placeholder="Select a category"
-          // options={isIncome ? optionsIncome : optionsExpense}
-          // styles={selectStyles}
-         />
+        <CustomSelect
+          value={value}
+          setValue={setValue}
+          isIncome={isIncome}
+          setCategory={setFieldValue}
+          // selectRef={selectInputRef}
+        />
         <ErrorText component="div" name="category" />
       </Box>
 
