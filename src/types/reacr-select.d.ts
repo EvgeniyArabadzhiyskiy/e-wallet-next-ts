4ee254285c050115e1daf556f7ef78e1e10be703
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-22 19:49:35 +0300

    Update reacr-select.d.ts

--- src/types/reacr-select.d.ts
+++ src/types/reacr-select.d.ts
@@ -1,10 +1,11 @@
-declare module "react-select/dist/declarations/src/Select" {
-  export interface Props<
-    Option,
-    IsMulti extends boolean,
-    Group extends GroupBase<Option>
-  > {
-    isIncome: boolean;
-    setFieldValue: any;
-  }
-}
+// import { OptionType } from "./optionType";
+
+// declare module "react-select/dist/declarations/src/Select" {
+//   export interface Props<Option, IsMulti extends boolean, Group extends GroupBase<Option>> {
+//     isIncome: boolean;
+//     setValue: Dispatch<SetStateAction<OptionType>>;
+//     value: OptionType;
+//     owner: IOwner;
+//     // setFieldValue: any;
+//   }
+// }
