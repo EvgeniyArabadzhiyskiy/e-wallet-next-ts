37eff17794ad190161d108e949692d9129293436
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-24 20:37:17 +0300

    Update TransactionForm.tsx

--- src/components/TransactionForm/TransactionForm.tsx
+++ src/components/TransactionForm/TransactionForm.tsx
@@ -3,29 +3,49 @@
 import { FormikHelpers } from "formik";
 import { useSession } from "next-auth/react";
 import { useMutation } from "@tanstack/react-query";
-import { ITransactionData, ITransactionValue } from "@/src/types/transactionValue";
 
-// import schema from "@/src/helpers/formValidation";
+import { transactionShema } from "@/src/helpers/formValidation";
 import { useGlobalState } from "../GlobalProvider/GlobalProvider";
 import { getTypeOperation } from "@/src/helpers/getTypeOperation";
-import { createTransaction } from "@/src/apiWallet/createTransaction";
-import TransactionFormFields from "../TransactionFormFields/TransactionFormFields";
+// import { createTransaction } from "@/src/apiWallet/createTransaction";
+import { ITransactionData, ITransactionValue } from "@/src/types/transactionValue";
 
-import { Title } from "./TransactionForm.styled";
 import FormContainer from "../FormContainer/FormContainer";
-import { transactionShema } from "@/src/helpers/formValidation";
+import TransactionFormFields from "../TransactionFormFields/TransactionFormFields";
+import { Title } from "./TransactionForm.styled";
+import { apiWallet } from "@/src/apiWallet/apiWallet";
+import axios from "axios";
+import { useState } from "react";
+import { NewTransaction } from "@/src/types/transactions";
 
 interface IProps {
   setIsIncome: any;
   isIncome: boolean;
 }
 
+// export const createTransaction = async (transaction: any) => {
+
+//   const options = {
+//     headers: {
+//       "Content-Type": "application/json;charset=utf-8",
+//       Authorization: `Bearer ${"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzYTM0ZGFhMTQyNGVhZDExNWVhNTJhNSIsImlhdCI6MTY4OTE0NjM0NSwiZXhwIjoxNjkwMzU1OTQ1fQ.GuTYaKcbkLR4odPZ3iJBd6ORIDPHZaAXwVKRfmSpoco"}`,
+//     },
+//   }
+
+//   const data = await axios.post(`https://wallet-backend-xmk0.onrender.com/api/transaction`, transaction, options);
+//   console.log("createTransaction  data:", data);
+//   return data;
+  
+// };
+
 
 
 export default function TransactionForm({ isIncome, setIsIncome }: IProps) {
-  const { setModalToggle } = useGlobalState();
   const session = useSession();
   const token = session.data?.user.token;
+
+  const { setModalToggle } = useGlobalState();
+  const [error, setError] = useState<Error | null>(null);
 
   const initialValues: ITransactionValue = {
     comment: "",
@@ -34,20 +54,26 @@ export default function TransactionForm({ isIncome, setIsIncome }: IProps) {
     date: new Date().toString(),
   };
 
-  const mutation = useMutation({
-    mutationFn: (transaction: ITransactionData) =>
-      createTransaction(transaction, token),
+  const mutation = useMutation<NewTransaction, Error, ITransactionData>({
+    // createTransaction должна возвращать тип NewTransaction
+    mutationFn: (transaction) =>
+    apiWallet.createTransaction(transaction, token),
 
     onSuccess: (data) => {
-      // console.log("onSuccess data:", data);
+      console.log("onSuccess data:", data);
+      setModalToggle("transaction");
     },
+
+    onError: (error) => {
+      console.log("TransactionForm  error:", error.message);
+      setError(error);
+    }
   });
 
   const onFormSubmit = async (
     values: ITransactionValue,
     { resetForm }: FormikHelpers<ITransactionValue>
   ) => {
-    console.log("TransactionForm  values:", typeof values.amount);
     const typeOperation = getTypeOperation(isIncome);
 
     const transaction: ITransactionData = {
@@ -57,8 +83,20 @@ export default function TransactionForm({ isIncome, setIsIncome }: IProps) {
 
     mutation.mutate(transaction);
     resetForm();
-    setModalToggle("transaction");
+    // setModalToggle("transaction");
   };
+
+  if (error) {
+    return (
+      <>
+        <h1 style={{ color: "white" }}>Произошла ошибка:</h1>
+        <h2 style={{ color: "white" }}>{error.message}</h2>
+        <button type="button" onClick={() => setModalToggle("transaction")}>
+          Cancel
+        </button>
+      </>
+    );
+  }
 
   return (
     <>
