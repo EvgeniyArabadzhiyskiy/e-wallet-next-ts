53569588379ea54be31fccf10cdfe92dc0d71c6a
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-24 20:37:06 +0300

    Update apiWallet.ts

--- src/apiWallet/apiWallet.ts
+++ src/apiWallet/apiWallet.ts
@@ -1,5 +1,7 @@
+import { createTransaction } from "./createTransaction";
 import { getAllTransactions } from "./getAllTransactions";
 
 export const apiWallet = {
+  createTransaction,
   getAllTransactions,
 };
