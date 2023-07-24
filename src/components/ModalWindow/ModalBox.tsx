16fb76ae673dba617dc9e379c64910d5c424cbaa
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-24 20:37:20 +0300

    Update ModalBox.tsx

--- src/components/ModalWindow/ModalBox.tsx
+++ src/components/ModalWindow/ModalBox.tsx
@@ -1,12 +1,10 @@
 "use client";
 
-import { useContext } from "react";
 import ModalWindow from "./ModalWindow";
-import { GlobalContext, useGlobalState } from "../GlobalProvider/GlobalProvider";
+import { useGlobalState } from "../GlobalProvider/GlobalProvider";
 
 const ModalBox = ({ children, modalName }: { children: React.ReactNode; modalName: string }) => {
-  // const { isModalOpen, setModalToggle } = useContext(GlobalContext);
-  const { isModalOpen, setModalToggle } = useGlobalState()
+  const { isModalOpen, setModalToggle } = useGlobalState();
 
   const isOpen = !!isModalOpen[modalName];
 
