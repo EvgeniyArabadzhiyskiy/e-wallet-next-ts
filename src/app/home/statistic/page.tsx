c1c8d94d26542e14a437a1ab8373959885e598fe
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-25 21:38:54 +0300

    Update page.tsx

--- src/app/home/statistic/page.tsx
+++ src/app/home/statistic/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { apiWallet } from "@/src/apiWallet/apiWallet";
+import { useUser } from "@/src/hooks/useUser";
 import { useQuery } from "@tanstack/react-query";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
@@ -8,17 +9,15 @@ import { useEffect, useState } from "react";
 export default function PageStatistic() {
   const [pageNum, setPageNum] = useState(1);
 
-  const session = useSession();
-  const userToken = session.data?.user.token;
-  console.log("PageStatistic  userToken:", userToken);
+  const { token } = useUser();
 
     const { data, isError, error, isFetching } = useQuery({
     queryKey: ["Transactions", 1],
-    queryFn: () => apiWallet.getAllTransactions(userToken, 1), 
+    queryFn: () => apiWallet.getAllTransactions(token, 1), 
     staleTime: Infinity,
     refetchOnWindowFocus: false,
     retry: 0,
-    enabled: !!userToken, 
+    enabled: !!token, 
   });
     console.log("PageStatistic  data:", data);
 
