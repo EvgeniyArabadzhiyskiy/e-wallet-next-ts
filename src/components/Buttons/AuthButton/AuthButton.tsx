c123754c6550f9fee10f9fd9b238bccc2c71c462
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-25 21:39:38 +0300

    Create AuthButton.tsx

---
+++ src/components/Buttons/AuthButton/AuthButton.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useUser } from "@/src/hooks/useUser";
+import { useGlobalState } from "../../GlobalProvider/GlobalProvider";
+import { ButtonExit, LinkSignIn, Text } from "./AuthButton.styled";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRightToBracket, faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+
+function AuthButton() {
+  const { user } = useUser();
+  const { setModalToggle } = useGlobalState();
+
+  const ButtonSignOut = (
+    <ButtonExit type="button" onClick={() => setModalToggle("logout")}>
+      <FontAwesomeIcon icon={faArrowRightFromBracket} />
+      <Text>Exit</Text>
+    </ButtonExit>
+  );
+
+  const ButtonSignIn = (
+    <LinkSignIn href="/login">
+      <FontAwesomeIcon icon={faArrowRightToBracket} />
+      <Text>Sign In</Text>
+    </LinkSignIn>
+  );
+
+  return <>{user ? ButtonSignOut : ButtonSignIn}</>;
+}
+
+export default AuthButton;
