82e58d69a2409d47396748879a7bd7f5084cf291
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-25 21:39:42 +0300

    Update Balance.tsx

--- src/components/Balance/Balance.tsx
+++ src/components/Balance/Balance.tsx
@@ -1,45 +1,13 @@
 "use client";
 
 import { useQuery } from "@tanstack/react-query";
+import { useUser } from "@/src/hooks/useUser";
 import { Wrapper, CurrencySign, Text, Title } from "./Balance.styled";
-import { useSession } from "next-auth/react";
-import { BALANCE, BASE_URL } from "@/src/constants/apiPath";
-import { fetcher } from "@/src/helpers/fetcher";
-
-export const getBalance = async (
-  authToken: string | undefined
-): Promise<any> => {
-  const options: RequestInit = {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${authToken}`,
-    },
-    cache: "no-store",
-  };
-
-  try {
-    const data = await fetcher<any>(`${BASE_URL}${BALANCE}`, options);
-
-    // console.log("data:", data);
-    return data;
-  } catch (error) {
-    console.log("Error:", (error as Error).message);
-    throw error;
-  }
-};
+import { getBalance } from "@/src/helpers/getBalance";
+import { useUserBalance } from "@/src/hooks/useUserBalance";
 
 export default function Balance() {
-  const session = useSession();
-  const userToken = session.data?.user.token;
-
-  const { data, isError, error, isFetching } = useQuery({
-    queryKey: ["Balance"],
-    queryFn: () => getBalance(userToken), // ИЛИ authToken
-    staleTime: Infinity,
-    refetchOnWindowFocus: false,
-    retry: 0,
-    enabled: !!userToken,
-  });
+  const { data } = useUserBalance();
 
   // if (isFetching) {
   //   return <h1>Loading</h1>
@@ -49,7 +17,7 @@ export default function Balance() {
     <Wrapper>
       <Title>Your balance</Title>
       <CurrencySign>₴ </CurrencySign>
-      <Text>{data?.userBalance}</Text>
+      <Text>{data}</Text>
     </Wrapper>
   );
 }
