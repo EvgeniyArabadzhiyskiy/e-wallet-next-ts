110cc94773a83a3f673b76ecb2b00f4686b55c3b
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-25 21:39:02 +0300

    Create ModalLogout.styled.tsx

---
+++ src/components/ModalLogOut/ModalLogout.styled.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import styled from "styled-components";
+
+export const Border = styled.div`
+  border: 10px solid rgba(255, 255, 255, 0.2);
+`;
+
+export const Modal = styled.div`
+  min-width: 300px;
+  background: rgba(0, 0, 0, 0.7);
+  padding: 30px 10px;
+`;
+
+export const Title = styled.h2`
+  font-weight: ${(p) => p.theme.fontWeights.bold};
+  font-size: ${(p) => p.theme.fontSizes.ml};
+  color: ${(p) => p.theme.colors.white};
+  line-height: 1.2;
+  text-align: center;
+
+  margin-bottom: 50px;
+  padding: 0 20px;
+`;
