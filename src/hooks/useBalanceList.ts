0eda28af058f8449f38c897343077a40a9348eb0
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-26 22:09:27 +0300

    Update useBalanceList.ts

--- src/hooks/useBalanceList.ts
+++ src/hooks/useBalanceList.ts
@@ -4,13 +4,13 @@ import { useUserBalance } from "./useUserBalance";
 import { ITransaction } from "../types/transactions";
 
 export const useBalanceList = (transactions: ITransaction[]) => {
+  const { data: totalBalance } = useUserBalance();
 
-  const { data: totalBalance = 0 } = useUserBalance();
+  const balances = useMemo(() => {
+    if (totalBalance) {
+      return balanceAfterTransaction(transactions, totalBalance);
+    }
+  }, [totalBalance, transactions]);
 
-  const balances = useMemo(
-    () => balanceAfterTransaction(transactions, totalBalance),
-    [totalBalance, transactions]
-  );
-
-  return balances;
+  return balances || [];
 };
