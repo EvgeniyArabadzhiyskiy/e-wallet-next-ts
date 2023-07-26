be9ab1d1ec9e6dcd82b0b9be41002fa98c683ff4
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-26 22:09:40 +0300

    Update DashBoardLayout.tsx

--- src/components/DashBoardLayout/DashBoardLayout.tsx
+++ src/components/DashBoardLayout/DashBoardLayout.tsx
@@ -1,27 +1,51 @@
-
-
+import getQueryClient from "@/src/lib/getQueryClient";
 import Balance from "../Balance/Balance";
 import Container from "../Container/Container";
 import Header from "../Header/Header";
 import Navigation from "../Navigation/Navigation";
 import stl from "./DashBoardLayout.module.scss";
+import { getBalance } from "@/src/helpers/getBalance";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/src/lib/auth";
+import { apiWallet } from "@/src/apiWallet/apiWallet";
+import { Hydrate, dehydrate } from "@tanstack/react-query";
 
-export default function DashBoardLayout({ children }: { children: React.ReactNode }) {
+export default async function DashBoardLayout({ children }: { children: React.ReactNode }) {
+  const session = await getServerSession(authOptions);
+  const authToken = session?.user.token;
+
+  const queryClient = getQueryClient();
+
+  if (authToken) {
+    await queryClient.prefetchQuery(["Balance"], () => getBalance(authToken));
+
+    await queryClient.prefetchInfiniteQuery({
+      queryKey: ["TransactionsList"],
+      queryFn: ({ pageParam = 1 }) =>
+        apiWallet.getAllTransactions(authToken, pageParam),
+    });
   
+  }
+
+  const dehydratedState = dehydrate(queryClient);
+
+  // const balance = queryClient.getQueriesData<any>(["Balance"]);
+  // console.log("TransactionList  Balance:+++++++++++++++++++++++++++++++++++", balance);
+
   return (
-    <> 
+    <Hydrate state={dehydratedState}>
       <div className={stl.section}>
         <Container>
           <div className={stl.wrapper}>
-             <div className={stl.sidebar}>
+            <div className={stl.sidebar}>
               <Navigation />
-              <Balance />
-             </div>
-            
-            {children}
+              {session && <Balance />}
             </div>
+
+            {children}
+          </div>
         </Container>
       </div>
-    </>
+    </Hydrate>
   );
 }
