2e21c0c535a02c735ab073802b5c177ddf8d806a
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-26 22:09:00 +0300

    Create TableItemDesktop.tsx

---
+++ src/components/HomeTab/TableItemDesktop/TableItemDesktop.tsx
@@ -0,0 +1,59 @@
+import {
+  CategoryName,
+  SumText,
+} from "../HomeTableDesctop/HomeTableDesctop.styled";
+import { StyledItem } from "./TableItemDesktop.styled";
+
+interface IProps {
+  operationDate: any;
+  typeOperation: any;
+  category: any;
+  comment: any;
+  amount: any;
+  itemBalance: any;
+}
+
+const TableItemDesktop = ({
+  operationDate,
+  typeOperation,
+  category,
+  comment,
+  amount,
+  itemBalance,
+  // isLongAmount, isLongBalance,
+  // onContextMenu, isOpenMenu, isDelete, onEdit, onClearId, onDelete
+}: 
+IProps) => {
+  const bodyTransaction = (
+    <>
+      <CategoryName>
+        {/* <SettingBtn onClick={onContextMenu} aria-label="settings"> */}
+        I
+        {/* </SettingBtn> */}
+      </CategoryName>
+
+      <CategoryName>{operationDate}</CategoryName>
+      <CategoryName>{typeOperation}</CategoryName>
+      <CategoryName>{category}</CategoryName>
+      <CategoryName>{comment}</CategoryName>
+
+      <SumText $typeColor={typeOperation}>{amount}</SumText>
+
+      <CategoryName>{itemBalance}</CategoryName>
+
+      {/* <ContextMenu
+        isDelete={isDelete}
+        isOpenMenu={isOpenMenu}
+        
+        onEdit={onEdit}
+        onDelete={onDelete}
+        onClearId={onClearId}
+        onContextMenu={onContextMenu}
+      /> */}
+    </>
+  );
+
+  return <StyledItem>{bodyTransaction}</StyledItem>;
+};
+
+export default TableItemDesktop;
