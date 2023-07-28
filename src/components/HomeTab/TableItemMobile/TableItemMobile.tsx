8a612c2747111111c2dd1e985e6a2bb6c7216804
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-28 21:39:51 +0300

    Update TableItemMobile.tsx

--- src/components/HomeTab/TableItemMobile/TableItemMobile.tsx
+++ src/components/HomeTab/TableItemMobile/TableItemMobile.tsx
@@ -1,50 +1,59 @@
-import { SumText } from "../HomeTableDesctop/HomeTableDesctop.styled";
-import { StyledList } from "../HomeTableMobile/HomeTabelMobile.styled";
+import { StyledList, SumText } from "./TableItemMobile.styled";
 
 interface IProps {
-    operationDate: any;
-    typeOperation: any;
-    category: any;
-    comment: any;
-    amount: any;
-    itemBalance: any;
-  }
+  operationDate: any;
+  typeOperation: any;
+  category: any;
+  comment: any;
+  amount: any;
+  itemBalance: any;
+}
 
+function TableItemMobile({
+  operationDate,
+  typeOperation,
+  category,
+  comment,
+  amount,
+  itemBalance,
+}: // isLongAmount, isLongBalance,
+// onContextMenu, isOpenMenu, isDelete, onEdit, onClearId, onDelete
+IProps) {
+  // const bodyTransaction =
+  // <span onClick={() => sendMsg(isLongBalance, itemBalance)}>
+  //     {isLongBalance ? "Click" : itemBalance}
+  // </span>
 
-function TableItemMobile  ({
-    operationDate,
-    typeOperation,
-    category,
-    comment,
-    amount,
-    itemBalance,
-    // isLongAmount, isLongBalance,
-    // onContextMenu, isOpenMenu, isDelete, onEdit, onClearId, onDelete
-  }: 
-  IProps)  {
-    const bodyTransaction = 
-    <span onClick={() => sendMsg(isLongBalance, itemBalance)}>
-        {isLongBalance ? "Click" : itemBalance}
-    </span>
-          
-    const content = <li>Balance {bodyTransaction}</li> ;
+  const content = <li>Balance {itemBalance}</li>;
 
-    return (
-        <StyledList borders={typeOperation}>
-        <li><SettingBtn onClick={onContextMenu} aria-label="settings"><GoSettings /></SettingBtn></li>
-        <li>Date <span>{operationDate}</span></li>
-        <li>Type <span>{typeOperation}</span></li>
-        <li>Category <span>{category}</span></li>
-        <li>Comment <span>{comment}</span></li>
-        
-        <li> Sum
-            <SumText textColor={typeOperation} onClick={() => sendMsg(isLongAmount, amount)}>
-              {isLongAmount ? "Click" : amount}
-            </SumText>
-        </li>
-        {content}
-    
-        {/* <ContextMenu
+  return (
+    <StyledList borders={typeOperation}>
+      {/* <li><SettingBtn onClick={onContextMenu} aria-label="settings"><GoSettings /></SettingBtn></li> */}
+      <li><button  aria-label="settings">I</button></li>
+      <li>
+        Date <span>{operationDate}</span>
+      </li>
+      <li>
+        Type <span>{typeOperation}</span>
+      </li>
+      <li>
+        Category <span>{category}</span>
+      </li>
+      <li>
+        Comment <span>{comment}</span>
+      </li>
+
+      <li>
+        Sum
+        <SumText
+          $typeColor={typeOperation}
+        >
+          {amount}
+        </SumText>
+      </li>
+      {content}
+
+      {/* <ContextMenu
             isDelete={isDelete}
             isOpenMenu={isOpenMenu}
             
@@ -53,11 +62,8 @@ function TableItemMobile  ({
             onClearId={onClearId}
             onContextMenu={onContextMenu}
         /> */}
-    
-        </StyledList>
-    );
+    </StyledList>
+  );
+}
 
-
-  };
-  
-  export default TableItemMobile;
+export default TableItemMobile;
