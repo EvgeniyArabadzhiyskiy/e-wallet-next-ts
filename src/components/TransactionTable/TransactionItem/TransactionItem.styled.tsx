6caa68936df673d81b64878aafca23ce554b4eba
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-30 22:09:53 +0300

    Create TransactionItem.styled.tsx

---
+++ src/components/TransactionTable/TransactionItem/TransactionItem.styled.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import styled, { css } from "styled-components";
+import { ItemTextPosition } from "../TransactionTable.styled";
+
+export const CommonStyle = css`
+  font-size: ${(p) => p.theme.fontSizes.s};
+  line-height: 1.5;
+
+  display: grid;
+  grid-template-columns: 5% 15% 15% 20% 20% 10% 15%;
+  align-items: center;
+`;
+
+
+export const StyledItem = styled.li<{ $borders: string }>`
+  position: relative;
+  overflow: hidden;
+
+  margin-bottom: 8px;
+  border-radius: 10px;
+  background-color: ${(p) => p.theme.colors.primaryBg};
+
+  border-left: 5px solid
+    ${(p) => (p.$borders === "income" ? "#24CCA7" : "#FF6596")};
+
+  @media (min-width: 768px) {
+    ${CommonStyle};
+
+    height: 52px;
+    margin-bottom: 0;
+    border-radius: inherit;
+    background-color: inherit;
+
+    border-left: none;
+
+    border-bottom: 1px solid #dcdcdf;
+  }
+`;
+
+
+
+
+
+export const SumColorText = styled.p<{ $typeColor: string }>`
+  color: ${(p) => (p.$typeColor === "+" ? "#24CCA7" : "#FF6596")};
+
+  display: flex;
+  justify-content: space-between;
+  padding: 12px 20px;
+
+  @media (min-width: 768px) {
+    ${ItemTextPosition};
+    display: block;
+    padding: 0;
+  }
+`;
+
+
+export const CategoryName = styled.span`
+  font-weight: 700;
+
+  @media (min-width: 768px) {
+    display: none;
+  }
+`;
