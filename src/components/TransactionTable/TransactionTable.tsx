c870a380baac8cdfbab4d73d6c6421198db0415b
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-31 21:01:21 +0300

    Update TransactionTable.tsx

--- src/components/TransactionTable/TransactionTable.tsx
+++ src/components/TransactionTable/TransactionTable.tsx
@@ -3,8 +3,12 @@
 import { useState } from "react"
 import { useBalanceList } from "@/src/hooks/useBalanceList";
 import { useLazyTransactions } from "@/src/hooks/useLazyTransactions";
+
 import TransactionItem from "./TransactionItem/TransactionItem";
+import ButtonAddTransactions from "../Buttons/ButtonAddTransactions/ButtonAddTransactions";
 import { Category, Table, TableBody, TableHeader } from "./TransactionTable.styled";
+import ModalBox from "../ModalWindow/ModalBox";
+import FlipCard from "../FlipCard/FlipCard";
 
 // interface IProps {
 //   balances: number[];
@@ -16,41 +20,56 @@ import { Category, Table, TableBody, TableHeader } from "./TransactionTable.styl
 function TransactionTable() {
   const { data: allTransactions = [], listElem, observerElem  } = useLazyTransactions();
   const balanceList = useBalanceList(allTransactions);
-  const [deleteId, setDeleteId] = useState<string[]>([]);
-  console.log("TransactionItem  deleteId:", deleteId);
+  const [deleteId, setDeleteId] = useState<string[]>([]);  // под вопросом 
+
+  const [editId, setEditId] = useState<string>("");
+  // console.log("TransactionTable  editId:", editId);
+  const [modalKey, setModalKey] = useState<"ADD" | "EDIT">("ADD")
+  // console.log("TransactionTable  modalKey:", modalKey);
 
   return (
-    <Table>
-      <TableHeader>
-        <Category>I</Category>
-        <Category>Date</Category>
-        <Category>Type</Category>
-        <Category>Category</Category>
-        <Category>Comment</Category>
-        <Category>Sum</Category>
-        <Category>Balance</Category>
-      </TableHeader>
+    <>
+      <Table>
+        <TableHeader>
+          <Category>I</Category>
+          <Category>Date</Category>
+          <Category>Type</Category>
+          <Category>Category</Category>
+          <Category>Comment</Category>
+          <Category>Sum</Category>
+          <Category>Balance</Category>
+        </TableHeader>
 
-      {allTransactions.length > 0 && (
-        <TableBody ref={listElem}>
-          {allTransactions.map((transaction, idx) => {
-            const balance = balanceList[idx];
+        {allTransactions.length > 0 && (
+          <TableBody ref={listElem}>
+            {allTransactions.map((transaction, idx) => {
+              const balance = balanceList[idx];
 
-            return (
-              <TransactionItem
-                deleteId={deleteId}
-                setDeleteId={setDeleteId}
-                key={transaction._id}
-                balance={balance}
-                transaction={transaction}
-              />
-            );
-          })}
+              return (
+                <TransactionItem
+                  deleteId={deleteId}
+                  setDeleteId={setDeleteId}
 
-          <div ref={observerElem} style={{ height: 5 }}></div>
-        </TableBody>
-      )}
-    </Table>
+                  setEditId={setEditId}
+                  key={transaction._id}
+                  balance={balance}
+                  transaction={transaction}
+                  setModalKey={setModalKey}
+                />
+              );
+            })}
+
+            <div ref={observerElem} style={{ height: 5 }}></div>
+          </TableBody>
+        )}
+      </Table>
+
+      <ButtonAddTransactions setModalKey={setModalKey} />
+
+      <ModalBox modalName="transaction">
+        <FlipCard modalKey={modalKey} editId={editId} />
+      </ModalBox>
+    </>
   );
 }
 
