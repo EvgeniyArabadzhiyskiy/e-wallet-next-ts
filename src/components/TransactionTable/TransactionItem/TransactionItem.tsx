f9008ccdc3b87a77a467e6ae81e15be94aca5719
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-07-31 21:01:40 +0300

    Update TransactionItem.tsx

--- src/components/TransactionTable/TransactionItem/TransactionItem.tsx
+++ src/components/TransactionTable/TransactionItem/TransactionItem.tsx
@@ -6,29 +6,45 @@ import {
   SumColorText,
 } from "./TransactionItem.styled";
 import { getSymbolType } from "@/src/helpers/getSymbolType";
-import { useRef, useState } from "react";
+import { Dispatch, SetStateAction, useRef, useState } from "react";
 import {
   InfiniteData,
   useMutation,
   useQueryClient,
 } from "@tanstack/react-query";
-import { ITransactions } from "@/src/types/transactions";
+import { ITransactions, RemoveTransaction } from "@/src/types/transactions";
 import { BASE_URL, TRANSACTIONS } from "@/src/constants/apiPath";
 import axios from "axios";
 import { apiWallet } from "@/src/apiWallet/apiWallet";
 import { useUser } from "@/src/hooks/useUser";
 import TransactionMenu from "../../TransactionMenu/TransactionMenu";
+import { fetcher } from "@/src/helpers/fetcher";
+import { useGlobalState } from "../../GlobalProvider/GlobalProvider";
 
 interface IProps {
   transaction: any;
   balance: any;
   deleteId: any;
   setDeleteId: any;
+  setModalKey: Dispatch<SetStateAction<"ADD" | "EDIT">>;
+  setEditId: Dispatch<SetStateAction<string>>;
 }
 
-const deleteTransaction = async (id: any) => {
-  const { data } = await axios.delete(`${BASE_URL}${TRANSACTIONS}/${id}`);
-  //   console.log("SecondPage  data:", data);
+const deleteTransaction = async (id: string, token: string | undefined) => {
+  const options: RequestInit = {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+      Authorization: `Bearer ${token}`,
+    },
+    // body: JSON.stringify(transaction),
+  };
+
+  const data = await fetcher<RemoveTransaction>(
+    `${BASE_URL}${TRANSACTIONS}/${id}`,
+    options
+  );
+
   return data;
 };
 
@@ -37,6 +53,8 @@ function TransactionItem({
   balance,
   deleteId,
   setDeleteId,
+  setModalKey,
+  setEditId,
 }: IProps) {
   const { _id, date, typeOperation, category, comment, amount } = transaction;
   const operationDate = moment(new Date(date)).format("DD.MM.YYYY");
@@ -44,84 +62,99 @@ function TransactionItem({
   const queryClient = useQueryClient();
   const { token } = useUser();
   const timeoutId = useRef<NodeJS.Timeout>();
+  const { setModalToggle } = useGlobalState();
 
   const [isOpenMenu, setIsOpenMenu] = useState(false);
   const [isDelete, setIsDelete] = useState(true);
 
-  const { mutate: removeTransaction } = useMutation({
-    mutationFn: deleteTransaction,
+  const { mutate: removeTransaction } = useMutation<
+    RemoveTransaction,
+    Error,
+    string
+  >({
+    mutationFn: (id) => deleteTransaction(id, token),
 
-    onSuccess: async (data: any) => {
-      const { pages } = queryClient.getQueryData<any>(["TransactionsList"]);
-      const lastPageNumber = pages.length;
+    onSuccess: async (data) => {
+      const infiniteData = queryClient.getQueryData<InfiniteData<ITransactions>>([
+        "TransactionsList",
+      ]);
 
-      const { transactions } = await apiWallet.getAllTransactions(
-        token,
-        lastPageNumber
-      );
+      if (!infiniteData) {
+        return
+      }
+
+      const lastPageNumber = infiniteData.pages.length;
+
+      const { transactions } = await apiWallet.getAllTransactions(token, lastPageNumber);
+
       const lastTransaction = transactions.pop();
 
-      const updatedPages = pages.map((page: any) =>
-        page.filter((item: any) => item._id !== data._id)
-      );
+      const updatedPages = infiniteData.pages.map((page) => {
+        const newCache = page.transactions.filter((item) => item._id !== data._id);
 
+        return {
+          ... page,
+          transactions: newCache,
+        }
+      });
+
+      
+      
       const lastPagesIdx = updatedPages.length - 1;
-      updatedPages[lastPagesIdx].push(lastTransaction);
+      lastTransaction && updatedPages[lastPagesIdx].transactions.push(lastTransaction);
 
-      queryClient.setQueryData<InfiniteData<ITransactions>>(
-        ["TransactionsList"],
-        (prev) => {
-          if (!prev) {
-            return undefined;
-          }
 
+      queryClient.setQueryData<InfiniteData<ITransactions>>(["TransactionsList"],(prev) => {
+        if (prev) {
           return {
             ...prev,
             pages: updatedPages,
           };
         }
-      );
+      });
+      
+      queryClient.invalidateQueries(["Balance"])
+      
     },
   });
 
-  const onContextMenu = () => {
-    setIsOpenMenu((prev) => !prev);
-  };
+  // const onContextMenu = () => {
+  //   setIsOpenMenu((prev) => !prev);
+  // };
 
   const onDelete = (id: string) => {
-    // console.log("onDelete  id:", id);
     setIsDelete(false);
 
     timeoutId.current = setTimeout(() => {
-      // removeTransaction(id);
+      removeTransaction(id);
 
-      setDeleteId((prev: any) => [...prev, id]);
-      // dispatch(setDeletedId(id))
-      // dispatch(deleteTransaction())
-    }, 300);
+      setDeleteId((prev: string[]) => [...prev, id]); // под вопросом 
+    }, 100);
   };
 
-  const ddd = (id: any) => {
-    const res = deleteId.filter((removedId: any) => removedId !== id);
-    return res
-  };
-
-  const onClearId = (id: any) => {
+  const onClearId = (id: string) => {
     setIsDelete(true);
 
-
-    setDeleteId(() => ddd(id))
+    setDeleteId((prev: any) => prev.filter((removeID: any) => removeID !== id)); // под вопросом 
 
     console.log("CANCEL");
     clearTimeout(timeoutId.current);
   };
+
+
+  const onEdit = (id: string) => {
+    setModalToggle("transaction")
+    setModalKey("EDIT")
+    setEditId(id)
+    setIsOpenMenu(false)
+  }
 
   return (
     <>
       <StyledItem $borders={typeOperation} key={transaction._id}>
         <Category>
           <CategoryName>O</CategoryName>
-          <button type="button" onClick={onContextMenu}>
+          <button type="button" onClick={() => setIsOpenMenu((prev) => !prev)}>
             O
           </button>
         </Category>
@@ -155,10 +188,11 @@ function TransactionItem({
         <TransactionMenu
           isDelete={isDelete}
           isOpenMenu={isOpenMenu}
-          // onEdit={onEdit}
+          setIsOpenMenu={setIsOpenMenu}
+
+          onEdit={() => onEdit(_id)}
           onDelete={() => onDelete(_id)}
           onClearId={() => onClearId(_id)}
-          onContextMenu={onContextMenu}
         />
       </StyledItem>
     </>
