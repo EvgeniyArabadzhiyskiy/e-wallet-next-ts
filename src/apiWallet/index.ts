5b9835ced22362a1b5ac161b0783cff5d91a703f
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-01 21:21:52 +0300

    Create index.ts

---
+++ src/apiWallet/index.ts
@@ -0,0 +1,13 @@
+import { useLazyTransactions } from "./useLazyTransactions";
+import { useCreateTransaction } from "./useCreateTransaction";
+import { useRemoveTransaction } from "./useRemoveTransaction";
+import { useEditTransaction } from "./useEditTransaction";
+import { useUserBalance } from "./useUserBalance";
+
+export {
+  useLazyTransactions,
+  useCreateTransaction,
+  useRemoveTransaction,
+  useEditTransaction,
+  useUserBalance
+};
