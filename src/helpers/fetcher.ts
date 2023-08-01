0742239cd3a51f5e32247bca475d970566cc2104
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-01 21:21:34 +0300

    Update fetcher.ts

--- src/helpers/fetcher.ts
+++ src/helpers/fetcher.ts
@@ -1,5 +1,7 @@
+import { BASE_URL } from "../constants/apiPath";
+
 export async function fetcher<T>(url: RequestInfo, options?: RequestInit): Promise<T> {
-  const response = await fetch(url, options);
+  const response = await fetch(`${BASE_URL}${url}`, options);
 
   if (!response.ok) {
     throw new Error(`Request failed with status ${response.status}`);
