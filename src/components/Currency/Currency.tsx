cb2dbe17e87f3053d0546835f0159fd510cf4113
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-01 21:21:14 +0300

    Update Currency.tsx

--- src/components/Currency/Currency.tsx
+++ src/components/Currency/Currency.tsx
@@ -2,35 +2,84 @@
 
 import {
   PrivatTableList,
-  // CurrencyList,
   CurrencyBox,
   TextTitle,
   CurrencyVektor,
+  CurrencyList,
 } from "./Currency.styled";
 // import grafSvg from "../../images/currencyVektor.svg";
 import axios from "axios";
 // import { toast } from "react-toastify";
-import { useEffect, useState } from "react";
+import { use, useEffect, useState } from "react";
+import CurrencyListItem from "./CurrencyListItem";
 
 //   import CurrencyListItem from './CurrencyListItem';
 
-
 const getApiMono = async () => {
-  const res = await axios
-    .get("https://api.monobank.ua/bank/currency")
-    .then((res) => {
-      const date = new Date();
-      localStorage.setItem("getMono", JSON.stringify(res.data));
-      localStorage.setItem("date", JSON.stringify(+date));
-      return res;
-    })
-    .catch((error) => error);
-  return res.data;
+  const res = await axios.get("https://api.monobank.ua/bank/currency");
+
+  const currency = res.data.filter((item: any) => {
+    const { currencyCodeA, currencyCodeB } = item;
+    const USDUAH = currencyCodeA === 840 && currencyCodeB === 980;
+    const EURUAH = currencyCodeA === 978 && currencyCodeB === 980;
+
+    if (USDUAH || EURUAH) {
+      return item;
+    }
+  });
+
+  const currentDate = Date.now();
+
+  localStorage.setItem("getMono", JSON.stringify(currency));
+  localStorage.setItem("date", JSON.stringify(currentDate));
+
+  return currency;
+
 };
+
+// const getApiMono = async () => {
+//   const res = await axios
+//     .get('https://api.monobank.ua/bank/currency')
+//     .then(res => {
+//       const date = new Date();
+//       localStorage.setItem('getMono', JSON.stringify(res.data));
+//       localStorage.setItem('date', JSON.stringify(+date));
+//       return res;
+//     })
+//     .catch(error => error);
+//   return res.data;
+// };
 
 const Currency = () => {
   const [arrow, setArrow] = useState(null);
   const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const savedApiDate = localStorage.getItem('date');
+    const savedApiMono = localStorage.getItem("getMono");
+
+    (async () => {
+      if (savedApiMono && savedApiDate) {
+        const parsedApiMono = JSON.parse(savedApiMono);
+        const parsedApiDate = JSON.parse(savedApiDate) as number;
+
+        const currentDate = Date.now();
+
+        const isUpdateApiMono = (currentDate - parsedApiDate) >= 3600000
+
+        if (isUpdateApiMono) {
+          getApiMono()
+        }
+
+        setArrow(parsedApiMono);
+        return
+      }
+      
+      const data = await getApiMono();
+      setArrow(data);
+      
+    })();
+  }, []);
 
   // useEffect(() => {
   //   const getCurrency = async () => {
@@ -44,7 +93,7 @@ const Currency = () => {
 
   //     const date = new Date();
 
-  //     const update = date - parsedApiDate >= 3600000;
+  //     const update = (+date - parsedApiDate) >= 3600000;
 
   //     if (!parsedApiDate) {
   //       localStorage.setItem('date', JSON.stringify(+date));
@@ -66,7 +115,8 @@ const Currency = () => {
   //       const newArrow = data.slice(0, 2);
   //       setArrow(newArrow);
   //     } catch (error) {
-  //       toast.error('Bank request error');
+  //       console.log("getCurrency  error:", error);
+  //       // toast.error('Bank request error');
   //     }
   //   };
 
@@ -82,12 +132,11 @@ const Currency = () => {
         <TextTitle>Sale</TextTitle>
       </PrivatTableList>
 
-      {/* <CurrencyList>
-          {!loading ? <CurrencyListItem arrow={arrow} /> : <Spinner />}
-        </CurrencyList> */}
+      <CurrencyList>
+        {true ? <CurrencyListItem arrow={arrow} /> : <h1>Load Spiner...</h1>}
+      </CurrencyList>
 
       <CurrencyVektor />
-
     </CurrencyBox>
   );
 };
