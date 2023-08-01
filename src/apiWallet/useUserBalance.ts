30aaefb96f35ddb68ac7ae13de340abd23fd9774
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-01 21:21:49 +0300

    Create useUserBalance.ts

---
+++ src/apiWallet/useUserBalance.ts
@@ -0,0 +1,21 @@
+import { useUser } from "../hooks/useUser";
+import { useQuery } from "@tanstack/react-query";
+import { IBalance } from "../types/transactions";
+import { getBalance } from "./balance";
+
+export const useUserBalance = () => {
+  const { token } = useUser();
+
+  const data = useQuery<IBalance, Error, number, string[]>({
+    queryKey: ["Balance"],
+    queryFn: () => getBalance(token), // ИЛИ authToken
+    select: (data) => data.userBalance,
+
+    staleTime: Infinity,
+    refetchOnWindowFocus: false,
+    retry: 0,
+    enabled: !!token,
+  });
+
+  return data;
+};
