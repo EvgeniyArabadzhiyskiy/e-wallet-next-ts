491d0e1607eec64eeedb38d4a5dfbf82808da4c3
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-01 21:20:36 +0300

    Delete apiWallet.ts

--- src/apiWallet/apiWallet.ts
+++
@@ -1,7 +0,0 @@
-import { createTransaction } from "./createTransaction";
-import { getAllTransactions } from "./getAllTransactions";
-
-export const apiWallet = {
-  createTransaction,
-  getAllTransactions,
-};
