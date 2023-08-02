e782e9826de0eab90c8a2563661a8b73ae7d9ec9
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-02 20:07:36 +0300

    Update page.tsx

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -24,7 +24,7 @@ export default function PageTransactions() {
     <>
       {/* <Hydrate state={dehydratedState}> */}
      
-      <TransactionTable />
+      {/* <TransactionTable /> */}
 
       {/* <ModalBox modalName="transaction">
         <FlipCard />
