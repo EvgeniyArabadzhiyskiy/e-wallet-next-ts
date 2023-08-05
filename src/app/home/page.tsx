b05d2341a57587c3ae3d8bafd8c6195629241f0a
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-05 21:55:50 +0300

    Update page.tsx

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,47 @@
+"use client"
+
+import Image from "next/image";
 import Link from "next/link";
+import EWalletBgTab from '@/public/images/tablet.webp';
+import styled from "styled-components";
+
+const Baner = styled.div`
+  position: relative;
+  width: 600px;
+  height: 600px;
+  border-radius: 50%;
+  background: url(${EWalletBgTab.src});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: contain;
+`
 
 export default function HomePage() {
   return (
-    <>
-      <Link href="/">HOME</Link>
-      <div style={{ height: 250, backgroundColor: "blue" }}></div>
-    </>
+    <div
+      style={{
+        // position: "relative",
+        width: 600,
+        height: 600,
+        borderRadius: "50%",
+        backgroundColor: "#212937",
+
+        // overflow: "hidden",
+      }}
+    >
+
+    
+    <Baner
+    
+    >
+      {/* <div style={{ height: 250,  backgroundColor: "blue" }}>BANER</div> */}
+      {/* <Image
+        src="/images/tablet.webp"
+        alt="baner"
+        fill={true}
+        style={{ objectFit: "cover" }}
+      /> */}
+    </Baner>
+    </div>
   );
 }
