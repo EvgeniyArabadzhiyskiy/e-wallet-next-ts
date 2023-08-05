b7e0fe8e063c07cad526177f330fa18878a60a2d
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-05 21:56:04 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,19 @@
-import Link from "next/link";
+import HeroSection from "../components/HeroSection/HeroSection";
 
-export default async function Home() {
-  return (
-    <>
-      <div>
-      <div>
-          <Link href="/home">HOME</Link>{" "}
-          <br />
-          <div style={{marginTop: 20}}></div>
-          <Link  href="/about">About</Link>{" "} 
-          <br />
-          <div style={{marginTop: 20}}></div>
-          <Link href="/currency">HYDRATE</Link>{" "}
-          <br />
-          <div style={{marginTop: 20}}></div>
-          <Link  href="/login">LOGIN</Link>
+export default function Home() {
+  return <HeroSection />;
+}
+
+{
+  /* <div>
+        <div>
+          <Link href="/home">HOME</Link> <br />
+          <div style={{ marginTop: 20 }}></div>
+          <Link href="/about">About</Link> <br />
+          <div style={{ marginTop: 20 }}></div>
+          <Link href="/currency">HYDRATE</Link> <br />
+          <div style={{ marginTop: 20 }}></div>
+          <Link href="/login">LOGIN</Link>
         </div>
-        <pre>
-          {/* <Link href="/home/product">PRODUCT</Link> */}
-        </pre>
-      </div>
-
-    
-    </>
-  );
+      </div> */
 }
