d05205e80914ff396f8463c02b7aece4b1550f53
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-05 21:56:01 +0300

    Update LinkButton.styled.tsx

--- src/components/Buttons/LinkButton/LinkButton.styled.tsx
+++ src/components/Buttons/LinkButton/LinkButton.styled.tsx
@@ -13,13 +13,13 @@ export const StyledLink = styled(Link)`
   justify-content: center;
 
   border: 1px solid #15275160;
-  color: ${(p) => p.theme.colors.secondaryBtn};
-  background-color: ${(p) => p.theme.colors.primaryBtn};
+  color: ${(p) => p.theme.colors.white};
+  background-color: ${(p) => p.theme.colors.primaryBgBtn};
   border-bottom: 5px solid #15275160;
   transition: all 150ms linear;
 
-  :hover,
-  :focus {
+  &:hover,
+  &:focus {
     border-color: #31a1cd;
     border-bottom: 1px solid #3187cd;
     color: #3187cd;
