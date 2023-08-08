8eb54e851cd56fb6401de8fb3283c682b88a19de
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-08 22:56:59 +0300

    Update login.ts

--- src/apiWallet/user/login.ts
+++ src/apiWallet/user/login.ts
@@ -1,7 +1,7 @@
 import { USER_LOGIN } from "@/src/constants/apiPath";
 import { fetcher } from "@/src/helpers/fetcher";
 import { ICredentials } from "@/src/types/registerValues";
-import { IAuthCredentials, User } from "@/src/types/user";
+import { IAuthCredentials, CurrentUser } from "@/src/types/user";
 
 export const login = async (credentials: IAuthCredentials) => {
   const options: RequestInit = {
@@ -12,7 +12,40 @@ export const login = async (credentials: IAuthCredentials) => {
     body: JSON.stringify(credentials),
   };
 
-  const data = await fetcher<User>(`${USER_LOGIN}`, options);
+  const data = await fetcher<CurrentUser>(`${USER_LOGIN}`, options);
 
   return data;
 };
+
+
+// callbacks: {
+//   async jwt({ token, user }) {
+//     // console.log("First+++++++++++++++++++++++++++++");
+
+//     // console.log("user:", user);
+//     // console.log("token>>>>>>>>>>>>>>>>>>>>>>>>>>>", token);
+//     // console.log("user========================",user);
+//     // return { ...token, ...user };
+
+//     if (user) {
+//       const result = {
+//         ...token,
+//         token: user.token,
+//         user: user.user,
+//       };
+//       // console.log("result***********фффффффффф", result);
+//       return result;
+//     } else {
+//       // console.log("jwt$$$$$$$$$$$$$$$", token);
+//       return token;
+//     }
+//   },
+//   async session({ session, token }) {
+//     const currentUser = {
+//       user: token.user,
+//       token: token.token,
+//     } as CurrentUser;
+
+//     return { ...session, ...currentUser };
+//   },
+// },
