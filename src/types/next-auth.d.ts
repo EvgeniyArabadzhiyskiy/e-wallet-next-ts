bfef60cbcd4d2a52db18e0a05403bffa20c983ea
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-08 22:57:12 +0300

    Update next-auth.d.ts

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,19 +1,60 @@
+// import NextAuth from "next-auth";
+// import { User } from "./user";
+
+// declare module "next-auth" {
+//   interface Session {
+//     user: User;
+//     // user?: User;
+//   }
+
+//   // interface User {
+//   //   token: string;
+
+//   //   user: {
+//   //     email: string;
+//   //     firstName: string;
+//   //     balance: number;
+//   //   };
+//   // }
+// }
+
 import NextAuth from "next-auth";
-import { User } from "./user";
+// import { User } from "./user";
 
 declare module "next-auth" {
   interface Session {
-    user: User;
     // user?: User;
+
+    user?: {
+      token: string;
+      email: string;
+      firstName: string;
+      balance: number;
+    };
+
+    // token?: string;
+    // user?: {
+    //   email: string;
+    //   firstName: string;
+    //   balance: number;
+    // };
   }
 
-  // interface User {
-  //   token: string;
+  interface User {
+    token: string;
 
-  //   user: {
-  //     email: string;
-  //     firstName: string;
-  //     balance: number;
-  //   };
-  // }
+    user: {
+      email: string;
+      firstName: string;
+      balance: number;
+    };
+  }
 }
+
+//=================================
+// token?: string;
+// currentUser?: {
+//   email: string;
+//   firstName: string;
+//   balance: number;
+// };
