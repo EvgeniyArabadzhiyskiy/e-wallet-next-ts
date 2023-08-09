2b492463d0fc364cc3b8d795791af1f44f9764b1
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-09 21:53:53 +0300

    Update page.tsx

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import EWalletBgTab from '@/public/images/tablet.webp';
 import styled from "styled-components";
+import AboutComp from "@/src/components/AboutComp";
 
 const Baner = styled.div`
   position: relative;
@@ -30,7 +31,7 @@ export default function HomePage() {
       }}
     >
 
-    
+    <AboutComp />
     <Baner
     
     >
