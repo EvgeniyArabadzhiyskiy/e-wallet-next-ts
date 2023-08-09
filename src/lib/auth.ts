1fe25f39a5f13879d5e23c589883e309256ad6b9
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-09 14:38:45 +0300

    Update auth.ts

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,10 @@
 import type { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+
 import { login } from "../apiWallet/user";
 import { CurrentUser, IAuthCredentials } from "../types/user";
-// import { User } from "next-auth";
 
 export const authOptions: NextAuthOptions = {
-  // secret: process.env.NEXTAUTH_SECRET,
-
   providers: [
     CredentialsProvider({
       name: "Sign in",
@@ -33,62 +31,22 @@ export const authOptions: NextAuthOptions = {
   ],
 
   callbacks: {
-    async jwt({ token, user, account }) {
-      // console.log("user:", user);
-      // console.log("token:", token);
-      // console.log("user========================",user);
-      // return { ...token, ...user };
-
+    async jwt({ token, user }) {
       if (user) {
-        const result = {
-          ...token,
-          token: user.token,
-          user: user.user,
-        };
-        // console.log("result***********фффффффффф", result);
-        return result;
-      } else {
-        return token;
+        token.token = user.token;
+        token.user = user.user;
       }
+
+      return token;
     },
+
     async session({ session, token }) {
-      // const currentUser = {
-      //   user: token.user,
-      //   token: token.token,
-      // } as CurrentUser;
-
-      // return { ...session, ...currentUser };
-      //=================================================
-
-      // const { iat, exp, jti, ...rest } = token as any;
-
-      // session.user = rest;
-      // return session;
-
-      // return {
-      //   ...session,
-
-      //   user: {
-      //     ...session.user,
-      //     token: token.token,
-      //     user: token.user,
-      //   },
-      // };
-
-      
-
       const currentUser = {
-        user: token.user,
         token: token.token,
+        user: token.user,
       } as CurrentUser;
-   
-      return {
-        ...session,
-        user: {
-          ...currentUser.user,
-          token: currentUser.token,
-        },
-      };
+
+      return { ...session, ...currentUser };
     },
   },
 
@@ -99,6 +57,5 @@ export const authOptions: NextAuthOptions = {
   pages: {
     signIn: "/login",
     error: "/login",
-    // newUser: 'register'
   },
 };
