b8033f6f40e8fc433b9fd601ec4f9247c4c4aeee
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-09 14:38:42 +0300

    Update useUser.ts

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -3,7 +3,7 @@ import { useSession } from "next-auth/react";
 export const useUser = () => {
   const { data, status, update } = useSession();
 
-  const token = data?.user?.token;
+  const token = data?.token;
   const user = data?.user;
   const isLoading = status === "loading";
 
