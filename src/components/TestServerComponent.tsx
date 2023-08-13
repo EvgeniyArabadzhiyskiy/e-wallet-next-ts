2e86b386ae72545ccc9b3b6665fd560de0cca141
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-13 19:09:27 +0300

    Create TestServerComponent.tsx

---
+++ src/components/TestServerComponent.tsx
@@ -0,0 +1,13 @@
+import { PageTitle } from "../app/home/statistic/_pageComponent/PageTitle.styled";
+
+const TestServerComponent = () => {
+  console.log("I am Server");
+
+  return (
+    <>
+      <PageTitle>Server Component</PageTitle>
+    </>
+  );
+};
+
+export default TestServerComponent;
