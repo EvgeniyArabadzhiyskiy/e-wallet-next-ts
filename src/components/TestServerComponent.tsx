169bbd46c0ca783d53ee27150d067dec171971bb
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-13 19:18:01 +0300

    Update TestServerComponent.tsx

--- src/components/TestServerComponent.tsx
+++ src/components/TestServerComponent.tsx
@@ -1,11 +1,11 @@
-import { PageTitle } from "../app/home/statistic/_pageComponent/PageTitle.styled";
+
 
 const TestServerComponent = () => {
   console.log("I am Server");
 
   return (
     <>
-      <PageTitle>Server Component</PageTitle>
+      <h1>Server Component</h1>
     </>
   );
 };
