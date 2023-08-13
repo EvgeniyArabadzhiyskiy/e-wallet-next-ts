1df668a11de06282255c1af6fa75455c7cc08688
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-13 19:09:19 +0300

    Update getQueryString.ts

--- src/helpers/getQueryString.ts
+++ src/helpers/getQueryString.ts
@@ -1,4 +1,6 @@
-export const getQueryString = ({month, year}: any) => {
+import { IStatPeriod } from "../types/statistics"
+
+export const getQueryString = ({month, year}: IStatPeriod) => {
     if (month && year)  return `/transactions/statistics?month=${month}&year=${year}`
     if (year)  return `/transactions/statistics?year=${year}`
     if (month)  return `/transactions/statistics?month=${month}`
