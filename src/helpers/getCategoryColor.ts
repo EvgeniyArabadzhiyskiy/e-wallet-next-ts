379689a79a3ae1697e79e7d586056dcf965a9dea
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-13 19:09:15 +0300

    Update getCategoryColor.ts

--- src/helpers/getCategoryColor.ts
+++ src/helpers/getCategoryColor.ts
@@ -1,25 +1,24 @@
-
-
+import { IColor } from "../types/statistics";
 
 // type KeyOfString<T> = Extract<keyof T, string>;
 
-interface IColor {
-    [key: string]: string
-}
+// interface IColor {
+//     [key: string]: string
+// }
 
 const categoryList: IColor = {
-    Food: '#f6a626',
-    Main: '#f4ee2b',
-    House: '#652ed3',
-    Auto: '#4bc0c0',
-    Children: '#36a3eb',
-    Development: '#ff6385',
-    Education: '#342ee9',
-    'Other expenses': '#00ad31',
-  };
-  
-  export const getCategoryColor = (value: keyof IColor) => {
-    const color = categoryList[value]
+  Food: "#f6a626",
+  Main: "#f4ee2b",
+  House: "#652ed3",
+  Auto: "#4bc0c0",
+  Children: "#36a3eb",
+  Development: "#ff6385",
+  Education: "#342ee9",
+  "Other expenses": "#00ad31",
+};
 
-    return color
-  };
+export const getCategoryColor = (value: keyof IColor) => {
+  const color = categoryList[value];
+
+  return color;
+};
