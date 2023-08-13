5041052447771394fa0a0799603306a51d5d8b2a
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-13 19:09:35 +0300

    Create getStatistics.ts

---
+++ src/apiWallet/statistic/getStatistics.ts
@@ -0,0 +1,20 @@
+import { BASE_URL } from "@/src/constants/apiPath";
+import { fetcher } from "@/src/helpers/fetcher";
+import { getQueryString } from "@/src/helpers/getQueryString";
+import { IStatPeriod, IStatistic } from "@/src/types/statistics";
+
+export const getStatistics = async (token: string | undefined, { month, year }: IStatPeriod ) => {
+  const options: RequestInit = {
+    method: "GET",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    // cache: "no-store",
+  };
+
+  const data = await fetcher<IStatistic[]>(`${getQueryString({ month, year })}`, options);
+
+  // console.log("data:", data);
+  return data
+    
+  };
