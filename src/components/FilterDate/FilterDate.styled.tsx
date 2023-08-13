cf7f33ae25efc2cb5e5674b0072e025d03d46d5a
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-13 19:09:02 +0300

    Update FilterDate.styled.tsx

--- src/components/FilterDate/FilterDate.styled.tsx
+++ src/components/FilterDate/FilterDate.styled.tsx
@@ -2,43 +2,16 @@
 
 import styled from "styled-components";
 
-export const InputWrapper = styled.div`
-  position: relative;
-  width: 280px;
-  margin: 0 auto;
-  margin-bottom: 20px;
-
-  @media screen and (min-width: 768px) {
-    width: 160px;
-    margin-bottom: 0px;
-  }
-`;
-
-export const Filter = styled.input`
-  padding-left: 20px;
-  font-size: 16px;
-  height: 50px;
-  width: 100%;
+export const StyledFilters = styled.div`
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 1.5;
+  padding: 0;
   border-radius: 30px;
-  outline: none;
-  border: none;
-  border: 1px solid black;
-  transition: border 250ms linear;
 
-  &:hover {
-    border: 1px solid #24cca7;
+  @media ${(p) => p.theme.media.medium} {
+    padding: 0 0 15px 0;
+    display: flex;
+    justify-content: space-between;
   }
-
-  &:focus {
-    border: none;
-    outline: 1px solid #24cca7;
-  }
-`;
-
-export const ClearButton = styled.button`
-  position: absolute;
-  top: 16px;
-  right: 25px;
-  background-color: transparent;
-  cursor: pointer;
 `;
