9d89d8d63b93c70f71f3427acc4d4ce17d0e1d55
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-13 19:08:19 +0300

    Create useStatistic.ts

---
+++ src/apiWallet/useStatistic.ts
@@ -0,0 +1,50 @@
+import { useMemo, useState } from "react";
+import { useUser } from "../hooks/useUser";
+import { useQuery } from "@tanstack/react-query";
+import { getStatistics } from "./statistic";
+
+export const useStatistic = () => {
+  const { token } = useUser();
+  const [year, setYear] = useState("");
+  const [month, setMonth] = useState("");
+
+  const queryData = useQuery({
+    queryKey: ["Statistics", { month, year }],
+    queryFn: () => getStatistics(token, { month, year }),
+    staleTime: Infinity,
+    refetchOnWindowFocus: false,
+    retry: 0,
+    enabled: !!token,
+  });
+
+  const { data = [] } = queryData;
+
+  const expensesData = useMemo(
+    () => data.filter(({ type }) => type === "expense"),
+    [data]
+  );
+
+  const expensesTotal = useMemo(
+    () => expensesData.reduce((acc, item) => acc + item.totalSum, 0),
+    [expensesData]
+  );
+
+  const incomeTotal = useMemo(() => data
+    .filter(({ type }) => type === "income")
+    .reduce((acc, item) => acc + item.totalSum, 0),
+    [data]
+  );
+
+  const result = {
+    ...queryData,
+    year,
+    setYear,
+    month,
+    setMonth,
+    expensesData,
+    expensesTotal,
+    incomeTotal,
+  };
+
+  return result;
+};
