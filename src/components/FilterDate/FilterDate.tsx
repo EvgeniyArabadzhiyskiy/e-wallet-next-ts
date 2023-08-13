f81f59f22a56807c5bc478654d098ced29479986
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-13 19:08:42 +0300

    Update FilterDate.tsx

--- src/components/FilterDate/FilterDate.tsx
+++ src/components/FilterDate/FilterDate.tsx
@@ -1,65 +1,17 @@
-import moment from "moment";
-import Datetime from 'react-datetime';
-import { ClearButton, Filter, InputWrapper } from "./FilterDate.styled";
+import DateInput from "./DateInput/DateInput";
+import { StyledFilters } from "./FilterDate.styled";
 
 interface IProps {
-  setMonth?: any;
-  setYear?: any;
-  value?: string;
+  setMonth: any;
+  setYear: any;
 }
 
-function FilterDate({ setMonth, setYear, value = "Month" }: IProps) {
-  const changeDate = (e: any) => {
-    const currentYear: any = moment(e).format("YYYY");
-    const currentMonth = new Date(e).getMonth();
-
-    if (!isNaN(currentYear) && currentYear) {
-      setYear && setYear(currentYear);
-    }
-
-    if (!isNaN(currentMonth) ?? currentMonth) {
-      setMonth && setMonth(String(currentMonth));
-    }
-  };
-
-  const renderView = (mode: any, renderDefault: any) => {
-    if (mode === "months") {
-      return <div className="style-month">{renderDefault()}</div>;
-    }
-
-    return renderDefault();
-  };
-
-  const renderFilterInput = (props: any) => {
-    const clear = () => {
-      props.onChange({ target: { value: '' } });
-
-      setMonth && setMonth(null);
-      setYear && setYear(null);
-    };
-
-    return (
-      <InputWrapper>
-        <Filter {...props} />
-        <ClearButton type='button' onClick={clear}>
-          X
-        </ClearButton>
-      </InputWrapper>
-    );
-  };
-
+function FilterDate({ setMonth, setYear }: IProps) {
   return (
-    <Datetime
-    //   name="date"
-      closeOnSelect
-      timeFormat={false}
-      dateFormat={value === 'Month' ? 'MMMM' : 'YYYY'}
-      
-      onChange={changeDate}
-      renderInput={renderFilterInput}
-      renderView={(mode, renderDefault) => renderView(mode, renderDefault)}
-      inputProps={{ placeholder: value, onKeyDown: e => e.preventDefault() }}
-    />
+    <StyledFilters>
+      <DateInput setMonth={setMonth} />
+      <DateInput setYear={setYear} value={"Year"} />
+    </StyledFilters>
   );
 }
 
