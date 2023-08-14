0520fcca438508a03a9b6bae09a084af56aa3039
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-14 22:13:41 +0300

    Update Statistics.tsx

--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -1,42 +1,26 @@
 "use client";
 
-import { useUser } from "@/src/hooks/useUser";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
-import { useMemo, useState } from "react";
-// import {
-//   Table,
-//   TableBody,
-//   TableFooter,
-//   TableHeader,
-//   FooterText,
-//   TableFooterItem,
-// } from "./Statistics.styled";
-import { getCategoryColor } from "@/src/helpers/getCategoryColor";
-import StatItem from "./StatItem";
-import { getStatistics } from "@/src/apiWallet/statistic";
-import { IColor } from "@/src/types/statistics";
 import { useStatistic } from "@/src/apiWallet/useStatistic";
-import FilterDate from "../FilterDate/FilterDate";
-import TestServerComponent from "../TestServerComponent";
 import StatTable from "./StatTable/StatTable";
 
 import stl from "../../app/home/statistic/page.module.scss";
 import { ChartWrapper, PageTitle, TableWrapper } from "./Statistics.styled";
 import Chart from "../Chart/Chart";
+import { useQueryClient } from "@tanstack/react-query";
 
 function Statistics() {
-  const { data = [], setMonth, setYear, expensesData, expensesTotal, incomeTotal } =
+  const { setMonth, setYear, expensesData, expensesTotal, incomeTotal } =
     useStatistic();
 
-    // const client = useQueryClient()
-    // const stat = client.getQueriesData(['Statistics'])
-    // console.log("Statistics  stat:", stat);
+    const client = useQueryClient()
+    const stat = client.getQueriesData(['Statistics'])
+    console.log("Statistics  stat:", stat);
 
   return (
-    <>
+    <div>
       <PageTitle>Statistics</PageTitle>
       <TableWrapper>
-        <ChartWrapper>{<Chart statistic={data} /> }</ChartWrapper>
+        <ChartWrapper>{<Chart /> }</ChartWrapper>
         <StatTable
           setMonth={setMonth}
           setYear={setYear}
@@ -45,7 +29,7 @@ function Statistics() {
           incomeTotal={incomeTotal}
         />
       </TableWrapper>
-    </>
+    </div>
   );
 }
 
