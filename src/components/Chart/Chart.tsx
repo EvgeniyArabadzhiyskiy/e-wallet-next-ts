b70cb4f6afd150d51b855ec967363962eb8ad938
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-15 20:34:49 +0300

    Update Chart.tsx

--- src/components/Chart/Chart.tsx
+++ src/components/Chart/Chart.tsx
@@ -2,19 +2,24 @@
 
 import { useMemo } from "react";
 import { Doughnut } from "react-chartjs-2";
-import { Chart as ChartJS, ArcElement, Tooltip, Legend, Title } from "chart.js";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend, Title, ChartData } from "chart.js";
 
 import { useStatistic } from "@/src/apiWallet";
 import { useUserBalance } from "@/src/apiWallet";
 import { getChartData } from "@/src/helpers/getChartData";
 import { getStatsResult } from "@/src/helpers/getStatsResult";
 import { createCenterTextPlugin } from "@/src/helpers/createCenterTextPlugin";
+import { IStatistic } from "@/src/types/statistics";
+import { DoughnutWrapper } from "./Chart.styled";
 
 ChartJS.register(ArcElement, Tooltip, Legend, Title);
 // console.log("ChartJS:", ChartJS.defaults.plugins.title);
 
-function Chart() {
-  const { data: statistic = [] } = useStatistic();
+interface IProps {
+  statistic: IStatistic[];
+}
+
+function Chart({ statistic }: IProps) {
   const result = getStatsResult(statistic);
   const data = getChartData(result);
 
@@ -24,13 +29,30 @@ function Chart() {
   }, [totalBalance]);
 
   return (
-    <>
+    <DoughnutWrapper>
       <Doughnut
         data={data}
-        options={{ cutout: "70%", plugins: { legend: { display: false } } }}
+        options={{
+          cutout: "70%",
+          aspectRatio: 1,
+           plugins: {
+            legend: {
+              display: false
+            },
+            tooltip: {
+              padding: 18,
+              titleColor: "#10b981",
+              titleFont: {
+                size: 16,
+              },
+              bodyFont: {
+                size: 14
+              }
+            }
+          } }}
         plugins={[centerTextPlugin]}
       />
-    </>
+    </DoughnutWrapper>
   );
 }
 
