81bc3dba0135b9d0fff6bf1ce66d9a084ac5a864
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-16 21:57:17 +0300

    Update getStatsResult.tsx

--- src/helpers/getStatsResult.tsx
+++ src/helpers/getStatsResult.tsx
@@ -4,7 +4,7 @@ import { getCategoryColor } from "./getCategoryColor";
 export const getStatsResult = (stats: IStatistic[]): IChartData => {
   const expenseResults = stats.filter(({ type }) => type === "expense");
 
-  const sum = expenseResults.map(({ totalSum }) => totalSum.toString());
+  const sum = expenseResults.map(({ totalSum }) => totalSum);
   const chartCategories = expenseResults.map(({ _id }) => _id);
   const colors = expenseResults.map(({ _id }) => getCategoryColor(_id));
 
