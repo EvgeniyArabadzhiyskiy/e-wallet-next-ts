d9d8cac999edfd9c51b8a5d55cf9554a65b1748f
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-16 21:57:14 +0300

    Update getChartData.ts

--- src/helpers/getChartData.ts
+++ src/helpers/getChartData.ts
@@ -1,19 +1,20 @@
 import { ChartData } from "chart.js";
 import { IChartData } from "../types/statistics";
 
-export const getChartData = (data: IChartData): ChartData<"doughnut", string[], string> => {
+export const getChartData = (data: IChartData) => {
     const { sum, colors, chartCategories } = data;
     
-    return {
+    const chartData: ChartData<"doughnut", number[], string> = {
       labels: chartCategories.length > 0 ? chartCategories : ['empty'],
       datasets: [
         {
           label: '# of Votes',
-          data: sum.length > 0 ? sum : ['100'],
+          data: sum.length > 0 ? sum : [100],
           backgroundColor:
             colors.length > 0 ? colors : ['#be2d3236'],
           borderWidth: 0,
         },
       ],
     };
+    return chartData
   };
