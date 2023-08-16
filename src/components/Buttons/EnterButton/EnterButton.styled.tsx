accc1ccfc6def98e1b0fd3f154bcebe242d890bb
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-16 21:56:42 +0300

    Update EnterButton.styled.tsx

--- src/components/Buttons/EnterButton/EnterButton.styled.tsx
+++ src/components/Buttons/EnterButton/EnterButton.styled.tsx
@@ -6,53 +6,61 @@ import { baseBtnStyle } from '../DefaultButton.styled';
 export const scale = keyframes`
     0% {
       border: 7px solid rgba(55, 53, 53, 0.4);
-      /* transform: scale(0.6);
-      opacity: 1; */
+     
     }
     50% {
       border: 1px solid rgba(55, 53, 53, 0.4);
-      /* transform: scale(1);
-      opacity: 0.7; */
+     
     }
     100% {
       border: 7px solid rgba(55, 53, 53, 0.4);
-      /* transform: scale(0.6);
-      opacity: 1; */
+    
     }
 `;
 
-export const StyledEnterBtn = styled.button`
+export const StyledEnterBtn = styled.button<{width: number[]; height: number}>`
   ${baseBtnStyle}
+  width: ${p => p.width[0]}%;
+  /* height: 50px; */
 
-  position: relative;
-  color: ${p => p.theme.palette.background.primaryBg};
-  border: 7px solid rgba(55, 53, 53, 0.4);
-  margin-top: ${p => p.theme.space[5]}px;
-  background-color: ${p => p.theme.colors.primaryBgBtn};
+  /* width: ${p => p.width}; */
+  height: ${p => p.height}px;
 
-  /* &:hover {
-  border: 7px solid ${p => p.theme.colors.primaryBgBtn};
+  /* position: relative; */
+  /* margin-top: ${p => p.theme.space[5]}px; */
+  /* color: ${p => p.theme.palette.background.primaryBg}; */
+  /* border: 7px solid rgba(55, 53, 53, 0.4); */
+  /* background-color: ${p => p.theme.colors.primaryBgBtn}; */
 
-} */
-
-  /* transition: border 300ms linear; */
+  color: #a1a1aa;
+  background-color: #022c22;
+  border-radius: 10px;
+  border: 2px solid #10b981;
+  transition: background-color 300ms linear;
 
   &:disabled {
     opacity: 0.7;
   }
 
-  &::before {
+  /* &::before {
     content: '';
     position: absolute;
     top: 0px;
     left: 0px;
     width: 100%;
     height: 100%;
-  }
+  } */
 
   &:hover,
   &:focus {
-    /* border: 4px solid rgb(255, 3, 3); */
-    animation: 2000ms ${scale} infinite;
+    /* animation: 2000ms ${scale} infinite; */
+    background-color: #064e3b;
   }
+
+  @media ${(p) => p.theme.media.medium} {
+    /* width: 300px; */
+    width: ${p => p.width[1]}px;
+  }
+
+  
 `;
