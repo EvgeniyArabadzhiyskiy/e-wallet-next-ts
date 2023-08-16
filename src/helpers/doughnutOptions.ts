bcb63419aad6ed572202604fbc74000ea32d85be
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-16 21:57:10 +0300

    Update doughnutOptions.ts

--- src/helpers/doughnutOptions.ts
+++ src/helpers/doughnutOptions.ts
@@ -1,9 +1,21 @@
-export const doughnutOptions = {
+import { ChartOptions } from "chart.js";
+
+export const doughnutOptions: ChartOptions<"doughnut"> = {
   cutout: "70%",
   aspectRatio: 1,
   plugins: {
     legend: {
       display: false,
+    },
+    tooltip: {
+      padding: 18,
+      titleColor: "#10b981",
+      titleFont: {
+        size: 16,
+      },
+      bodyFont: {
+        size: 14,
+      },
     },
   },
 };
