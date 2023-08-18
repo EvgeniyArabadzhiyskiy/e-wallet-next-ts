960b4f613d0c798d5a25c9d7e59d25ff81e54f80
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-18 19:48:21 +0300

    Update apiPath.ts

--- src/constants/apiPath.ts
+++ src/constants/apiPath.ts
@@ -1,4 +1,6 @@
 export const BASE_URL = 'https://wallet-backend-xmk0.onrender.com/api';
+// export const BASE_URL = 'http://localhost:4001/api';
+
 
 export const USER_CURRENT = '/users/current';
 export const USER_REGISTER = '/users/register';
@@ -7,3 +9,8 @@ export const USER_LOGOUT = '/users/logout';
 
 export const TRANSACTIONS = '/transactions';
 export const BALANCE = '/transactions/balance';
+
+export const AUTH_GOOGLE = '/auth-google/google';
+
+// https://wallet-backend-xmk0.onrender.com/api/auth-google/google-redirect
+// http://localhost:4001/api/auth-google/google-redirect
