3eb99e99cea7779349703fefc027b63898d32535
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-19 22:09:16 +0300

    Update EnterButton.styled.tsx

--- src/components/Buttons/EnterButton/EnterButton.styled.tsx
+++ src/components/Buttons/EnterButton/EnterButton.styled.tsx
@@ -29,16 +29,7 @@ interface IBtnProps {
 export const StyledEnterBtn = styled.button<IBtnProps>`
   ${baseBtnStyle}
   width: ${(p) => p.width.mobile};
-  /* height: 50px; */
-
-  /* width: ${(p) => p.width}; */
   height: ${(p) => p.height}px;
-
-  /* position: relative; */
-  /* margin-top: ${(p) => p.theme.space[5]}px; */
-  /* color: ${(p) => p.theme.palette.background.primaryBg}; */
-  /* border: 7px solid rgba(55, 53, 53, 0.4); */
-  /* background-color: ${(p) => p.theme.colors.primaryBgBtn}; */
 
   color: #a1a1aa;
   background-color: #022c22;
@@ -50,23 +41,12 @@ export const StyledEnterBtn = styled.button<IBtnProps>`
     opacity: 0.7;
   }
 
-  /* &::before {
-    content: '';
-    position: absolute;
-    top: 0px;
-    left: 0px;
-    width: 100%;
-    height: 100%;
-  } */
-
   &:hover,
   &:focus {
-    /* animation: 2000ms ${scale} infinite; */
     background-color: #064e3b;
   }
 
   @media ${(p) => p.theme.media.medium} {
-    /* width: 300px; */
     width: ${(p) => p.width.desctop};
   }
 `;
