be263db0a39ab5a3582669eafec0989dfb7bd908
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-19 22:08:41 +0300

    Update Statistics.styled.tsx

--- src/components/Statistics/Statistics.styled.tsx
+++ src/components/Statistics/Statistics.styled.tsx
@@ -27,57 +27,6 @@ export const ChartWrapper = styled.div`
 
   @media screen and (min-width: 768px) {
     margin-bottom: 0;
+    min-width: 300px;
   }
 `;
-
-// export const Table = styled.div`
-//   @media ${(p) => p.theme.media.medium} {
-//     width: 336px;
-//   }
-
-//   @media ${(p) => p.theme.media.large} {
-//     width: 395px;
-//   }
-// `;
-
-// export const TableHeader = styled.div`
-//   display: flex;
-//   justify-content: space-between;
-//   align-items: center;
-
-//   height: 58px;
-//   padding: 15px 20px;
-//   margin-bottom: 20px;
-//   border-radius: 30px;
-
-//   font-weight: 700;
-//   font-size: 18px;
-//   line-height: 1.5;
-
-//   color: ${(p) => p.theme.colors.primaryText};
-//   background-color: ${(p) => p.theme.colors.primaryBg};
-// `;
-
-// export const TableBody = styled.ul`
-//   padding: 0 20px;
-//   line-height: 1.5;
-// `;
-
-// export const TableFooter = styled.ul`
-//   margin: 0;
-//   border-radius: 10px;
-
-//   font-size: 16px;
-//   font-weight: 700;
-//   line-height: 1.5;
-// `;
-
-// export const TableFooterItem = styled.li`
-//   display: flex;
-//   justify-content: space-between;
-//   padding: 12px 20px;
-// `;
-
-// export const FooterText = styled.p`
-//   color: ${(p) => (p.type === "expense" ? "#FF6596" : "#24CCA7")};
-// `;
