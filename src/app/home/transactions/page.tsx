9ce5f88c178bf45c7f21db0d8025c5dfe1efaf8c
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-19 22:09:26 +0300

    Update page.tsx

--- src/app/home/transactions/page.tsx
+++ src/app/home/transactions/page.tsx
@@ -1,5 +1,12 @@
 import TransactionTable from "@/src/components/TransactionTable/TransactionTable";
+import { Suspense } from "react";
 
 export default function PageTransactions() {
-  return <TransactionTable />;
+  return (
+    <>
+      <Suspense fallback={<h1 style={{ color: "white" }}>SUSPENSE...</h1>}>
+        <TransactionTable />
+      </Suspense>
+    </>
+  );
 }
