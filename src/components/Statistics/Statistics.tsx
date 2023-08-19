4d1c83a8229286ec5d27799e96f154b8c23b7338
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-19 22:08:43 +0300

    Update Statistics.tsx

--- src/components/Statistics/Statistics.tsx
+++ src/components/Statistics/Statistics.tsx
@@ -1,9 +1,8 @@
 "use client";
 
 import { useStatistic } from "@/src/apiWallet/useStatistic";
-import StatTable from "./StatTable/StatTable";
+import StatTable from "./StatTable";
 
-import stl from "../../app/home/statistic/page.module.scss";
 import { ChartWrapper, PageTitle, TableWrapper } from "./Statistics.styled";
 import Chart from "../Chart/Chart";
 import { useQueryClient } from "@tanstack/react-query";
