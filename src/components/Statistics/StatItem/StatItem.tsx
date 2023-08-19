58a0072cfe30fd3f48865ca300fcf11d1a9f04fc
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-19 22:08:47 +0300

    Update StatItem.tsx

--- src/components/Statistics/StatItem/StatItem.tsx
+++ src/components/Statistics/StatItem/StatItem.tsx
@@ -20,28 +20,3 @@ function StatItem({ categoryColor, totalSum, id }: IProps) {
 }
 
 export default StatItem;
-
-//======================================================
-// import stl from "./StatItem.module.scss"
-
-// interface IProps {
-//   categoryColor: string;
-//   totalSum: number;
-//   id: string | number;
-// }
-
-// function StatItem({ categoryColor, totalSum, id }: IProps) {
-//   console.log("Client Item");
-  
-//   return (
-//     <li className={stl.table__item}>
-//       <div className={stl.wrapper}>
-//         <span style={{backgroundColor: categoryColor}} className={stl.color__text}  />
-//         <span>{id}</span>
-//       </div>
-//       <p>{totalSum}</p>
-//     </li>
-//   );
-// }
-
-// export default StatItem;
