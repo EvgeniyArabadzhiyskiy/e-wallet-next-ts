2f9f1763060723c52487c71c7be837194842097c
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-20 22:05:23 +0300

    Update page.tsx

--- src/app/home/statistic/page.tsx
+++ src/app/home/statistic/page.tsx
@@ -2,14 +2,40 @@
 
 // import { useEffect, useState } from "react";
 
+import { getBalance } from "@/src/apiWallet/balance";
+import { getStatistics } from "@/src/apiWallet/statistic";
 import Statistics from "@/src/components/Statistics";
+import { authOptions } from "@/src/lib/auth";
+import getQueryClient from "@/src/lib/getQueryClient";
+import { Hydrate, dehydrate } from "@tanstack/react-query";
+import { getServerSession } from "next-auth";
 
-export default function PageStatistic() {
+export default async function PageStatistic() {
   // const [pageNum, setPageNum] = useState(1);
+
+  const session = await getServerSession(authOptions);
+  const authToken = session?.token;
+
+  const queryClient = getQueryClient();
+
+  if (authToken) {
+    // const balanceQuery = queryClient.prefetchQuery(["Balance"], () => getBalance(authToken));
+    
+    const statisticsQuery =  queryClient.prefetchQuery({
+      queryKey: ["Statistics", { month: "", year: "" }],
+      queryFn: () => getStatistics(authToken, { month: "", year: "" }),
+    });
+
+    await Promise.allSettled([ statisticsQuery])
+  }
+
+  const dehydratedState = dehydrate(queryClient);
 
   return (
     <>
-      <Statistics />
+      <Hydrate state={dehydratedState}>
+        <Statistics />
+      </Hydrate>
 
       {/* <Link href="/">HOME</Link>
       <h1>Page Statistic </h1>
