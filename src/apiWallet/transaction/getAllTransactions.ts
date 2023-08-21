259188aea6b45e0460f0fc71220d3b16d5c4c254
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-21 21:10:14 +0300

    Update getAllTransactions.ts

--- src/apiWallet/transaction/getAllTransactions.ts
+++ src/apiWallet/transaction/getAllTransactions.ts
@@ -5,9 +5,9 @@ import { ITransactions } from "../../types/transactions";
 
 export const getAllTransactions = 
 async (authToken: string | undefined, pageNum: number) => {
-  await new Promise((res) =>
-    setTimeout(() => res(console.log("Promise resolve")), 8000)
-  );
+  // await new Promise((res) =>
+  //   setTimeout(() => res(console.log("Promise resolve")), 5000)
+  // );
 
   const options: RequestInit = {
     method: "GET",
