c9020d8d4cd33b5e0025bfc29c574304eeb84e2a
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-21 21:10:19 +0300

    Update page.tsx

--- src/app/home/transactions/page.tsx
+++ src/app/home/transactions/page.tsx
@@ -1,45 +1,49 @@
 import { getBalance } from "@/src/apiWallet/balance";
 import { getAllTransactions } from "@/src/apiWallet/transaction";
+import LoaderTransaction from "@/src/components/LoaderTransaction/LoaderTransaction";
 import TransactionTable from "@/src/components/TransactionTable/TransactionTable";
+import WrapperTransaction from "@/src/components/WrapperTransaction";
 import { authOptions } from "@/src/lib/auth";
 import getQueryClient from "@/src/lib/getQueryClient";
 import { Hydrate,  dehydrate } from "@tanstack/react-query";
 import { getServerSession } from "next-auth";
+import { Suspense } from "react";
 
 
-export default async function PageTransactions() {
+export default  function PageTransactions() {
   //   await new Promise((res) =>
   //   setTimeout(() => res(console.log("Promise resolve")), 4000)
   // );
-  const session = await getServerSession(authOptions);
-  const authToken = session?.token;
+  // const session = await getServerSession(authOptions);
+  // const authToken = session?.token;
 
-  const queryClient = getQueryClient();
+  // const queryClient = getQueryClient();
 
-  if (authToken) {
-    // const balanceQuery = queryClient.prefetchQuery(["Balance"], () =>
-    //   getBalance(authToken)
-    // );
+  // if (authToken) {
+  //   const balanceQuery = queryClient.prefetchQuery(["Balance"], () =>
+  //     getBalance(authToken)
+  //   );
 
-    const transactionsQuery = await queryClient.prefetchInfiniteQuery({
-      queryKey: ["TransactionsList"],
-      queryFn: ({ pageParam = 1 }) =>
-        getAllTransactions(authToken, pageParam),
+  //   const transactionsQuery = await queryClient.prefetchInfiniteQuery({
+  //     queryKey: ["TransactionsList"],
+  //     queryFn: ({ pageParam = 1 }) =>
+  //       getAllTransactions(authToken, pageParam),
         
-    });
+  //   });
     
-  }
+  // }
 
-  const dehydratedState = dehydrate(queryClient);
+  // const dehydratedState = dehydrate(queryClient);
 
   return (
     <>
-      <Hydrate state={dehydratedState}>
+      {/* <Hydrate state={dehydratedState}>
         <TransactionTable />
-      </Hydrate>
+      </Hydrate> */}
 
-      {/* <Suspense fallback={<h1 style={{ color: "white" }}>SUSPENSE...</h1>}>
-      </Suspense> */}
+      <Suspense fallback={<LoaderTransaction />}>
+        <WrapperTransaction />
+      </Suspense>
     </>
   );
 }
