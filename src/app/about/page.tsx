8a8a4ec43feea153acef2fb0c9e3332f76c4ae50
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-22 17:53:33 +0300

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,7 +2,7 @@
 
 // import { Inter, Fira_Code } from "next/font/google";
 // import Navigation from "@/src/components/Navigation/Navigation";
-import LoaderTransaction from "@/src/components/LoaderTransaction/LoaderTransaction";
+import LoaderTransaction from "@/src/components/TransactionLoader/TransactionLoader";
 import PokemonList from "@/src/components/PokemonList";
 import PokemonListClient from "@/src/components/PokemonListClient";
 import TransactionList from "@/src/components/TransactionList/TransactionList";
