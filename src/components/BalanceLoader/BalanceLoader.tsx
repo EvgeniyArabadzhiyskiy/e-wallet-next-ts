a309196b96b8482102912cd2dfe7e0ed0b2e8a62
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-22 19:35:07 +0300

    Update BalanceLoader.tsx

--- src/components/BalanceLoader/BalanceLoader.tsx
+++ src/components/BalanceLoader/BalanceLoader.tsx
@@ -1,16 +1,21 @@
-"use client";
+// "use client";
 
+import { StyledLink } from "../Buttons/LinkButton/LinkButton.styled";
 import { Wrapper, Title, Text, Loader } from "./BalanceLoader.styled";
+import stl from "./BalanceLoader.module.scss";
 
 function BalanceLoader() {
   const placeholderBalance = -87710.0;
   return (
-    <Wrapper>
-      <Title>Your balance</Title>
-      <Text>
-        <Loader>{placeholderBalance.toFixed(2)}</Loader>
-      </Text>
-    </Wrapper>
+    // <Wrapper>
+    //   <Title>Your balance</Title>
+    //   <Loader>{placeholderBalance.toFixed(2)}</Loader>
+    // </Wrapper>
+
+    <div className={stl.wrapper}>
+      <p className={stl.balance__title}>Your balance</p>
+      <div className={stl.loading}>{placeholderBalance.toFixed(2)}</div>
+    </div>
   );
 }
 
