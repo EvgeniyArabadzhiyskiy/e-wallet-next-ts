b1d9358397d57eb73015614d184c49102aaad5bd
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-22 19:57:55 +0300

    Update TransactionLoader.styled.tsx

--- src/components/TransactionLoader/TransactionLoader.styled.tsx
+++ src/components/TransactionLoader/TransactionLoader.styled.tsx
@@ -1,50 +1,50 @@
-"use client";
+// "use client";
 
-import styled, { keyframes } from "styled-components";
+// import styled, { keyframes } from "styled-components";
 
-const loadingAnimation = keyframes`
-  0% {
-    background-position: 0% 0;
-  }
-  50% {
-    background-position: 100% 0;
-  }
-  100% {
-    background-position: 0 0;
-  }
-`;
+// const loadingAnimation = keyframes`
+//   0% {
+//     background-position: 0% 0;
+//   }
+//   50% {
+//     background-position: 100% 0;
+//   }
+//   100% {
+//     background-position: 0 0;
+//   }
+// `;
 
-export const Loader = styled.div`
-  height: 308px;
+// export const Loader = styled.div`
+//   height: 308px;
 
-  background: linear-gradient(to left, #111827, #a7acb1);
-  background-size: 200% 100%;
+//   background: linear-gradient(to left, #111827, #a7acb1);
+//   background-size: 200% 100%;
 
-  filter: blur(10px);
-  animation: ${loadingAnimation} 1s ease infinite;
+//   filter: blur(10px);
+//   animation: ${loadingAnimation} 1s ease infinite;
 
-  @media (min-width: 768px) {
-    width: 672px;
+//   @media (min-width: 768px) {
+//     width: 672px;
 
-    border-bottom: 1px solid #dcdcdf;
-  }
-`;
+//     border-bottom: 1px solid #dcdcdf;
+//   }
+// `;
 
 
-const animationTest = keyframes`
-   0% {
-    background-color: #f96b12;
-  }
-  100% {
-    background-color: #12f443;
-  }
-`;
+// const animationTest = keyframes`
+//    0% {
+//     background-color: #f96b12;
+//   }
+//   100% {
+//     background-color: #12f443;
+//   }
+// `;
 
-export const Test = styled.div`
-  width: 400px;
-  height: 50px;
+// export const Test = styled.div`
+//   width: 400px;
+//   height: 50px;
 
-  background-color: #a06d6d;
+//   background-color: #a06d6d;
 
-  animation: ${animationTest} 1.5s infinite linear alternate;
-`;
+//   animation: ${animationTest} 1.5s infinite linear alternate;
+// `;
