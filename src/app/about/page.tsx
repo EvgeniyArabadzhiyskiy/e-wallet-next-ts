2536380d9527efc7a4554a6d82c4951003616976
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-23 21:05:56 +0300

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -8,6 +8,11 @@ import PokemonListClient from "@/src/components/PokemonListClient";
 import TransactionList from "@/src/components/TransactionList/TransactionList";
 import axios from "axios";
 import { Suspense } from "react";
+import getQueryClient from "@/src/lib/getQueryClient";
+import { Hydrate, dehydrate } from "@tanstack/react-query";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/src/lib/auth";
+import { getBalance } from "@/src/apiWallet/balance";
 
 // const inter = Inter({
 //   subsets: ["latin"],
@@ -22,9 +27,28 @@ const getPokemon = async () => {
   return data.results;
 };
 
-export default  function AboutPage() {
+export default async function AboutPage() {
   // const pokemons = await getPokemon();
   //   console.log("PokemonList  pokemons:", pokemons);
+
+  const session = await getServerSession(authOptions);
+  const authToken = session?.token;
+
+  const queryClient = getQueryClient();
+
+  // if (authToken) {
+  //   const balanceQuery = await queryClient.prefetchQuery(["Balance"], () => getBalance(authToken));
+    
+   
+
+  
+  // }
+
+  const balance = queryClient.getQueriesData<any>(["Balance"]);
+  console.log("About  Balance:+++++++++++++++++++++++++++++++++++", balance);
+
+
+  const dehydratedState = dehydrate(queryClient);
 
   return (
     <div>
@@ -46,13 +70,11 @@ export default  function AboutPage() {
       {/* <Suspense fallback={<h1 style={{ color: "white" }}>SUSPENSE...</h1>}>
         <PokemonList />
       </Suspense> */}
-
-      {/* <PokemonListClient /> */}
-
+      <Hydrate state={dehydratedState}>
+        <PokemonListClient />
+      </Hydrate>
       {/* <LoaderTransaction /> */}
       {/* <TransactionList /> */}
-
-
     </div>
   );
 }
