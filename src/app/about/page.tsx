a5bb2fb5905784bb9f858fd55350a4896da03df3
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-26 13:05:18 +0300

    Update page.tsx

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -77,7 +77,7 @@ export default async function AboutPage() {
       {/* </Hydrate> */}
       {/* <LoaderTransaction /> */}
       {/* <TransactionLoader /> */}
-      {/* <StatisticLoader /> */}
+      <StatisticLoader />
     </div>
   );
 }
