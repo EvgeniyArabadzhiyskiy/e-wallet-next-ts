b633229530397b244a17b15909173f2811d3f388
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-27 21:57:08 +0300

    Update PokemonListClient.tsx

--- src/components/PokemonListClient.tsx
+++ src/components/PokemonListClient.tsx
@@ -5,11 +5,12 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useUserBalance } from "../apiWallet";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const getPokemon = async () => {
-  await new Promise((res) =>
-    setTimeout(() => res(console.log("Promise resolve")), 4000)
-  );
+  // await new Promise((res) =>
+  //   setTimeout(() => res(console.log("Promise resolve")), 4000)
+  // );
 
   const { data } = await axios(
     `https://pokeapi.co/api/v2/pokemon?offset=10&limit=10`
@@ -21,6 +22,9 @@ export default function PokemonListClient() {
   // const queryClient = useQueryClient();
   // const Balance = queryClient.getQueriesData<any>(["Balance"]);
   // console.log(" Balance:", Balance[0]);
+
+  const router = useRouter()
+  
 
   const { data: totalBalance = 0 } = useUserBalance();
   // console.log("PokemonListClient  totalBalance:", totalBalance);
@@ -37,13 +41,26 @@ export default function PokemonListClient() {
   //     })();
   //   },[]);
 
-  // const { data: pokemons = [], isLoading} = useQuery({
-  //     queryKey: ["Pokemons"],
-  //     queryFn: getPokemon,
-  //     staleTime: Infinity,
-  //     refetchOnWindowFocus: false,
-  // })
+  const { data: pokemons = [], isError, error, isLoading} = useQuery({
+    queryKey: ["Pokemons"],
+    queryFn: getPokemon,
+    staleTime: Infinity,
+    refetchOnWindowFocus: false,
+  })
   // console.log("isLoading====================", isLoading);
+  // console.log("PokemonListClient  isError:", isError);
+
+  if (isError) {
+    // return (
+    //   <>
+    //     {/* <h1 style={{color: "white"}}>{error.message}</h1> */}
+    //     <button  type="button" onClick={() => console.log("Try Now Again")}>
+    //       Try Again
+    //     </button>
+    //   </>
+    // );
+    router.push("/home")
+  }
 
   return (
     <>
@@ -51,7 +68,7 @@ export default function PokemonListClient() {
         Pokemon List Client Balance: {totalBalance}
       </h1>
       <Link style={{color: "white"}} href="/home/statistic">Statistic</Link>
-      {/* {isLoading 
+      {isLoading 
       ? <h1 style={{ color: "white" }}>QUERY LOADING...</h1>
       : <ul>
         {pokemons.map((item: any) => {
@@ -61,7 +78,7 @@ export default function PokemonListClient() {
             </li>
           );
         })}
-      </ul>} */}
+      </ul>}
     </>
   );
 }
