0b88129417d575507ff20f94cd21a90362d4b0ba
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-27 20:39:31 +0300

    Update Providers.tsx

--- src/components/Providers/Providers.tsx
+++ src/components/Providers/Providers.tsx
@@ -2,7 +2,6 @@
 
 import { useEffect, useState } from "react";
 import { MediaContextProvider } from "@/src/lib/media";
-import { GlobalStyle } from "@/src/styles/theme/GlobalStyle";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "styled-components";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -14,7 +13,20 @@ interface IProps {
 }
 
 export const Providers = ({ children }: IProps) => {
-  const [queryClient] = useState(() => new QueryClient());
+  const [queryClient] = useState(() => new QueryClient(
+    {
+        defaultOptions: {
+          queries: {
+            staleTime: Infinity,
+
+            refetchOnMount: false,
+            refetchOnReconnect: false,
+            refetchOnWindowFocus: false,
+            retry: 0,
+          },
+        },
+      }
+  ));
   const { theme } = useGlobalState();
 
   // if (isLoading) {
@@ -30,7 +42,6 @@ export const Providers = ({ children }: IProps) => {
           </QueryClientProvider>
         </MediaContextProvider>
       </ThemeProvider>
-      {/* <GlobalStyle /> */}
     </>
   );
 };
