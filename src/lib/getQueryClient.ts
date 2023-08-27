67023899cebe769cfd8460b7eb5db23b02348086
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-27 21:10:28 +0300

    Update getQueryClient.ts

--- src/lib/getQueryClient.ts
+++ src/lib/getQueryClient.ts
@@ -2,17 +2,18 @@ import { QueryClient } from "@tanstack/react-query";
 import { cache } from "react";
 
 const getQueryClient = cache(() => new QueryClient(
-   // {
-  //     defaultOptions: {
-  //       queries: {
-  //         staleTime: Infinity,
+   {
+      defaultOptions: {
+        queries: {
+          staleTime: Infinity,
+          cacheTime: Infinity,
 
-  //         refetchOnMount: false,
-  //         refetchOnReconnect: false,
-  //         refetchOnWindowFocus: false,
-  //         retry: 0,
-  //       },
-  //     },
-  //   }
+          refetchOnMount: false,
+          refetchOnReconnect: false,
+          refetchOnWindowFocus: false,
+          retry: 0,
+        },
+      },
+    }
 ));
 export default getQueryClient;
