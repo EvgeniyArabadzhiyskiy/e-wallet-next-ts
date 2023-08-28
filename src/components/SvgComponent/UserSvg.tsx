9a576bd3dece029d53e6ef3e8e6ab1bf4b0d4a81
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-28 23:07:31 +0300

    Create UserSvg.tsx

---
+++ src/components/SvgComponent/UserSvg.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import { SVGProps } from "react";
+
+const UserSvg = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 25 24"
+    {...props}
+  >
+    <g clipPath="url(#a)">
+      <path
+        fill="#E0E0E0"
+        d="M3.5 5v14a2 2 0 0 0 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2h-14a2 2 0 0 0-2 2Zm12 4c0 1.66-1.34 3-3 3s-3-1.34-3-3 1.34-3 3-3 3 1.34 3 3Zm-9 8c0-2 4-3.1 6-3.1s6 1.1 6 3.1v1h-12v-1Z"
+      />
+    </g>
+    <defs>
+      <clipPath id="a">
+        <path fill="#fff" d="M.5 0h24v24H.5z" />
+      </clipPath>
+    </defs>
+  </svg>
+);
+export default UserSvg;
