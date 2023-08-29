346b6dd3a0865dc69506e800355b9b20daccc396
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-29 21:48:14 +0300

    Update fetcher.ts

--- src/helpers/fetcher.ts
+++ src/helpers/fetcher.ts
@@ -4,9 +4,19 @@ export async function fetcher<T>(url: RequestInfo, options?: RequestInit): Promi
   const response = await fetch(`${BASE_URL}${url}`, options);
 
   if (!response.ok) {
-    // throw new Error(`Request failed with status ${response.status}`);
-    throw new Error(`${response.status}`);
+    const errorText = await response.text();
+    const errorData = JSON.parse(errorText);
+    console.log("rrr:", errorData);
+
+    // const errorData = await response.json();
+    const errorMessage = errorData.message || `Request failed with status ${response.status}.`;
+    throw new Error(errorMessage);
   }
 
-  return await response.json();
+  const data = await response.json();
+  console.log("data:", data);
+  
+  return data;
+
+  
 }
