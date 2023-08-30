d8f047e5a59dd758b5726c53f14a917a42a0319d
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-08-30 21:18:32 +0300

    Create CreateTransactionError.styled.tsx

---
+++ src/components/Errors/CreateTransactionError/CreateTransactionError.styled.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import styled from "styled-components";
+
+export const ErrorTitle = styled.p`
+  margin-bottom: 30px;
+  color: #10b981;
+  font-size: 24px;
+  text-align: center;
+`;
+
+export const ErrorText = styled.p`
+  color: #a1a1aa;
+  font-size: 28px;
+`;
