c6ae5b37d599efe54b61b41c99faa049eee27e99
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-09-01 22:20:12 +0300

    Update LinkButton.styled.tsx

--- src/components/Buttons/LinkButton/LinkButton.styled.tsx
+++ src/components/Buttons/LinkButton/LinkButton.styled.tsx
@@ -4,27 +4,15 @@ import Link from "next/link";
 import styled from "styled-components";
 import { baseBtnStyle } from "../DefaultButton.styled";
 
-export const StyledLink = styled(Link)`
+export const StyledLink = styled(Link)<{ $maxWidth: string }>`
   ${baseBtnStyle}
   height: 50px;
-  width: 80%;
+  width: 100%;
+  max-width: ${(p) => p.$maxWidth};
 
   display: flex;
   align-items: center;
   justify-content: center;
-
-  /* border: 1px solid #15275160;
-  color: ${(p) => p.theme.colors.white};
-  background-color: ${(p) => p.theme.colors.primaryBgBtn};
-  border-bottom: 5px solid #15275160;
-  transition: all 150ms linear; */
-
-  /* &:hover,
-  &:focus {
-    border-color: #31a1cd;
-    border-bottom: 1px solid #3187cd;
-    color: #3187cd;
-  } */
 
   color: #a1a1aa;
   background-color: #022c22;
@@ -35,9 +23,5 @@ export const StyledLink = styled(Link)`
   &:hover,
   &:focus {
     background-color: #064e3b;
-  }
-
-  @media ${(p) => p.theme.media.medium} {
-    width: 300px;
   }
 `;
