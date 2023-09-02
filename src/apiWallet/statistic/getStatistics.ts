5d37ac5cda20dd7dd16123fa9a8a508dc4084b17
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-09-02 21:41:47 +0300

    Update getStatistics.ts

--- src/apiWallet/statistic/getStatistics.ts
+++ src/apiWallet/statistic/getStatistics.ts
@@ -1,13 +1,8 @@
-import { BASE_URL } from "@/src/constants/apiPath";
 import { fetcher } from "@/src/helpers/fetcher";
 import { getQueryString } from "@/src/helpers/getQueryString";
 import { IStatPeriod, IStatistic } from "@/src/types/statistics";
 
 export const getStatistics = async (token: string | undefined, { month, year }: IStatPeriod ) => {
-  // await new Promise((res) =>
-  //   setTimeout(() => res(console.log("Promise resolve")), 4000)
-  // );
-
   const options: RequestInit = {
     method: "GET",
     headers: {
@@ -16,9 +11,5 @@ export const getStatistics = async (token: string | undefined, { month, year }: 
     // cache: "no-store",
   };
 
-  const data = await fetcher<IStatistic[]>(`${getQueryString({ month, year })}`, options);
-
-  // console.log("data:", data);
-  return data
-    
-  };
+  return await fetcher<IStatistic[]>(`${getQueryString({ month, year })}`, options);
+};
