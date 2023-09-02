7781716c2a1f6b37755bbb6e652d8efc33b5868a
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-09-02 21:42:22 +0300

    Update HeroSection.tsx

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,88 +1,10 @@
-// "use client";
-
 import stl from "./HeroSection.module.scss";
 import Container from "../Container/Container";
-import LinkButton from "../Buttons/LinkButton/LinkButton";
+import LinkButton from "../Buttons/LinkButton";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/src/lib/auth";
 import Image from "next/image";
-
-// import Finance1 from "@/public/images/finance-6.webp";
-// import Finance2 from "@/public/images/finance-9.webp";
-// import Finance3 from "@/public/images/finance-17.webp";
-// import Finance4 from "@/public/images/finance-18.webp";
-
-
-// import styled from "styled-components";
-
-const heroCardList = [
-  {
-    id: "1",
-    image: {
-      src: "/images/finance-17.webp",
-      alt: "image",
-    },
-    title: "Control Your Finances with Ease",
-    description:
-      "Review your transaction history, whether it is payments, transfers, or deposits, all in one place.",
-  },
-  {
-    id: "2",
-    image: {
-      src: "/images/finance-6.webp",
-      alt: "image",
-    },
-    title: "Your Financial Picture in One Place",
-    description:
-      "Track your balance, payments, and transfers with the convenience of our app.",
-  },
-
-  {
-    id: "3",
-    image: {
-      src: "/images/finance-9.webp",
-      alt: "image",
-    },
-    title: "Manage Your Money Hassle-Free",
-    description:
-      "Transaction reports and balance always at your fingertips, so you can focus on what truly matters.",
-  },
-  {
-    id: "4",
-    image: {
-      src: "/images/finance-18.webp",
-      alt: "image",
-    },
-    title: "Real-Time Balance and Funds Movement",
-    description:
-      "Do not miss a beat – our E-Wallet will help you stay on top of every transaction.",
-  },
-];
-
-
-
-// const Wrapper = styled.div`
-//   position: relative;
-//   width: 100%;
-//   padding: 30%;
-
-//   background-image: url("/images/finance-9.webp");
-//   background-repeat: no-repeat;
-//   background-position: center;
-//   background-size: cover;
-// `;
-
-// const Item = styled.li`
-//   width: 100%;
-//   border: 3px solid #064e3b;
-//   border-radius: 10px;
-//   color: #a1a1aa;
-//   overflow: hidden;
-
-//   @media (min-width: 1280px) {
-//     width: calc(100% / 2 - 50px);
-//   }
-// `;
+import { heroCardList } from "@/src/helpers/heroCardList";
 
 async function HeroSection() {
   const session = await getServerSession(authOptions);
@@ -103,16 +25,15 @@ async function HeroSection() {
               <span className={stl.hero__title__accent}> totally FREE</span>
             </p>
 
-            {
-              session
-               ? <LinkButton href="/home/transactions" text="WALLET" maxWidth="300px" />
-               :
-              <LinkButton href="/login" text="Start in 30 seconds" maxWidth="300px" />
-            }
+            <div className={stl.link__wrapper}>
+              {session
+                ? <LinkButton href="/home/transactions" text="WALLET" maxWidth="300px" />
+                : <LinkButton href="/login" text="START IN 30 SECONDS" maxWidth="300px" />
+              }
+            </div>
           </div>
-          <div>
-            <div className={stl.banner}></div>
-          </div>
+
+          <div className={stl.banner}></div>
 
           <ul className={stl.card__list}>
             {heroCardList.map((item) => {
@@ -131,9 +52,7 @@ async function HeroSection() {
                   </div>
                 </li>
               );
-            })}
-
-           
+            })}     
           </ul>
         </div>
       </Container>
@@ -142,60 +61,3 @@ async function HeroSection() {
 }
 
 export default HeroSection;
- {/* <li className={stl.card}>
-              <Image src={Finance4} alt="img" />
-              <Wrapper></Wrapper>
-
-              <div className={stl.card__title__wrapper}>
-                <h1 className={stl.card__title}>
-                  Your Financial Picture in One Place
-                </h1>
-                <p>
-                  Track your balance, payments, and transfers with the
-                  convenience of our app.
-                </p>
-              </div>
-            </li>
-
-            <li className={stl.card}>
-              <Image src={Finance2} alt="img" />
-              <Wrapper></Wrapper>
-
-              <div className={stl.card__title__wrapper}>
-                <h1 className={stl.card__title}>
-                  Manage Your Money Hassle-Free
-                </h1>
-                <p>
-                  Transaction reports and balance always at your fingertips, so
-                  you can focus on what truly matters.
-                </p>
-              </div>
-            </li>
-
-            <li className={stl.card}>
-              <Image src={Finance1} alt="img" />
-              <Wrapper></Wrapper>
-              <div className={stl.card__title__wrapper}>
-                <h1 className={stl.card__title}>
-                  Control Your Finances with Ease
-                </h1>
-                <p>
-                  Review your transaction history, whether it is payments,
-                  transfers, or deposits, all in one place.
-                </p>
-              </div>
-            </li>
-
-            <li className={stl.card}>
-              <Image src={Finance3} alt="img" />
-              <Wrapper></Wrapper>
-              <div className={stl.card__title__wrapper}>
-                <h1 className={stl.card__title}>
-                  Real-Time Balance and Funds Movement
-                </h1>
-                <p>
-                  Do not miss a beat – our E-Wallet will help you stay on top of
-                  every transaction.
-                </p>
-              </div>
-            </li> */}
