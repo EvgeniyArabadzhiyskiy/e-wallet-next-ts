62d3a6f2c6e08565a3fa5a336b835da2b54d4416
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-09-02 21:42:45 +0300

    Update getMessageForError.ts

--- src/helpers/getMessageForError.ts
+++ src/helpers/getMessageForError.ts
@@ -3,23 +3,18 @@ export const getMessageForError = (errorCode: string) => {
     case "401":
       return {
         reason: "You are not authorized to access this resource.",
-        text: "Please log in or authenticate to proceed."
-      }
-      
-      case "404":
-          return {
-            reason: "Oops, the page you're looking for doesn't exist.",
-            text: "Please check the URL and try again."
-          }
-          default:
-              return {
-                reason: `An error occurred ${errorCode}.`,
-                text: "Please try again later."
-              }
-              
-            }
-        };
-        
-        // "You are not authorized to access this resource. Please log in or authenticate to proceed.";
-        // "Oops, the page you're looking for doesn't exist. Please check the URL and try again.";
-        // `An error occurred ${errorCode}. Please try again later.`;
+        text: "Please log in or authenticate to proceed.",
+      };
+
+    case "404":
+      return {
+        reason: "Oops, the page you're looking for doesn't exist.",
+        text: "Please check the URL and try again.",
+      };
+    default:
+      return {
+        reason: "An error occurred.",
+        text: "Please try again later.",
+      };
+  }
+};
