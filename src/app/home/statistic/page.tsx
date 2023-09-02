52a5617181fcbaf320958becbe4cfcbd8f1ed90a
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-09-02 21:43:03 +0300

    Update page.tsx

--- src/app/home/statistic/page.tsx
+++ src/app/home/statistic/page.tsx
@@ -2,56 +2,14 @@
 
 // import { useEffect, useState } from "react";
 
-import { getBalance } from "@/src/apiWallet/balance";
-import { getStatistics } from "@/src/apiWallet/statistic";
+import { Suspense } from "react";
 import StatisticLoader from "@/src/components/StatisticLoader";
 import StatisticWrapper from "@/src/components/StatisticWrapper";
-import Statistics from "@/src/components/Statistics";
-import TransactionWrapper from "@/src/components/TransactionWrapper";
-import { authOptions } from "@/src/lib/auth";
-import getQueryClient from "@/src/lib/getQueryClient";
-import { Hydrate, dehydrate } from "@tanstack/react-query";
-import { getServerSession } from "next-auth";
-import { Suspense } from "react";
 
 export default async function PageStatistic() {
-  // const [pageNum, setPageNum] = useState(1);
-
-  // const session = await getServerSession(authOptions);
-  // const authToken = session?.token;
-
-  // const queryClient = getQueryClient();
-
-  // if (authToken) {
-  //   const balanceQuery = queryClient.prefetchQuery(["Balance"], () =>
-  //     getBalance(authToken)
-  //   );
-
-  //   const statisticsQuery = queryClient.prefetchQuery({
-  //     queryKey: ["Statistics", { month: "", year: "" }],
-  //     queryFn: () => getStatistics(authToken, { month: "", year: "" }),
-  //   });
-
-  //   await Promise.allSettled([balanceQuery, statisticsQuery]);
-  // }
-
-  // const balance = queryClient.getQueriesData<any>(["Balance"]);
-  // console.log("TransactionList  Balance:+++++++++++++++++++++++++++++++++++", balance);
-
-  // const dehydratedState = dehydrate(queryClient);
-
   return (
-    <>
-      <Suspense fallback={<StatisticLoader />}>
-        <StatisticWrapper />
-      </Suspense>
-
-      {/* <Link href="/">HOME</Link>
-      <h1>Page Statistic </h1>
-      <div style={{height:250, backgroundColor: "green"}} ></div> */}
-      {/* <button type="button" onClick={() => setPageNum((p) => p + 1)}>
-        Click
-      </button> */}
-    </>
+    <Suspense fallback={<StatisticLoader />}>
+      <StatisticWrapper />
+    </Suspense>
   );
 }
