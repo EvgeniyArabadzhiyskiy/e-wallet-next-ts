3f4bd883b100c77ec89145700d7e7bbd5cd92d6b
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-09-02 21:42:17 +0300

    Create ErrorPage.styled.tsx

---
+++ src/components/Errors/ErrorPage/ErrorPage.styled.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import styled from "styled-components";
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  column-gap: 20px;
+  margin-top: 28px;
+`;
