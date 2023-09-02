68b326cb6e83f8164a980f52c214e2276311d72e
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-09-02 21:42:47 +0300

    Create heroCardList.ts

---
+++ src/helpers/heroCardList.ts
@@ -0,0 +1,45 @@
+import { IHeroCard } from "../types/heroCard";
+
+export const heroCardList: IHeroCard[] = [
+  {
+    id: "1",
+    image: {
+      src: "/images/finance-1.webp",
+      alt: "data base",
+    },
+    title: "Control Your Finances with Ease",
+    description:
+      "Review your transaction history, whether it is payments, transfers, or deposits, all in one place.",
+  },
+  {
+    id: "2",
+    image: {
+      src: "/images/finance-2.webp",
+      alt: "finance chart",
+    },
+    title: "Your Financial Picture in One Place",
+    description:
+      "Track your balance, payments, and transfers with the convenience of our app.",
+  },
+
+  {
+    id: "3",
+    image: {
+      src: "/images/finance-3.webp",
+      alt: "finance trend",
+    },
+    title: "Manage Your Money Hassle-Free",
+    description:
+      "Transaction reports and balance always at your fingertips, so you can focus on what truly matters.",
+  },
+  {
+    id: "4",
+    image: {
+      src: "/images/finance-4.webp",
+      alt: "statstic",
+    },
+    title: "Real-Time Balance and Funds Movement",
+    description:
+      "Do not miss a beat – our E-Wallet will help you stay on top of every transaction.",
+  },
+];
