f212c597fce9d6662dbcd964a71016fb6838723e
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-09-03 19:39:30 +0300

    Update ErrorPage.styled.tsx

--- src/components/Errors/ErrorPage/ErrorPage.styled.tsx
+++ src/components/Errors/ErrorPage/ErrorPage.styled.tsx
@@ -2,9 +2,25 @@
 
 import styled from "styled-components";
 
+export const ErrorContainer = styled.div`
+  padding: 30px 0;
+
+  @media ${(p) => p.theme.media.medium} {
+    padding: 30px;
+  }
+`;
+
 export const ButtonWrapper = styled.div`
   display: flex;
-  justify-content: center;
-  column-gap: 20px;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 20px;
   margin-top: 28px;
+
+  @media ${(p) => p.theme.media.medium} {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    column-gap: 20px;
+  }
 `;
