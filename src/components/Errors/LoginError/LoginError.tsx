9b6cfced137a2b4539131c4ad405ec76f6384a28
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-09-03 19:39:34 +0300

    Update LoginError.tsx

--- src/components/Errors/LoginError/LoginError.tsx
+++ src/components/Errors/LoginError/LoginError.tsx
@@ -1,24 +1,15 @@
 import { Box } from "../../Box/Box";
-import LinkButton from "../../Buttons/LinkButton";
 import { ErrorText } from "../ErrorText.styled";
-
+import LinkButton from "../../Buttons/LinkButton";
 
 interface IProps {
-    errorMessage: string;
-    // resetError: () => void
-  }
-  
-  function LoginError({ errorMessage }: IProps) {
-    // const { setModalToggle } = useGlobalState();
-  
-    // const errorMessage = getMessageForError(error.message);
-  
-    const onCancelError = () => {
-    //   resetError();
-    }
-    return (
-      <>
-        <ErrorText as="p" mb={30} color="#10b981" fontSize="20px">
+  errorMessage: string;
+}
+
+function LoginError({ errorMessage }: IProps) {
+  return (
+    <>
+      <ErrorText as="p" mb={30} color="#10b981" fontSize="20px">
           There was a problem
         </ErrorText>
   
@@ -29,19 +20,12 @@ interface IProps {
         <Box display="flex" justifyContent="center" >
           <LinkButton href="/login" text="Go back" maxWidth="180px" />
         </Box>
-  
-        {/* <ErrorText as="p" color="#a1a1aa" fontSize={["m"]}>
-          {errorMessage.text}
-        </ErrorText> */}
-  
-        {/* <ButtonWrapper>
-          <CancelButton
-            cancelText="CANCEL"
-            onClick={onCancelError}
-          />
-        </ButtonWrapper> */}
-      </>
-    );
-  }
-  
-  export default LoginError;
+
+        <Box display="flex" justifyContent="center" mt={4} >
+          <LinkButton href="/register" text="Registration" maxWidth="180px" />
+        </Box>
+    </>
+  );
+}
+
+export default LoginError;
