9e41cc23d8f87503331c855def33d1ef4b815ea1
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-09-06 20:38:42 +0300

    Create LoginError.styled.tsx

---
+++ src/components/Errors/LoginError/LoginError.styled.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import styled from "styled-components";
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 20px;
+`;
