0ea24dfc1e259a0a48cd835ba30060515f6bc009
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-09-13 21:03:13 +0300

    Update LoginError.tsx

--- src/components/Errors/LoginError/LoginError.tsx
+++ src/components/Errors/LoginError/LoginError.tsx
@@ -1,34 +1,35 @@
-import { Box } from "../../Box/Box";
+import { Dispatch, SetStateAction } from "react";
 import { ErrorText } from "../ErrorText.styled";
 import LinkButton from "../../Buttons/LinkButton";
 import { ButtonWrapper } from "./LoginError.styled";
+import EnterButton from "../../Buttons/EnterButton";
 
 interface IProps {
   errorMessage: string;
+  resetError: Dispatch<SetStateAction<string>>;
 }
 
-function LoginError({ errorMessage }: IProps) {
+function LoginError({ errorMessage, resetError }: IProps) {
   return (
     <>
       <ErrorText as="p" mb={30} color="#10b981" fontSize="20px">
-          There was a problem
-        </ErrorText>
+        There was a problem
+      </ErrorText>
   
-        <ErrorText as="p" mb={30} color="primaryBtn" fontSize="ml" fontWeight="bold">
-          {errorMessage}
-        </ErrorText>
+      <ErrorText as="p" mb={30} color="primaryBtn" fontSize="ml" fontWeight="bold">
+        {errorMessage}
+      </ErrorText>
 
-        {/* <Box display="flex" justifyContent="center" > */}
-        {/* </Box> */}
-
-        <ButtonWrapper>
-          <LinkButton href="/login" text="Go back" maxWidth="180px" />
-
-          <LinkButton href="/register" text="Registration" maxWidth="180px" />
-
-        </ButtonWrapper>
-        {/* <Box display="flex" justifyContent="center" mt={4} > */}
-        {/* </Box> */}
+      <ButtonWrapper>
+        {/* <LinkButton href="/login" text="Go back" maxWidth="180px" /> */}
+        <EnterButton
+          height={50}
+          maxWidth="180px"
+          enterText="Try again"
+          onClick={() => resetError("")}
+        />
+        <LinkButton href="/register" text="Registration" maxWidth="180px" />
+      </ButtonWrapper>
     </>
   );
 }
