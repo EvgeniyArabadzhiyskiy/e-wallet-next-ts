f96647c060bd7cd3b4447a2a8c860d996cc4015e
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-10-05 22:17:09 +0300

    Delete AuthServerProvider.tsx

--- src/components/AuthServerProvider.tsx
+++
@@ -1,9 +0,0 @@
-import { getServerSession } from "next-auth";
-import { authOptions } from "../lib/auth";
-
-export default async function AuthServerProvider() {
-    const session = await getServerSession(authOptions);
-
-    return <></>
-    
-}
