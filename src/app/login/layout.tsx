a3d71593292d5f001db00f7eb954e15f9630423f
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-10-05 22:16:57 +0300

    Update layout.tsx

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,7 +1,6 @@
 import LoginPageLayout from "@/src/components/LoginPageLayout/LoginPageLayout";
 
 import { Metadata } from "next";
-// import { Container } from "@/components/Container/Container";
 
 export const metadata: Metadata = {
   title: "Login Page",
@@ -11,13 +10,3 @@ export const metadata: Metadata = {
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <LoginPageLayout>{children}</LoginPageLayout>;
 }
-
-// export default function Layout({ children }: { children: React.ReactNode }) {
-//   return (
-//     <Section>
-//       <Container>
-//         <Wrapper>{children}</Wrapper>
-//       </Container>
-//     </Section>
-//   );
-// }
