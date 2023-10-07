02589d2be2d168ee099b80d5db6ca18ebe66ce83
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-10-07 21:48:55 +0300

    Update deleteTransaction.ts

--- src/apiWallet/transaction/deleteTransaction.ts
+++ src/apiWallet/transaction/deleteTransaction.ts
@@ -3,14 +3,14 @@ import { TRANSACTIONS } from "../../constants/apiPath";
 import { RemoveTransaction } from "../../types/transactions";
 
 export const deleteTransaction = async (id: string, token: string | undefined) => {
-    const options: RequestInit = {
-      method: "DELETE",
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    };
-  
-    const data = await fetcher<RemoveTransaction>(`${TRANSACTIONS}/${id}`, options);
-  
-    return data;
+  const options: RequestInit = {
+    method: "DELETE",
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  };
+
+  const data = await fetcher<RemoveTransaction>(`${TRANSACTIONS}/${id}`, options);
+
+  return data;
 };
