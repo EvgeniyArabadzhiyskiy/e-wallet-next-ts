87a47de581098f080e4764764fce8310de83ae32
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-10-11 21:27:10 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,5 @@
-import HeroSection from "../components/HeroSection/HeroSection";
+import HeroSection from "../components/HeroSection";
 
 export default function Home() {
   return <HeroSection />;
-}
-
-{
-  /* <div>
-        <div>
-          <Link href="/home">HOME</Link> <br />
-          <div style={{ marginTop: 20 }}></div>
-          <Link href="/about">About</Link> <br />
-          <div style={{ marginTop: 20 }}></div>
-          <Link href="/currency">HYDRATE</Link> <br />
-          <div style={{ marginTop: 20 }}></div>
-          <Link href="/login">LOGIN</Link>
-        </div>
-      </div> */
 }
