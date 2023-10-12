b86c2832ff4b8a08e9e17736cd2586fef6fa6a2b
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-10-12 21:37:26 +0300

    Update Balance.tsx

--- src/components/Balance/Balance.tsx
+++ src/components/Balance/Balance.tsx
@@ -1,20 +1,14 @@
 "use client";
 
-import { useQueryClient } from "@tanstack/react-query";
-import { Wrapper, CurrencySign, Text, Title } from "./Balance.styled";
 import { useUserBalance } from "@/src/apiWallet";
+import { Wrapper, Text, Title } from "./Balance.styled";
 
 export default function Balance() {
   const { data = 0 } = useUserBalance();
 
-  // const queryClient = useQueryClient();
-  // const Balance = queryClient.getQueriesData<any>(["Balance"]);
-  // console.log(" Balance:", Balance[0]);
-
   return (
     <Wrapper>
       <Title>Your balance</Title>
-      {/* <CurrencySign>₴ </CurrencySign> */}
       <Text>{data.toFixed(2)}</Text>
     </Wrapper>
   );
