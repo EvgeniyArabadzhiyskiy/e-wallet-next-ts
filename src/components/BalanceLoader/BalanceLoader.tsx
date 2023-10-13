7a81da4cc32e108c463fa33a6fb6431fe71971f7
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-10-13 14:25:16 +0300

    Update BalanceLoader.tsx

--- src/components/BalanceLoader/BalanceLoader.tsx
+++ src/components/BalanceLoader/BalanceLoader.tsx
@@ -6,6 +6,7 @@ import stl from "./BalanceLoader.module.scss";
 function BalanceLoader() {
   const placeholderBalance = -87710.0;
   return (
+    // или styled component BalanceLoader
     // <Wrapper>
     //   <Title>Your balance</Title>
     //   <Loader>{placeholderBalance.toFixed(2)}</Loader>
