befb6c6b341c6a6e7f096ee88929bebccd69aa84
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-10-19 21:26:18 +0300

    Update CustomSelect.tsx

--- src/components/CustomSelect/CustomSelect.tsx
+++ src/components/CustomSelect/CustomSelect.tsx
@@ -1,16 +1,11 @@
-import ReactSelect, {
-  GroupBase,
-  SingleValue,
-  // Props,
-} from "react-select";
-import { Props } from "react-select/dist/declarations/src/Select";
-// import Select from "react-select/dist/declarations/src/Select";
-import { optionsExpense, optionsIncome } from "@/src/constants/selectOptions";
-import { selectStyles } from "@/src/styles/selectStyles";
-import { Dispatch,  SetStateAction, useMemo, useState } from "react";
-import { OptionType } from "@/src/types/optionType";
 import { FormikErrors } from "formik";
+import { Dispatch,  SetStateAction } from "react";
+import ReactSelect, { SingleValue } from "react-select";
+
+import { selectStyles } from "@/src/styles/selectStyles";
+import { OptionType } from "@/src/types/optionType";
 import { ITransactionValue } from "@/src/types/transactionValue";
+import { optionsExpense, optionsIncome } from "@/src/constants/selectOptions";
 
 interface IProps {
   isIncome: boolean;
@@ -22,61 +17,11 @@ interface IProps {
     value: any,
     shouldValidate?: boolean | undefined
   ) => Promise<void | FormikErrors<ITransactionValue<string>>>;
-
-  // selectRef: RefObject<Select<OptionType, false, GroupBase<OptionType>>>;
 }
 
-function isOptionData(data: any): data is OptionType {
-  return (
-    data !== null &&
-    typeof data === "object" &&
-    typeof data.value === "string" &&
-    typeof data.label === "string"
-  );
-}
-
-// export default function SelectComponent({
-//   isIncome,
-//   setFieldValue,
-//   selectRef,
-// }: IProps) {
-
-//   const onSelectChange = (data: unknown, eee) => {
-//     console.log("onSelectChange  eee:", eee);
-
-//     if (isOptionData(data)) {
-//       setFieldValue("category", data.label);
-//     }
-//   };
-
-//   return (
-//     <ReactSelect
-//       ref={selectRef}
-//       name="category"
-//       options={isIncome ? optionsIncome : optionsExpense}
-//       placeholder="Select a category"
-//       onChange={onSelectChange}
-//       styles={selectStyles}
-//     />
-//   );
-// }
-
-export default function CustomSelect(
-  // <Option = OptionType,
-  //   IsMulti extends boolean = false,
-  //   Group extends GroupBase<Option> = GroupBase<Option>>
-  {
-    value,
-    setValue,
-    isIncome,
-    setCategory,
-    //  setFieldValue, selectRef,
-    // ...props
-  }: IProps
-) {
+export default function CustomSelect({ value, setValue, isIncome, setCategory }: IProps) {
 
   const onChange = (option: SingleValue<OptionType>) => {
-    
     if (option) {
       setCategory("category", option.label)
       setValue({
