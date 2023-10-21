1843d95bf9558de9af5be892ec25e80c8434c3e7
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-10-21 22:09:47 +0300

    Update FilterDate.tsx

--- src/components/FilterDate/FilterDate.tsx
+++ src/components/FilterDate/FilterDate.tsx
@@ -1,5 +1,5 @@
 import { Dispatch, SetStateAction } from "react";
-import DateInput from "./DateInput/DateInput";
+import DateInput from "./DateInput";
 import { StyledFilters } from "./FilterDate.styled";
 
 interface IProps {
