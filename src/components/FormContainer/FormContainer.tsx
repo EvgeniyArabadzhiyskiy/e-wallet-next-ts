26a233d056d7bb29f6caaa6c4d04c8f4386f6797
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-10-25 19:56:54 +0300

    Update FormContainer.tsx

--- src/components/FormContainer/FormContainer.tsx
+++ src/components/FormContainer/FormContainer.tsx
@@ -1,8 +1,9 @@
-import { ValidationSchema } from "@/src/helpers/formValidation";
+import { Form, Formik, FormikProps, FormikHelpers } from "formik";
+
 import { ILoginValues } from "@/src/types/loginValues";
 import { IRegisterValues } from "@/src/types/registerValues";
 import { ITransactionValue } from "@/src/types/transactionValue";
-import { Form, Formik, FormikProps, FormikHelpers, FormikValues } from "formik";
+import { ValidationSchema } from "@/src/helpers/formValidation";
 
 type FormValues = ILoginValues | IRegisterValues | ITransactionValue 
 
@@ -26,107 +27,3 @@ function FormContainer <T extends FormValues>(props: IProps<T>) {
 }
 
 export default FormContainer;
-
-//========================================================
-// const LoginFormContainer: React.FC<IProps<ILoginValues>> = ({
-//   initialValues,
-//   validationSchema,
-//   onSubmit,
-//   render,
-// }) => {
-//   return (
-//     <Formik
-//       onSubmit={onSubmit}
-//       initialValues={initialValues}
-//       validationSchema={validationSchema}
-//     >
-//       {(formik) => <Form>{render(formik)}</Form>}
-//     </Formik>
-//   );
-// };
-
-// const RegisterFormContainer: React.FC<IProps<IRegisterValues>> = ({
-//   initialValues,
-//   validationSchema,
-//   onSubmit,
-//   render,
-// }) => {
-//   return (
-//     <Formik
-//       onSubmit={onSubmit}
-//       initialValues={initialValues}
-//       validationSchema={validationSchema}
-//     >
-//       {(formik) => <Form>{render(formik)}</Form>}
-//     </Formik>
-//   );
-// };
-
-// const TransactionFormContainer: React.FC<IProps<ITransactionValue>> = ({
-//   initialValues,
-//   validationSchema,
-//   onSubmit,
-//   render,
-// }) => {
-//   return (
-//     <Formik
-//       onSubmit={onSubmit}
-//       initialValues={initialValues}
-//       validationSchema={validationSchema}
-//     >
-//       {(formik) => <Form>{render(formik)}</Form>}
-//     </Formik>
-//   );
-// };
-
-// export { LoginFormContainer, RegisterFormContainer, TransactionFormContainer };
-
-//======================================================
-// interface IProps<T> {
-//   initialValues: T;
-//   validationSchema: any;
-//   onSubmit: (values: T, formikHelpers: FormikHelpers<T>) => Promise<void>;
-//   render: (formik: FormikProps<T>) => React.ReactNode;
-// }
-
-// const FormContainer: React.FC<IProps<any>> = ({
-//   initialValues,
-//   validationSchema,
-//   onSubmit,
-//   render,
-// }) => {
-//   return (
-//     <>
-//       <Formik
-//         onSubmit={onSubmit}
-//         initialValues={initialValues}
-//         validationSchema={validationSchema}
-//       >
-//         {(formik) => <Form>{render(formik)}</Form>}
-//       </Formik>
-//     </>
-//   );
-// };
-
-// export default FormContainer;
-
-//======================================================
-// import axios, { AxiosResponse } from "axios";
-
-// interface IUser {
-//   name: string;
-//   age: number;
-//   city: string;
-// }
-
-// async function fetchData(): Promise<IUser> {
-//   try {
-//     const response: AxiosResponse<IUser> = await axios.get("https://example.com/api/data");
-//     const user: IUser = response.data;
-//     return user;
-//   } catch (error) {
-//     // Обработка ошибки
-//     console.error("Error fetching data:", error);
-//     throw error; // Возможно, выбросить ошибку для дальнейшей обработки
-//   }
-// }
