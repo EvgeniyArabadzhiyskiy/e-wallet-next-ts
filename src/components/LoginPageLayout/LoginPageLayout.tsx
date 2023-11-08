0810eca648f28c39a66e27da8e5b9d5cbd51e1df
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-11-08 23:20:25 +0200

    Update LoginPageLayout.tsx

--- src/components/LoginPageLayout/LoginPageLayout.tsx
+++ src/components/LoginPageLayout/LoginPageLayout.tsx
@@ -1,4 +1,4 @@
-import Container from "../Container/Container";
+import Container from "../Container";
 import stl from "./LoginPageLayout.module.scss";
 
 export default function LoginPageLayout({
@@ -10,10 +10,6 @@ export default function LoginPageLayout({
     <>
       <div className={stl.background}>
         <h1 className={stl.background__title}>Wallet App</h1>
-        {/* <div className={[stl.circle, stl.circle__1].join(" ")}></div>
-        <div className={[stl.circle, stl.circle__2].join(" ")}></div>
-        <div className={[stl.circle, stl.circle__3].join(" ")}></div>
-        <div className={[stl.circle, stl.circle__4].join(" ")}></div> */}
       </div>
       <div className={stl.section}>
         <Container>
