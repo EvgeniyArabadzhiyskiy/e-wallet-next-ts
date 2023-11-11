d4f8b68fe17aedd72194f9139cfb40928ac1fe33
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-11-11 21:36:06 +0200

    Update Logo.tsx

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -3,11 +3,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { usePathname } from "next/navigation";
-
-import logo from "../../../public/images/logo.png";
 import { LogoText, LogoWrap, ImgWrapper } from "./Logo.styled";
-
-import { Media } from "@/src/lib/media";
 
 function Logo() {
   const pathName = usePathname();
@@ -15,36 +11,13 @@ function Logo() {
 
   return (
     <Link href="/">
+      <LogoWrap>
+        <ImgWrapper>
+          <Image src="/images/logo.png" alt="logo" width={40} height={40} />
+        </ImgWrapper>
 
-        <LogoWrap>
-          <ImgWrapper>
-            <Image  src="/images/logo.png" alt="logo" width={40} height={40}    />
-          </ImgWrapper>
-
-          <LogoText $isLoginPage={isLoginPage}>Wallet</LogoText>
-          
-        </LogoWrap>
-      {/* {isDesctop && (  
-        <Image style={{marginRight: 16}} src={logoBig} width={40} height={40} alt="logo" /> 
-        <div style={{width: "200px", height: "50px", background: "tomato"}} ></div>
-        <Statistic />
-        )}   */}
-
-      {/* {isMobile && (
-          <Image style={{marginRight: 16}} src={logo} width={30} height={30} alt="logo"  />
-          <div style={{width: "200px", height: "50px", background: "blue"}} ></div>
-        )} */}
-
-      {/* <Media at="sm">
-          <Image style={{marginRight: 16}} src={logo} width={30} height={30} alt="logo"  />
-          <div style={{width: "200px", height: "50px", background: "blue"}} ></div>
-          <Statistic  />
-        </Media> */}
-
-      {/* <Media greaterThan="sm">
-          <Image style={{marginRight: 16}} src={logoBig} width={40} height={40} alt="logo" />
-          <div style={{width: "200px", height: "50px", background: "tomato"}} ></div>
-        </Media> */}
+        <LogoText $isLoginPage={isLoginPage}>Wallet</LogoText>
+      </LogoWrap>
     </Link>
   );
 }
