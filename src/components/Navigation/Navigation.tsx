28294ce9e9550980d830d800b35e907dca2c7336
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-11-19 22:08:26 +0200

    Update Navigation.tsx

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -10,7 +10,7 @@ export default function Navigation() {
   const path = usePathname();
 
   return (
-    <>
+    <nav>
       <List>
         <ItemLink>
           <NavLink href="/home" $isActive={path === "/home"}>
@@ -31,36 +31,6 @@ export default function Navigation() {
           </NavLink>
         </ItemLink>
       </List>
-    </>
+    </nav>
   );
 }
-
-// const web = {
-//   test: /\.svg$/,
-//   use: [{ loader: "@svgr/webpack", options: { native: true } }],
-// };
-
-// import Link from "next/link";
-// import stl from "./Navigation.module.scss";
-// import { useParams, usePathname, useSelectedLayoutSegment } from "next/navigation";
-
-// export default function Navigation() {
-//   const path = usePathname()
-
-//   return (
-//     <>
-//       <ul className={stl.desctop__nav__list}>
-//         <li>
-//           <Link  className={`${stl.desctop__navlink} ${path === "/home" ? stl.active : ''}`} href="/home">
-//             <p className={stl.desctop__navlink__text}>Home</p>
-//           </Link>
-//         </li>
-//         <li>
-//           <Link className={`${stl.desctop__navlink} ${path === "/about" ? stl.active : ''}`} href="/about">
-//             <p className={stl.desctop__navlink__text}>Statistic</p>
-//           </Link>
-//         </li>
-//       </ul>
-//     </>
-//   );
-// }
