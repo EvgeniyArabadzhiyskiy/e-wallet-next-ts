51c36d5afe29f27c4eca599c1a4d8d36414d19be
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2023-11-26 23:59:34 +0200

    Update RegistrationForm.tsx

--- src/components/RegistrationForm/RegistrationForm.tsx
+++ src/components/RegistrationForm/RegistrationForm.tsx
@@ -3,25 +3,27 @@
 import { useState } from "react";
 import { FormikHelpers } from "formik";
 import { signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import { useScaleForm } from "@/src/hooks/useScaleForm";
 import { ICredentials, IRegisterValues } from "@/src/types/registerValues";
 
-import Logo from "../Logo/Logo";
-import { Title } from "../Title/Title.styled";
-import { FormWrap } from "../LoginForm/LoginForm.styled";
-import FormContainer from "../FormContainer/FormContainer";
-import RegisterFormFields from "../RegisterFormFields/RegisterFormFields";
-import { registerSchema } from "@/src/helpers/formValidation";
-import { register } from "@/src/apiWallet/user";
 import { Box } from "../Box/Box";
+import { register } from "@/src/apiWallet/user";
+import { registerSchema } from "@/src/helpers/formValidation";
+import { FormWrap } from "../LoginForm/LoginForm.styled";
+
+import Logo from "../Logo";
 import AuthError from "../Errors/AuthError";
+import FormContainer from "../FormContainer";
+import RegisterFormFields from "../RegisterFormFields";
 
 
 export default function RegistrationForm() {
+  const router = useRouter();
+
   const isScale = useScaleForm();
   const [isLoading, setIsLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('')
-  // console.log("RegistrationForm  error:", errorMessage);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const initialValues: IRegisterValues = {
     email: "",
@@ -39,15 +41,17 @@ export default function RegistrationForm() {
     setIsLoading(true);
 
     try {
-      const newUser = await register(userCredentials)
-      console.log("RegistrationForm  resulrRegistration:", newUser.user.email);
+      const newUser = await register(userCredentials);
 
       const response = await signIn('credentials', {
         email: email,
         password: password,
-        redirect: true,
-        callbackUrl: '/home/transactions'
+        redirect: false,
+        // redirect: true,
+        // callbackUrl: '/home/transactions'
       });
+
+      router.push('/home/transactions');
     } catch (error) {
       const message = (error as Error).message
 
@@ -64,29 +68,12 @@ export default function RegistrationForm() {
     } finally {
       setIsLoading(false);
     }
-    
-
-    // setIsLoading(true);
-
-   
-
-    // setIsLoading(false);
-
+  
     resetForm();
   };
 
   return (
     <FormWrap $isScale={isScale}>
-      {isLoading && <h1>Loading...</h1>}
-      {/* <Title
-        as="h2"
-        mb={5}
-        color="expense"
-        fontSize={["l"]}
-        textAlign="center"
-      >
-        Register Page
-      </Title> */}
       <Box mb={60}>
         <Logo />
       </Box>
@@ -111,66 +98,3 @@ export default function RegistrationForm() {
   );
 }
 
-
-
-
-// function ddd(x: any) {
-//   if (x === 20) {
-//     console.log('Hello');
-//   }
-//   else {
-//     console.log("Djon");
-//   }
-// }
-// ddd(230)
-
-// export default function RegistrationForm() {
-//   const isScale = useScaleForm();
-//   const [isLoading, setIsLoading] = useState(false);
-
-//   const initialValues: IRegisterValues = {
-//     email: "",
-//     password: "",
-//     confirmPassword: "",
-//     name: "",
-//   };
-
-//   const handleSubmit = async (
-//     values: IRegisterValues,
-//     { resetForm }: FormikHelpers<IRegisterValues>
-//   ) => {
-//     setIsLoading(true);
-
-//     try {
-//       const user = await signIn("credentials", {
-//         email: values.email,
-//         password: values.password,
-//         confirmPassword: values.confirmPassword,
-//         name: values.name,
-//         redirect: false,
-//       });
-
-//       resetForm();
-//     } catch (error) {
-//       console.log("RegistrationForm  error:", error);
-//       // Обработка ошибок
-//     } finally {
-//       setIsLoading(false);
-//     }
-//   };
-
-//   return (
-//     <FormWrap $isScale={isScale}>
-//       {isLoading && <Spinner />}
-//       <Title as="h2" mb={5} color="expense" fontSize={["ml"]} textAlign="center">Register Page</Title>
-//       <Logo />
-
-//       <FormContainer
-//         onSubmit={handleSubmit}
-//         initialValues={initialValues}
-//         validationSchema={schema.register}
-//         render={(formik: FormikProps<IRegisterValues>) => <RegisterFormFields formik={formik} />}
-//       />
-//     </FormWrap>
-//   );
-// }
