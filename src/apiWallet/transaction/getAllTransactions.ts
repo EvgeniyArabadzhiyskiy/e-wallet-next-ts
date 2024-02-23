fb4940a21c099b329866f09771094a0a3eb5d368
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-02-23 21:21:14 +0200

    Update getAllTransactions.ts

--- src/apiWallet/transaction/getAllTransactions.ts
+++ src/apiWallet/transaction/getAllTransactions.ts
@@ -2,8 +2,59 @@ import { fetcher } from "../../helpers/fetcher";
 import { TRANSACTIONS } from "../../constants/apiPath";
 import { ITransactions } from "../../types/transactions";
 import { isITransactions } from "../../helpers/isITransactions";
+// import { cookies } from "next/headers";
+// import { verifyToken } from "@/src/helpers/verifyToken";
+// import { TRPCError } from "@trpc/server";
+// import prisma from "../../lib/prismaClient";
 
-export const getAllTransactions = 
+// export const getAllTransactions = async (
+//   limit: number = 10,
+//   skip: number = 0
+// ) => {
+//   const nextCookies = cookies();
+//   const userID = await verifyToken(nextCookies);
+
+//   if (!userID) {
+//     throw new TRPCError({ code: "UNAUTHORIZED" });
+//   }
+
+//   const transactions = await prisma.transaction.findMany({
+//     where: {
+//       owner: {
+//         equals: userID,
+//       },
+//     },
+//     orderBy: [{ timestamps: "desc" }, { createdAt: "desc" }],
+//     skip,
+//     take: limit,
+//   });
+
+//   const user = await prisma.user.findFirst({
+//     where: {
+//       id: userID,
+//     },
+//   });
+
+//   if (!user) {
+//     throw new TRPCError({ code: "UNAUTHORIZED" });
+//   }
+
+//   const filtred = transactions.map((el) => {
+//     const { updatedAt, id, ...props } = el;
+//     const res = {
+//       _id: id,
+//       ...props,
+//     };
+//     return res;
+//   });
+
+//   return {
+//     transactions: filtred,
+//     balance: user.balance,
+//   };
+// };
+
+export const getAllTransactions =
 async (token: string | undefined, pageNum: number) => {
   // await new Promise((res) => setTimeout(() => res(console.log("Promise resolve")), 1000));
 
@@ -14,15 +65,14 @@ async (token: string | undefined, pageNum: number) => {
     },
     // cache: "no-store",
   };
-  
+
   try {
     const data = await fetcher<ITransactions>
     (`${TRANSACTIONS}?page=${pageNum}&limit=10`, options);
-    
+
     if (!isITransactions(data)) {
       throw new Error("Invalid data format");
     }
-    // console.log("data:", data);
 
     return data;
 
