140a93ee977dc4d5b1b2101099f785a79da6f5c1
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-02-23 21:21:20 +0200

    Update useEditTransaction.ts

--- src/apiWallet/useEditTransaction.ts
+++ src/apiWallet/useEditTransaction.ts
@@ -8,26 +8,27 @@ import { ChangedTransaction, ITransactions } from "../types/transactions";
 import { ITransactionData } from "../types/transactionValue";
 import { editTransaction } from "./transaction";
 import { useGlobalState } from "../components/GlobalProvider/GlobalProvider";
+import { trpc } from "../trpc/client";
 
-export const useEditTransaction = (editId: string) => {
-  const { token } = useUser();
+
+export const useEditTransaction = () => {
   const queryClient = useQueryClient();
   const { setModalToggle } = useGlobalState();
 
-  const mutation = useMutation<ChangedTransaction, Error, ITransactionData>({
-    mutationFn: (transaction) => editTransaction(editId, transaction, token),
-
+  const mutation = trpc.transactionRouter.editTransaction.useMutation({
     onSuccess: (data) => {
+
       queryClient.setQueryData<InfiniteData<ITransactions>>(
-        ["TransactionsList"],
+        [["transactionRouter", "getAllTransactions"],{ input: { limit: 10 }, type: "infinite" }],
         (prev) => {
+          console.log("useEditTransaction  prev:", prev);
           if (!prev) {
             return undefined;
           }
 
           const updatedPages = prev.pages.map((page) => {
             const newCache = page.transactions.map((transaction) => {
-              if (transaction._id === data._id) {
+              if (transaction.id === data.id) {
                 return data;
               }
 
@@ -52,7 +53,56 @@ export const useEditTransaction = (editId: string) => {
 
       setModalToggle("transaction");
     },
-  });
+  })
 
   return mutation;
 };
+
+
+// export const useEditTransaction = (editId: string) => {
+//   const { token } = useUser();
+//   const queryClient = useQueryClient();
+//   const { setModalToggle } = useGlobalState();
+
+//   const mutation = useMutation<ChangedTransaction, Error, ITransactionData>({
+//     mutationFn: (transaction) => editTransaction(editId, transaction, token),
+
+//     onSuccess: (data) => {
+//       queryClient.setQueryData<InfiniteData<ITransactions>>(
+//         ["TransactionsList"],
+//         (prev) => {
+//           if (!prev) {
+//             return undefined;
+//           }
+
+//           const updatedPages = prev.pages.map((page) => {
+//             const newCache = page.transactions.map((transaction) => {
+//               if (transaction._id === data._id) {
+//                 return data;
+//               }
+
+//               return transaction;
+//             });
+
+//             return {
+//               ...page,
+//               transactions: newCache,
+//             };
+//           });
+
+//           return {
+//             ...prev,
+//             pages: updatedPages,
+//           };
+//         }
+//       );
+
+//       queryClient.invalidateQueries({ queryKey: ["Balance"] });
+//       queryClient.invalidateQueries({ queryKey: ["Statistics"] });
+
+//       setModalToggle("transaction");
+//     },
+//   });
+
+//   return mutation;
+// };
