85b3fb61a91901325b322b0fc5b2df689b76c8e0
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-02-28 22:22:32 +0200

    Update useStatistic.ts

--- src/apiWallet/useStatistic.ts
+++ src/apiWallet/useStatistic.ts
@@ -3,38 +3,63 @@ import { useUser } from "../hooks/useUser";
 import { useQuery } from "@tanstack/react-query";
 import { getStatistics } from "./statistic";
 import { IStatistic } from "../types/statistics";
+import { trpc } from "../trpc/client";
 
 export const useStatistic = () => {
   const { token } = useUser();
   const [year, setYear] = useState("");
   const [month, setMonth] = useState("");
 
-  const queryData = useQuery<IStatistic[], Error, IStatistic[]>({
-    queryKey: ["Statistics", { month, year }],
-    queryFn: () => getStatistics(token, { month, year }),
-    staleTime: Infinity,
-    refetchOnWindowFocus: false,
-    retry: 0,
-    enabled: !!token,
-  });
+  const queryData = trpc.statisticRouter.getStatistic.useQuery(undefined, {
+    queryKey: ["statisticRouter.getStatistic", undefined],
+    // cacheTime: 0,
+  
+  })
+
+  // const queryData = useQuery<IStatistic[], Error, IStatistic[]>({
+  //   queryKey: ["Statistics"],
+  //   queryFn: async () => {
+  //     const res = await fetch("/api/statistic");
+  //     const data = await res.json()
+  //     console.log("queryFn:  data:", data);
+
+  //     return data
+  //   },
+  //   cacheTime: 0,
+  //   // staleTime: Infinity,
+  //   // refetchOnWindowFocus: false,
+  //   // retry: 0,
+    
+  // });
+
+ 
 
   const { data = [] } = queryData;
 
-  const expensesData = useMemo(
-    () => data.filter(({ type }) => type === "expense"),
-    [data]
-  );
+  // const expensesData = useMemo(
+  //   () => data.filter(({ type }) => type === "expense"),
+  //   [data]
+  // );
 
-  const expensesTotal = useMemo(
-    () => expensesData.reduce((acc, item) => acc + item.totalSum, 0),
-    [expensesData]
-  );
+  // const expensesTotal = useMemo(
+  //   () => expensesData.reduce((acc, item) => acc + item.totalSum, 0),
+  //   [expensesData]
+  // );
 
-  const incomeTotal = useMemo(() => data
-    .filter(({ type }) => type === "income")
-    .reduce((acc, item) => acc + item.totalSum, 0),
-    [data]
-  );
+  // const incomeTotal = useMemo(() => data
+  //   .filter(({ type }) => type === "income")
+  //   .reduce((acc, item) => acc + item.totalSum, 0),
+  //   [data]
+  // );
+
+  const expensesData =  data.filter(({ type }) => type === "expense");
+  const expensesTotal = expensesData.reduce((acc, item) => acc + item.totalSum, 0);
+
+  const incomeTotal = data
+  .filter(({ type }) => type === "income")
+  .reduce((acc, item) => acc + item.totalSum, 0);
+
+  
 
   const result = {
     ...queryData,
