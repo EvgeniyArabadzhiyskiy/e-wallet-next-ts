a51ac806df4235be2a8931103d2211bcb2cabd51
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-01 22:10:46 +0200

    Update getBalance.ts

--- src/apiWallet/balance/getBalance.ts
+++ src/apiWallet/balance/getBalance.ts
@@ -1,28 +1,44 @@
 import { fetcher } from "../../helpers/fetcher";
 import { IBalance } from "../../types/transactions";
 import { BALANCE } from "../../constants/apiPath";
+import prisma from "../../lib/prismaClient";
+import { TRPCError } from "@trpc/server";
 
-export const getBalance = async (authToken: string | undefined) => {
-
-  const options: RequestInit = {
-    method: "GET",
-    headers: {
-      Authorization: `Bearer ${authToken}`,
+export const getBalance = async (userID: string) => {
+  const user = await prisma.user.findFirst({
+    where: {
+      id: userID,
     },
-    cache: "no-store",
-  };
+  });
 
-  try {
-    const data = await fetcher<IBalance>(`${BALANCE}`, options);
-
-    if (typeof data !== "object" || typeof data.userBalance !== "number") {
-      throw new Error("Invalid data format");
-    }
-
-    return data;
-    
-  } catch (error) {
-    console.log("Error:", (error as Error).message);
-    throw error;
+  if (!user) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
   }
+
+  return { userBalance: user.balance };
 };
+
+// export const getBalance = async (authToken: string | undefined) => {
+
+//   const options: RequestInit = {
+//     method: "GET",
+//     headers: {
+//       Authorization: `Bearer ${authToken}`,
+//     },
+//     cache: "no-store",
+//   };
+
+//   try {
+//     const data = await fetcher<IBalance>(`${BALANCE}`, options);
+
+//     if (typeof data !== "object" || typeof data.userBalance !== "number") {
+//       throw new Error("Invalid data format");
+//     }
+
+//     return data;
+
+//   } catch (error) {
+//     console.log("Error:", (error as Error).message);
+//     throw error;
+//   }
+// };
