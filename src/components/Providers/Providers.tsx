0c0964d6995b27b71c667f1b754e8f90f9fce741
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-03 21:31:25 +0200

    Update Providers.tsx

--- src/components/Providers/Providers.tsx
+++ src/components/Providers/Providers.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { MediaContextProvider } from "@/src/lib/media";
-import { SessionProvider } from "next-auth/react";
+// import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "styled-components";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useGlobalState } from "../GlobalProvider/GlobalProvider";
@@ -23,8 +23,8 @@ function Providers({ children }: IProps) {
             staleTime: Infinity,
             cacheTime: Infinity,
 
-            refetchOnMount: false,
-            refetchOnReconnect: false,
+            // refetchOnMount: false,    // Не обновляло данные при переходе на /statistic
+            // refetchOnReconnect: false,
             refetchOnWindowFocus: false,
             retry: 0,
           },
@@ -58,7 +58,7 @@ function Providers({ children }: IProps) {
         <MediaContextProvider disableDynamicMediaQueries>
           <trpc.Provider client={trpcClient} queryClient={queryClient}>
             <QueryClientProvider client={queryClient}>
-              <SessionProvider>{children}</SessionProvider>
+              {children}
             </QueryClientProvider>
           </trpc.Provider>
         </MediaContextProvider>
