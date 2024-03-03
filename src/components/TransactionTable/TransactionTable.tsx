835075da2cef95161718d61c8e34703512f67b61
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-03 21:34:55 +0200

    Update TransactionTable.tsx

--- src/components/TransactionTable/TransactionTable.tsx
+++ src/components/TransactionTable/TransactionTable.tsx
@@ -2,22 +2,43 @@
 
 import { useEffect, useState } from "react"
 import { useBalanceList } from "@/src/hooks/useBalanceList";
-import { useLazyTransactions } from "@/src/apiWallet";
+import { useLazyTransactions, useRemoveTransaction, useStatistic } from "@/src/apiWallet";
 
-import TransactionItem from "./TransactionItem/TransactionItem";
-import ButtonAddTransactions from "../Buttons/ButtonAddTransactions/ButtonAddTransactions";
-import { Category, Table, TableBody, TableHeader } from "./TransactionTable.styled";
-import ModalBox from "../ModalWindow/ModalBox";
 import FlipCard from "../FlipCard";
+import ModalBox from "../ModalWindow";
 import SettingsSvg from "../SvgComponent/SettingsSvg";
-import ScrollToTop from "../ScrollToTop/ScrollToTop";
+import ScrollToTop from "../ScrollToTop";
+import TransactionItem from "./TransactionItem";
+import ButtonAddTransactions from "../Buttons/ButtonAddTransactions";
+import { Category, Table, TableBody, TableHeader } from "./TransactionTable.styled";
+import { InfiniteData, QueryClient, useQueryClient } from "@tanstack/react-query";
+import { ITransactions } from "@/src/types/transactions";
+import { trpc } from "@/src/trpc/client";
+import getQueryClient from "@/src/lib/getQueryClient";
 
 function TransactionTable() {
   const { data: allTransactions = [], listElem, observerElem, isError, error } = useLazyTransactions();
   const balanceList = useBalanceList(allTransactions);
-
+  
   const [editId, setEditId] = useState<string>("");
   const [modalKey, setModalKey] = useState<"ADD" | "EDIT">("ADD");
+
+  const [isDisabled, setIsDisabled] = useState<boolean>(false);
+  // const [isDis, setIsDis] = useState<boolean>(false);
+
+  const queryClient = useQueryClient();
+  // const cache = queryClient.getQueryCache()
+  // console.log("TransactionTable  cache:", cache.queries);
+
+//   useEffect(() => {
+//     (async () => {
+//       const res = await fetch(`http://localhost:3000/api/current-user`);
+
+//       const data = await res.json();
+// console.log("Data================", data);
+//     })()
+//   })
+
 
   if (isError) {
     throw new Error(error.message)
@@ -46,8 +67,10 @@ function TransactionTable() {
 
               return (
                 <TransactionItem
+                  isDisabled={isDisabled}
+                  setIsDisabled={setIsDisabled}
                   setEditId={setEditId}
-                  key={transaction._id}
+                  key={transaction.id}
                   balance={balance}
                   transaction={transaction}
                   setModalKey={setModalKey}
@@ -59,6 +82,16 @@ function TransactionTable() {
           </TableBody>
         )}
       </Table>
+      {/* <button onClick={() => {
+        // queryClient.removeQueries({ queryKey: ["Statistic"] });
+        // queryClient.removeQueries({ queryKey: [['statisticRouter', 'getStatistic'] ] });
+        // queryClient.removeQueries({ queryKey: [['transactionRouter', 'getBalance'] ] });
+
+        // queryClient.invalidateQueries({ queryKey: [['transactionRouter', 'getBalance']] });
+        // queryClient.invalidateQueries({ queryKey: [['statisticRouter', 'getStatistic']] });
+        // setIsDis(p => !p)
+        // console.log("REMOVE BUTTON", cache.queries);
+      }}>Delete Cache</button> */}
 
       <ButtonAddTransactions setModalKey={setModalKey} />
 
