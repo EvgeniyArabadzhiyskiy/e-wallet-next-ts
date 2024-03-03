96fba4d3f5361613878614f5aaaa20b1a02be9c5
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-03 21:28:21 +0200

    Update BalanceComponentWrapper.tsx

--- src/components/BalanceComponentWrapper/BalanceComponentWrapper.tsx
+++ src/components/BalanceComponentWrapper/BalanceComponentWrapper.tsx
@@ -1,55 +1,24 @@
-import { getServerSession } from "next-auth";
-import { Hydrate, dehydrate } from "@tanstack/react-query";
-// import { getBalance } from "@/src/apiWallet/balance";
-import { authOptions } from "@/src/lib/auth";
-import getQueryClient from "@/src/lib/getQueryClient";
 import Balance from "../Balance/Balance";
-import { cookies } from "next/headers";
-import { verifyToken } from "@/src/helpers/verifyToken";
-import { TRPCError } from "@trpc/server";
-import prisma from "../../lib/prismaClient"
+import getQueryClient from "@/src/lib/getQueryClient";
 
-const getBalance = async () => {
-  const nextCookies = cookies();
-  const userID = await verifyToken(nextCookies);
-
-  if (!userID) {
-    throw new TRPCError({ code: "UNAUTHORIZED" });
-  };
-
-  const user = await prisma.user.findFirst({
-    where: {
-      id: userID,
-    },
-  });
-
-  if (!user) {
-    throw new TRPCError({ code: "UNAUTHORIZED" });
-  };
-
-  return { userBalance: user.balance };
-}
+import { getUserID } from "@/src/helpers/getUserID";
+import { getBalance } from "@/src/apiWallet/balance";
+import { Hydrate, dehydrate } from "@tanstack/react-query";
 
 async function BalanceComponentWrapper() {
-  // const session = await getServerSession(authOptions);
-  // const authToken = session?.token;
+  const userID = await getUserID();
 
   const queryClient = getQueryClient();
 
-  // if (authToken) {
-    // await queryClient.prefetchQuery(["Balance"], () => getBalance(authToken));
-
-    await queryClient.prefetchQuery({
-      queryKey: [['transactionRouter', 'getBalance'], { type: "query" }], 
-      queryFn: getBalance,
-    });
-  // }
+  await queryClient.prefetchQuery({
+    queryKey: [["transactionRouter", "getBalance"], { type: "query" }],
+    queryFn: () => getBalance(userID),
+  });
 
   const dehydratedState = dehydrate(queryClient);
 
   return (
     <Hydrate state={dehydratedState}>
-      {/* {session && <Balance />} */}
       <Balance />
     </Hydrate>
   );
