4dd5e583f614f8576ee1231e3a9ed1515f1535a0
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-04 21:53:29 +0200

    Update RegistrationForm.tsx

--- src/components/RegistrationForm/RegistrationForm.tsx
+++ src/components/RegistrationForm/RegistrationForm.tsx
@@ -2,14 +2,11 @@
 
 import { useState } from "react";
 import { FormikHelpers } from "formik";
-import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useScaleForm } from "@/src/hooks/useScaleForm";
-import { ICredentials, IRegisterValues } from "@/src/types/registerValues";
 
 import { Box } from "../Box/Box";
-import { register } from "@/src/apiWallet/user";
-import { registerSchema } from "@/src/helpers/formValidation";
+import { TRegistrationValues, registrationValidator } from "@/src/helpers/formValidation";
 import { FormWrap } from "../LoginForm/LoginForm.styled";
 
 import Logo from "../Logo";
@@ -23,43 +20,29 @@ export default function RegistrationForm() {
   const router = useRouter();
 
   const isScale = useScaleForm();
-  // const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
 
-  const initialValues: IRegisterValues = {
+  const initialValues: TRegistrationValues = {
     email: "",
     password: "",
     confirmPassword: "",
-    name: "",
+    firstName: "",
   };
 
-  const { mutate: signUp, isLoading } = trpc.authRouter.signUp.useMutation({
+  const { mutate: signUp, isLoading, error, isError, reset } = trpc.authRouter.signUp.useMutation({
     onSuccess: () => {
       router.push('/login');
     }
   });
 
   const handleSubmit = async (
-    { email, password, name: firstName }: IRegisterValues,
-    { resetForm }: FormikHelpers<IRegisterValues>
+    { email, password, firstName, confirmPassword }: TRegistrationValues,
+    { resetForm }: FormikHelpers<TRegistrationValues>
   ) => {
-    const userCredentials: ICredentials = { email, password, firstName };
-
-    // setIsLoading(true);
-
+    
     try {
-      // const newUser = await register(userCredentials);
-      signUp(userCredentials)
+      signUp({ email, password, firstName, confirmPassword })
 
-      // const response = await signIn('credentials', {
-      //   email: email,
-      //   password: password,
-      //   redirect: false,
-      //   // redirect: true,
-      //   // callbackUrl: '/home/transactions'
-      // });
-
-      // router.push('/home/transactions');
     } catch (error) {
       const message = (error as Error).message
 
@@ -73,11 +56,8 @@ export default function RegistrationForm() {
         default:
           setErrorMessage("Server Error");
       } 
-    } finally {
-      // setIsLoading(false);
-    }
-  
-    resetForm();
+    } 
+    // resetForm();
   };
 
   return (
@@ -86,22 +66,20 @@ export default function RegistrationForm() {
         <Logo />
       </Box>
 
-      {errorMessage 
+      {error
         ? <AuthError
             href="/login" 
             text="Login" 
-            errorMessage={errorMessage} 
-            resetError={setErrorMessage} 
+            errorMessage={error.message} 
+            resetError={reset} 
           />
-        : <FormContainer<IRegisterValues>
-        onSubmit={handleSubmit}
-        initialValues={initialValues}
-        validationSchema={registerSchema}
-        render={(formik) => <RegisterFormFields formik={formik} loading={isLoading} />}
-      />
+        : <FormContainer<TRegistrationValues>
+            onSubmit={handleSubmit}
+            initialValues={initialValues}
+            validationSchema={registrationValidator}
+            render={(formik) => <RegisterFormFields formik={formik} loading={isLoading} />}
+          />
       }  
-
-      
     </FormWrap>
   );
 }
