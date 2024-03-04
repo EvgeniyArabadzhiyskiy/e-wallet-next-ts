06eb3a5a87abd6a78514699537260d3468915201
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-04 21:52:33 +0200

    Update AuthError.tsx

--- src/components/Errors/AuthError/AuthError.tsx
+++ src/components/Errors/AuthError/AuthError.tsx
@@ -1,4 +1,3 @@
-import { Dispatch, SetStateAction } from "react";
 import { ErrorText } from "../ErrorText.styled";
 import LinkButton from "../../Buttons/LinkButton";
 import { ButtonWrapper } from "./AuthError.styled";
@@ -8,17 +7,24 @@ interface IProps {
   href: string;
   text: string;
   errorMessage: string;
-  resetError: Dispatch<SetStateAction<string>>;
+  resetError: () => void;
 }
 
 function AuthError({ href, text, errorMessage, resetError }: IProps) {
+  console.log("AuthError  errorMessage:", errorMessage);
   return (
     <>
       <ErrorText as="p" mb={30} color="#10b981" fontSize="20px">
         There was a problem
       </ErrorText>
-  
-      <ErrorText as="p" mb={30} color="primaryBtn" fontSize="ml" fontWeight="bold">
+
+      <ErrorText
+        as="p"
+        mb={30}
+        color="primaryBtn"
+        fontSize="ml"
+        fontWeight="bold"
+      >
         {errorMessage}
       </ErrorText>
 
@@ -27,7 +33,7 @@ function AuthError({ href, text, errorMessage, resetError }: IProps) {
           height={50}
           maxWidth="180px"
           enterText="Try again"
-          onClick={() => resetError("")}
+          onClick={resetError}
         />
         <LinkButton href={href} text={text} maxWidth="180px" />
       </ButtonWrapper>
