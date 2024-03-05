402861705e2ce6d9c3d8c7318fdd9d028351a519
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-05 22:06:15 +0200

    Update editTransaction.ts

--- src/apiWallet/transaction/editTransaction.ts
+++ src/apiWallet/transaction/editTransaction.ts
@@ -1,18 +1,64 @@
-import { fetcher } from "../../helpers/fetcher"
-import { TRANSACTIONS } from "../../constants/apiPath"
-import { ChangedTransaction } from "../../types/transactions"
+import { TRPCError } from "@trpc/server";
+import prisma from "../../lib/prismaClient";
+import { type TEditTransactionValues } from "@/src/helpers/inputSchemas";
 
-export const editTransaction = async (id: string, transation: any, token: string | undefined) => {
-    const options: RequestInit = {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json;charset=utf-8",
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify(transation),
-    }
-  
-    const data = await fetcher<ChangedTransaction>(`${TRANSACTIONS}/edit/${id}`, options)
-  
-    return data
-}
+export const editTransaction = async (userID: string, updatedValues: TEditTransactionValues) => {
+  const { typeOperation, comment, category, amount, date, transactionID } =
+    updatedValues;
+
+  const user = await prisma.user.findFirst({
+    where: {
+      id: userID,
+    },
+  });
+
+  if (!user) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+
+  const prevTransaction = await prisma.transaction.findFirst({
+    where: {
+      owner: user.id,
+      id: transactionID,
+    },
+  });
+
+  if (!prevTransaction) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+
+  const sum = typeOperation === "income" ? amount : -amount;
+
+  const updatedTransaction = await prisma.transaction.update({
+    where: {
+      owner: user.id,
+      id: transactionID,
+    },
+
+    data: {
+      category,
+      comment,
+      amount: sum,
+      date,
+      typeOperation,
+    },
+  });
+
+  const newBalance =
+    user.balance - prevTransaction.amount + updatedTransaction.amount;
+
+  const updatedUser = await prisma.user.update({
+    where: {
+      id: user.id,
+    },
+
+    data: {
+      balance: newBalance,
+    },
+  });
+
+  return {
+    ...updatedTransaction,
+    timestamps: updatedTransaction.timestamps.toString(),
+  };
+};
