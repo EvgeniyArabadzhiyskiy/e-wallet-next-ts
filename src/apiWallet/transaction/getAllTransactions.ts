3fbbe4d44da793a783b71fa5cb483031cd386c4d
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-05 22:06:17 +0200

    Update getAllTransactions.ts

--- src/apiWallet/transaction/getAllTransactions.ts
+++ src/apiWallet/transaction/getAllTransactions.ts
@@ -1,8 +1,3 @@
-import { fetcher } from "../../helpers/fetcher";
-import { TRANSACTIONS } from "../../constants/apiPath";
-import { ITransactions } from "../../types/transactions";
-import { isITransactions } from "../../helpers/isITransactions";
-import { cookies } from "next/headers";
 import { TRPCError } from "@trpc/server";
 import prisma from "../../lib/prismaClient";
 
@@ -14,7 +9,7 @@ export const getAllTransactions = async (
   if (!userID) {
     throw new TRPCError({ code: "UNAUTHORIZED" });
   }
-  
+
   const user = await prisma.user.findFirst({
     where: {
       id: userID,
@@ -46,46 +41,4 @@ export const getAllTransactions = async (
   return {
     transactions: serializedTransactions,
   };
-
-  // const filtred = transactions.map((el) => {
-  //   const { updatedAt, id, ...props } = el;
-  //   const res = {
-  //     _id: id,
-  //     ...props,
-  //   };
-  //   return res;
-  // });
-
-  // return {
-  //   transactions: filtred,
-  //   balance: user.balance,
-  // };
 };
-
-// export const getAllTransactions =
-// async (token: string | undefined, pageNum: number) => {
-//   // await new Promise((res) => setTimeout(() => res(console.log("Promise resolve")), 1000));
-
-//   const options: RequestInit = {
-//     method: "GET",
-//     headers: {
-//       Authorization: `Bearer ${token}`
-//     },
-//     // cache: "no-store",
-//   };
-
-//   try {
-//     const data = await fetcher<ITransactions>
-//     (`${TRANSACTIONS}?page=${pageNum}&limit=10`, options);
-
-//     if (!isITransactions(data)) {
-//       throw new Error("Invalid data format");
-//     }
-
-//     return data;
-
-//   } catch (error) {
-//     console.log("Error:", (error as Error).message);
-//     throw error
-//   }
-// }
