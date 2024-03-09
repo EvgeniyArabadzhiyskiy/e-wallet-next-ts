a6e2e557a361dc712429d6723ce8ed8e1c8a8771
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-09 21:56:43 +0200

    Update useCreateTransaction.ts

--- src/apiWallet/useCreateTransaction.ts
+++ src/apiWallet/useCreateTransaction.ts
@@ -1,23 +1,11 @@
-
-import {
-  InfiniteData,
-  useMutation,
-  useQueryClient,
-} from "@tanstack/react-query";
-import {
-  // ITransaction,
-  ITransactions,
-  // NewTransaction,
-} from "../types/transactions";
-// import { createTransaction } from "./transaction";
-// import { ITransactionData } from "../types/transactionValue";
-import { useGlobalState } from "../components/GlobalProvider/GlobalProvider";
+import { InfiniteData, useQueryClient } from "@tanstack/react-query";
+import { ITransactions } from "../types/transactions";
 import { trpc } from "../trpc/client";
-
+import { useModalWindow } from "../hooks/useModalWindow";
 
 export const useCreateTransaction = () => {
   const queryClient = useQueryClient();
-  const { setModalToggle } = useGlobalState();
+  const setModalToggle = useModalWindow((state) => state.setModalToggle);
 
   const mutation = trpc.transactionRouter.createTransaction.useMutation({
     onSuccess: (data) => {
@@ -58,67 +46,9 @@ export const useCreateTransaction = () => {
 
       queryClient.invalidateQueries({ queryKey: [['transactionRouter', 'getBalance']] });
       queryClient.invalidateQueries({ queryKey: [['statisticRouter', 'getStatistic']] });
-      // queryClient.invalidateQueries({ queryKey: [['statisticRouter', 'getStatistic'], { input: {month: '2', year: '2024'}, type: "query" }] });
       setModalToggle("transaction");
     },
   })
 
   return mutation;
 };
-
-
-
-
-// export const useCreateTransaction = () => {
-//   const { token } = useUser();
-//   const queryClient = useQueryClient();
-//   const { setModalToggle } = useGlobalState();
-
-//   const mutation = useMutation<NewTransaction, Error, ITransactionData>({
-//     // createTransaction должна возвращать тип Promise<NewTransaction>
-//     mutationFn: (transaction) => createTransaction(transaction, token),
-
-//     onSuccess: (data) => {
-//       const { position, updatedAt, ...props } = data;
-//       const createTransaction: ITransaction = props;
-
-//       let newData = createTransaction;
-
-//       queryClient.setQueryData<InfiniteData<ITransactions>>(["TransactionsList"], (prev) => {
-//           if (!prev) {
-//             return undefined;
-//           }
-
-//           const updatedPages = prev.pages.map((page) => {
-//             const newCache = [newData, ...page.transactions].sort(
-//               (a, b) => Date.parse(b.date) - Date.parse(a.date));
-              
-//             if (newCache.length > 10) {
-//               const lastTransaction = newCache.pop();
-              
-//               if (lastTransaction) {
-//                 newData = lastTransaction;
-//               }
-//             }
-
-//             return {
-//               ...page,
-//               transactions: newCache,
-//             };
-//           });
-
-//           return {
-//             ...prev,
-//             pages: updatedPages,
-//           };
-//         }
-//       );
-
-//       queryClient.invalidateQueries({ queryKey: ["Balance"] });
-//       queryClient.invalidateQueries({ queryKey: ["Statistics"] });
-//       setModalToggle("transaction");
-//     },
-//   });
-
-//   return mutation;
-// };
