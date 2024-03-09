ce3e27221b3634b83086d74e71f6745a1c74b443
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-09 21:56:55 +0200

    Update route.ts

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,14 +1,14 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import { appRouter } from "@/src/trpc";
 import { createTRPCContext } from "@/src/trpc/context";
 
-const handler = (req: NextRequest) => {
+const handler = (req: NextRequest, res: NextResponse) => {
   return fetchRequestHandler({
     req,
     endpoint: "/api/trpc",
     router: appRouter,
-    createContext: () => createTRPCContext(req),
+    createContext: () => createTRPCContext(req, res),
   });
 };
 
