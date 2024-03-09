23aafca7b6408340fefb4e93514936049157d86d
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-09 21:57:02 +0200

    Update ButtonAddTransactions.tsx

--- src/components/Buttons/ButtonAddTransactions/ButtonAddTransactions.tsx
+++ src/components/Buttons/ButtonAddTransactions/ButtonAddTransactions.tsx
@@ -1,16 +1,16 @@
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import { useGlobalState } from "../../GlobalProvider/GlobalProvider";
 import { Button } from "./ButtonAddTransactions.styled";
 import { Dispatch, SetStateAction } from "react";
+import { useModalWindow } from "@/src/hooks/useModalWindow";
 
 interface IProps {
   setModalKey: Dispatch<SetStateAction<"ADD" | "EDIT">>;
 }
 
 function ButtonAddTransactions({setModalKey}: IProps) {
-  const { setModalToggle } = useGlobalState();
+  const setModalToggle = useModalWindow((state) => state.setModalToggle);
 
   const onAddTransactions = () => {
     setModalToggle("transaction");
