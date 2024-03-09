fd01b457d96b5100a3315a5a74e282d82751b64a
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-09 21:56:50 +0200

    Update useRemoveTransaction.ts

--- src/apiWallet/useRemoveTransaction.ts
+++ src/apiWallet/useRemoveTransaction.ts
@@ -1,7 +1,6 @@
-import { ITransaction, ITransactions, RemoveTransaction } from "@/src/types/transactions";
-import { InfiniteData, useQueryClient } from "@tanstack/react-query";
-import { deleteTransaction, getAllTransactions } from "./transaction";
 import { trpc } from "../trpc/client";
+import { ITransactions } from "@/src/types/transactions";
+import { InfiniteData, useQueryClient } from "@tanstack/react-query";
 
 export const useRemoveTransaction = (lastPageNumber: number) => {
   const queryClient = useQueryClient();
@@ -13,22 +12,7 @@ export const useRemoveTransaction = (lastPageNumber: number) => {
         [["transactionRouter", "getAllTransactions"],{ input: { limit: 10 }, type: "infinite" }]
       );
       
-     
       if (!infiniteData) return;
-
-      // const lastPageNumber = infiniteData.pages.length;
-
-      // const res = await fetch("/api/getTransactions", {
-      //   method: "POST",
-      //   headers: {
-      //     "Content-Type": "application/json;charset=utf-8",
-      //   },
-      //   body: JSON.stringify({ page: lastPageNumber })
-      // });
-
-      // const { transactions } = await res.json() as ITransactions;
-
-      // const lastTransaction = transactions?.pop();
 
       const { transactions: allTransactions } = infiniteData.pages
       .reduce((acc, page) => {
@@ -79,67 +63,8 @@ export const useRemoveTransaction = (lastPageNumber: number) => {
 
       queryClient.invalidateQueries({ queryKey: [['transactionRouter', 'getBalance']] });
       queryClient.invalidateQueries({ queryKey: [['statisticRouter', 'getStatistic']] });
-      // queryClient.invalidateQueries({ queryKey: ["Statistics"] });
     },
   });
-
-
-  
-  // const mutation = useMutation<RemoveTransaction, Error, string>({
-  //   mutationFn: (id) => deleteTransaction(id, token),
-
-  //   onSuccess: async (data) => {
-  //     const infiniteData = queryClient.getQueryData<InfiniteData<ITransactions>>(["TransactionsList"]);
-
-  //     if (!infiniteData) {
-  //       return;
-  //     }
-
-  //     const lastPageNumber = infiniteData.pages.length;
-  //     const { transactions } = await getAllTransactions(token, lastPageNumber);
-
-  //     const lastTransaction = transactions?.pop();
-
-  //     const { transactions: allTransaction, userBalance } = infiniteData.pages.reduce((acc, page) => {
-  //       return {
-  //         transactions: [...acc.transactions, ...page.transactions],
-  //         userBalance: page.userBalance,
-  //       }
-  //     },{ transactions: [], userBalance: 0 });
-
-  //     const filtredTransaction = allTransaction.filter((item) => item._id !== data._id)
-
-  //     const isLastPage = (allTransaction.length / lastPageNumber) !== 10
-
-  //     if (!isLastPage) {
-  //       lastTransaction && filtredTransaction.push(lastTransaction);
-  //     }
-
-  //     const transactionArrays = [];
-
-  //     for (let i = 0; i < filtredTransaction.length; i += 10) {
-  //       transactionArrays.push(filtredTransaction.slice(i, i + 10));
-  //     }
-
-  //     const newPages = transactionArrays.map((transactions) => ({
-  //       transactions,
-  //       userBalance,
-  //     }));
-
-  //     queryClient.setQueryData<InfiniteData<ITransactions>>( ["TransactionsList"], (prev) => {
-  //         if (prev) {
-  //           return {
-  //             ...prev,
-  //             pages: newPages,
-  //           };
-  //         }
-  //       }
-  //     );
-
-  //     queryClient.invalidateQueries({ queryKey: ["Balance"] });
-  //     queryClient.invalidateQueries({ queryKey: ["Statistics"] });
-  //   },
-  // });
 
   return mutation;
 };
