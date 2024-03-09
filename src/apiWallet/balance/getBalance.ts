a0c6d1c94b8df51006ea32263bc60eb5242eb110
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-09 21:56:40 +0200

    Update getBalance.ts

--- src/apiWallet/balance/getBalance.ts
+++ src/apiWallet/balance/getBalance.ts
@@ -1,6 +1,3 @@
-// import { fetcher } from "../../helpers/fetcher";
-// import { IBalance } from "../../types/transactions";
-// import { BALANCE } from "../../constants/apiPath";
 import prisma from "../../lib/prismaClient";
 import { TRPCError } from "@trpc/server";
 
@@ -21,28 +18,3 @@ export const getBalance = async (userID: string | null) => {
 
   return { userBalance: user.balance };
 };
-
-// export const getBalance = async (authToken: string | undefined) => {
-
-//   const options: RequestInit = {
-//     method: "GET",
-//     headers: {
-//       Authorization: `Bearer ${authToken}`,
-//     },
-//     cache: "no-store",
-//   };
-
-//   try {
-//     const data = await fetcher<IBalance>(`${BALANCE}`, options);
-
-//     if (typeof data !== "object" || typeof data.userBalance !== "number") {
-//       throw new Error("Invalid data format");
-//     }
-
-//     return data;
-
-//   } catch (error) {
-//     console.log("Error:", (error as Error).message);
-//     throw error;
-//   }
-// };
