bddf097a9820dcb3ccecdc8bd89e304d1de9e967
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-10 21:45:23 +0200

    Update BalanceLoader.tsx

--- src/components/BalanceLoader/BalanceLoader.tsx
+++ src/components/BalanceLoader/BalanceLoader.tsx
@@ -1,17 +1,8 @@
-// "use client";
-
-// import { Wrapper, Title, Text, Loader } from "./BalanceLoader.styled";
 import stl from "./BalanceLoader.module.scss";
 
 function BalanceLoader() {
   const placeholderBalance = -87710.0;
   return (
-    // или styled component BalanceLoader
-    // <Wrapper>
-    //   <Title>Your balance</Title>
-    //   <Loader>{placeholderBalance.toFixed(2)}</Loader>
-    // </Wrapper>
-
     <div className={stl.wrapper}>
       <p className={stl.balance__title}>Your balance</p>
       <div className={stl.loading}>{placeholderBalance.toFixed(2)}</div>
