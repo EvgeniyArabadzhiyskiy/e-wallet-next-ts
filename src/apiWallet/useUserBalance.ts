f1e513e776676cbc9cfc457c4a6fad7dde99fd0a
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-10 21:45:09 +0200

    Update useUserBalance.ts

--- src/apiWallet/useUserBalance.ts
+++ src/apiWallet/useUserBalance.ts
@@ -1,33 +1,9 @@
-
-// import { useQuery } from "@tanstack/react-query";
-// import { IBalance } from "../types/transactions";
-// import { getBalance } from "./balance";
 import { trpc } from "../trpc/client";
 
 export const useUserBalance = () => {
-  const data = trpc.transactionRouter.getBalance.useQuery(undefined, {
+  const queryData = trpc.transactionRouter.getBalance.useQuery(undefined, {
     select: (data) => data.userBalance,
-    staleTime: Infinity,
-    refetchOnWindowFocus: false,
-    retry: 0,
   });
 
-  return data;
+  return queryData;
 };
-
-// export const useUserBalance = () => {
-//   const { token } = useUser();
-
-//   const data = useQuery<IBalance, Error, number, string[]>({
-//     queryKey: ["Balance"],
-//     queryFn: () => getBalance(token),
-//     select: (data) => data.userBalance,
-
-//     staleTime: Infinity,
-//     refetchOnWindowFocus: false,
-//     retry: 0,
-//     enabled: !!token,
-//   });
-
-//   return data;
-// };
