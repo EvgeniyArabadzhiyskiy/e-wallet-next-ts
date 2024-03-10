4d91cf9a0f4d2660ac167a18045e4027e672360d
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-10 21:45:16 +0200

    Update page.tsx

--- src/app/home/transactions/page.tsx
+++ src/app/home/transactions/page.tsx
@@ -2,6 +2,13 @@ import { Suspense } from "react";
 import TransactionLoader from "@/src/components/TransactionLoader";
 import TransactionWrapper from "@/src/components/TransactionWrapper";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Transaction Page",
+  description: "e-Wallet Transactions",
+};
+
 export default function PageTransactions() {
   return (
     <Suspense fallback={<TransactionLoader />}>
