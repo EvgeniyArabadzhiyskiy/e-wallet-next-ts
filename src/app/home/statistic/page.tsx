1bf6d72a33d62b26b4298c5f44ed1d34c71b2b85
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-10 21:45:14 +0200

    Update page.tsx

--- src/app/home/statistic/page.tsx
+++ src/app/home/statistic/page.tsx
@@ -2,6 +2,13 @@ import { Suspense } from "react";
 import StatisticLoader from "@/src/components/StatisticLoader";
 import StatisticWrapper from "@/src/components/StatisticWrapper";
 
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Statistic Page",
+  description: "e-Wallet Statistics",
+};
+
 export default async function PageStatistic() {
   return (
     <Suspense fallback={<StatisticLoader />}>
