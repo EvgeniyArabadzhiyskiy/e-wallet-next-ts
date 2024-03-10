b03d1c3f3279ba1f1008016e70df68ce5d26c4d4
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-10 21:45:41 +0200

    Update Container.tsx

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -2,26 +2,4 @@ import stl from "./Container.module.scss";
 
 export default function Container({ children }: { children: React.ReactNode }) {
   return <div className={stl.container}>{children}</div>;
-  // return <div className="container" >{children}</div>;
 }
-
-
-// "use client"
-// import styled from "styled-components";
-
-// export const Container = styled.div`
-//   margin: 0 auto;
-
-//   @media screen and (max-width: 767px) {
-//     max-width: 480px;
-//   }
-
-//   @media screen and (min-width: 768px) {
-//     width: 768px;
-//   }
-
-//   @media screen and (min-width: 1280px) {
-//     width: 1280px;
-//   }
-// `;
-
