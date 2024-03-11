57083e3a46274619ef17d013befc4771eb63df88
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-11 22:16:28 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,38 +1,29 @@
 /** @type {import('next').NextConfig} */
 const path = require("path");
 
-// const withImages = require('next-images');
-
 const nextConfig = {
   // reactStrictMode: true,
-
-  // experimental: {
-  //   appDir: true,
-  // },
+  async headers() {
+    return [
+      {
+        source: "/home/transactions",
+        headers: [
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "unsafe-none",
+          },
+        ],
+      },
+    ];
+  },
 
   compiler: {
     styledComponents: true,
   },
 
-  // env: {
-  //   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-  //   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET
-  // },
-
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
-
-  
-  // ====== SVG =========
-  // webpack: (config) => {
-  //   config.module.rules.push({
-  //     test: /\.svg$/i,
-  //     use: ['@svgr/webpack'],
-  //   });
-  //   return config;
-  // },
 };
 
 module.exports = nextConfig;
-// module.exports = withImages(nextConfig);
