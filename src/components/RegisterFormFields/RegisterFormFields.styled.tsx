447fb52a2859de0e9ddc25714b1aa5cd0de0d401
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-11 22:16:49 +0200

    Update RegisterFormFields.styled.tsx

--- src/components/RegisterFormFields/RegisterFormFields.styled.tsx
+++ src/components/RegisterFormFields/RegisterFormFields.styled.tsx
@@ -7,23 +7,23 @@ import {
   changeMainLineColor,
 } from "@/src/helpers/passwordIndicators";
 
-export const PasswordIndicator = styled.div<{ length: string }>`
+export const PasswordIndicator = styled.div<{ $length: string }>`
   position: absolute;
   bottom: -10px;
   left: 0;
   width: 100%;
   height: 4px;
   border-radius: 2px;
-  background-color: ${(p) => changeMainLineColor(p.length)};
+  background-color: ${(p) => changeMainLineColor(p.$length)};
 
   &::after {
     position: absolute;
     bottom: 0;
     left: 0;
     content: "";
-    width: ${(p) => changeAfterLineWidth(p.length)};
+    width: ${(p) => changeAfterLineWidth(p.$length)};
     height: 4px;
     border-radius: 2px;
-    background-color: ${(p) => changeAfterLineColor(p.length)};
+    background-color: ${(p) => changeAfterLineColor(p.$length)};
   }
 `;
