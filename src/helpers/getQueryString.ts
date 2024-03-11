80137feee052d28ba3cdf68e7656640af866fd7f
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-11 22:18:07 +0200

    Delete getQueryString.ts

--- src/helpers/getQueryString.ts
+++
@@ -1,8 +0,0 @@
-import { IStatPeriod } from "../types/statistics"
-
-export const getQueryString = ({month, year}: IStatPeriod) => {
-    if (month && year)  return `/transactions/statistics?month=${month}&year=${year}`
-    if (year)  return `/transactions/statistics?year=${year}`
-    if (month)  return `/transactions/statistics?month=${month}`
-    return '/transactions/statistics'
-  } 
