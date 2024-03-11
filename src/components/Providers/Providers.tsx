98ccd8b93c270075c0735af5fc01b60ed855c59a
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-11 22:16:46 +0200

    Update Providers.tsx

--- src/components/Providers/Providers.tsx
+++ src/components/Providers/Providers.tsx
@@ -3,10 +3,15 @@
 import { useState } from "react";
 import { ThemeProvider } from "styled-components";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { useGlobalState } from "../GlobalProvider/GlobalProvider";
-import { darkTheme, lightTheme } from "@/src/styles/theme/theme";
+import { walletTheme } from "@/src/styles/theme/theme";
 import { trpc } from "@/src/trpc/client";
 import { httpBatchLink } from "@trpc/client";
+
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { SessionProvider } from "next-auth/react";
+
+const clientId =
+  "259286260568-kl6201hqpnakfuu3e8uk4riuojbodkkb.apps.googleusercontent.com";
 
 interface IProps {
   children?: React.ReactNode;
@@ -20,9 +25,6 @@ function Providers({ children }: IProps) {
           queries: {
             staleTime: Infinity,
             cacheTime: Infinity,
-
-            // refetchOnMount: false,    // Не обновляло данные при переходе на /statistic
-            // refetchOnReconnect: false,
             refetchOnWindowFocus: false,
             retry: 0,
           },
@@ -44,22 +46,19 @@ function Providers({ children }: IProps) {
     })
   );
 
-  // const { theme } = useGlobalState();
-
-  // if (isLoading) {
-  //   return <h1>Loading Theme..</h1>;
-  // }
-
   return (
     <>
-      {/* <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}> */}
-      <ThemeProvider theme={lightTheme}>
-        <trpc.Provider client={trpcClient} queryClient={queryClient}>
-          <QueryClientProvider client={queryClient}>
-            {children}
-          </QueryClientProvider>
-        </trpc.Provider>
-      </ThemeProvider>
+      <GoogleOAuthProvider clientId={clientId}>
+        {/* <SessionProvider> */}
+        <ThemeProvider theme={walletTheme}>
+          <trpc.Provider client={trpcClient} queryClient={queryClient}>
+            <QueryClientProvider client={queryClient}>
+              {children}
+            </QueryClientProvider>
+          </trpc.Provider>
+        </ThemeProvider>
+        {/* </SessionProvider> */}
+      </GoogleOAuthProvider>
     </>
   );
 }
