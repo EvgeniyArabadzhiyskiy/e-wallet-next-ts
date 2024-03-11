57b042ab069a69457445065343ce4488cb04a221
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-11 22:18:21 +0200

    Create getErrorMessage.ts

---
+++ src/helpers/getErrorMessage.ts
@@ -0,0 +1,20 @@
+export const getErrorMessage = (errorCode: string) => {
+  switch (errorCode) {
+    case "401":
+      return {
+        reason: "You are not authorized to access this resource.",
+        text: "Please log in or authenticate to proceed.",
+      };
+
+    case "404":
+      return {
+        reason: "Oops, the page you're looking for doesn't exist.",
+        text: "Please check the URL and try again.",
+      };
+    default:
+      return {
+        reason: "An error occurred.",
+        text: "Please try again later.",
+      };
+  }
+};
