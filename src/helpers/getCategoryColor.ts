78ace52569286f20389cad891455bfa6e28c22f5
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-12 21:31:46 +0200

    Update getCategoryColor.ts

--- src/helpers/getCategoryColor.ts
+++ src/helpers/getCategoryColor.ts
@@ -1,18 +1,12 @@
-import { IColor } from "../types/statistics";
+import { categoryList } from "../constants/categoryList";
 
-const categoryList: IColor = {
-  Food: "#f6a626",
-  Main: "#fde047",
-  House: "#a855f7",
-  Auto: "#4bc0c0",
-  Children: "#4ade80",
-  Development: "#ff6385",
-  Education: "#3b82f6",
-  "Other expenses": "#00ad31",
-};
-
-export const getCategoryColor = (value: keyof IColor) => {
-  const color = categoryList[value];
-
-  return color;
+export const getCategoryColor = (label: string) => {
+  for (const category in categoryList) {
+    if (categoryList.hasOwnProperty(category)) {
+      if (categoryList[category].label === label) {
+        return categoryList[category].value;
+      }
+    }
+  }
+  return "#FF0000";
 };
