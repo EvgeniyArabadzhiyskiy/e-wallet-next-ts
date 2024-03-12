8053b901e05d4a422506184c20fa422b08004f1e
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-12 21:30:39 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,21 +2,6 @@
 const path = require("path");
 
 const nextConfig = {
-  // reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: "/home/transactions",
-        headers: [
-          {
-            key: "Cross-Origin-Embedder-Policy",
-            value: "unsafe-none",
-          },
-        ],
-      },
-    ];
-  },
-
   compiler: {
     styledComponents: true,
   },
