0785476e73739300be9ebbc0278622031f2963f5
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-12 21:31:41 +0200

    Update formValidation.ts

--- src/helpers/formValidation.ts
+++ src/helpers/formValidation.ts
@@ -1,25 +1,4 @@
 import * as yup from "yup";
-import { ITransactionValue } from "../types/transactionValue";
-import { IRegisterValues } from "../types/registerValues";
-import { ILoginValues } from "../types/loginValues";
-
-type TransactionSchema = ITransactionValue<number>;
-
-export type ValidationSchema = yup.SchemaOf<
-  ILoginValues | IRegisterValues | TransactionSchema
->;
-
-// const valueAfterComma = (value: number | undefined) => {
-//   if (!value) return false;
-//   const idxComma = String(value).indexOf(".");
-//   const isTwoDigits = String(value).length - idxComma < 4;
-
-//   if (idxComma === -1) {
-//     return true;
-//   }
-
-//   return isTwoDigits;
-// };
 
 const valueAfterComma = (value: number | undefined) => {
   if (!value) return false;
@@ -27,7 +6,7 @@ const valueAfterComma = (value: number | undefined) => {
   return !decimalPart || decimalPart.length <= 2;
 };
 
-export const registerSchema: ValidationSchema = yup.object().shape({
+export const registrationValidator = yup.object().shape({
   email: yup.string().email("Invalid email").required(),
   password: yup
     .string()
@@ -38,19 +17,19 @@ export const registerSchema: ValidationSchema = yup.object().shape({
     .string()
     .oneOf([yup.ref("password")], "Password do not match")
     .required(),
-  name: yup
+  firstName: yup
     .string()
     .min(1, "Minimun 1 characters")
     .max(12, "Maximum 12 characters")
     .required("Required"),
 });
 
-export const loginSchema: ValidationSchema = yup.object().shape({
+export const loginValidator = yup.object().shape({
   email: yup.string().email("Invalid email").required(),
   password: yup.string().required("Required"),
 });
 
-export const transactionShema: ValidationSchema = yup.object().shape({
+export const transactionValidator = yup.object().shape({
   category: yup.string().required("This field is required"),
 
   amount: yup
@@ -58,7 +37,7 @@ export const transactionShema: ValidationSchema = yup.object().shape({
     .typeError("You need to enter a number")
     .positive("Only positive value")
     .test("is-decimal", "Maximum two digits after comma", (value) =>
-      valueAfterComma(value)
+      valueAfterComma(Number(value))
     )
     .required("This field is required"),
 
@@ -70,48 +49,10 @@ export const transactionShema: ValidationSchema = yup.object().shape({
   date: yup.string().required(),
 });
 
-// const schema = {
-//   register: yup.object().shape({
-//     email: yup.string().email("Invalid email").required(),
-//     password: yup
-//       .string()
-//       .min(6, "Minimun 6 characters")
-//       .max(12, "Maximum 12 characters")
-//       .required("Required"),
-//     confirmPassword: yup
-//       .string()
-//       .oneOf([yup.ref("password")], "Password do not match")
-//       .required(),
-//     name: yup
-//       .string()
-//       .min(1, "Minimun 1 characters")
-//       .max(12, "Maximum 12 characters")
-//       .required("Required"),
-//   }),
+export type TRegistrationValues = yup.Asserts<typeof registrationValidator>;
+export type TLoginValues = yup.Asserts<typeof loginValidator>;
+export type TTransactionValues = yup.Asserts<typeof transactionValidator>;
 
-//   login: yup.object().shape({
-//     email: yup.string().email("Invalid email").required(),
-//     password: yup.string().required("Required"),
-//   }),
-
-//   transactionShema: yup.object().shape({
-//     category: yup.string().required("This field is required"),
-
-//     amount: yup
-//       .number()
-//       .typeError("You need to enter a number")
-//       .positive("Only positive value")
-//       .test("is-decimal", "Maximum two digits after comma", (value) =>
-//         valueAfterComma(value)
-//       )
-//       .required("This field is required"),
-
-//     comment: yup
-//       .string()
-//       .min(2, "Must be longer than 2 letters")
-//       .max(20, "Must be shorter than 20 letters")
-//       .required("This field is required"),
-//   }),
-// };
-
-// export default schema;
+export type ValidationSchema = yup.Schema<
+  TLoginValues | TRegistrationValues | TTransactionValues
+>;
