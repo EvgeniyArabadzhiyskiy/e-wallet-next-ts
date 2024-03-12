d35589600a5be115cf28e0bedd2189cb48c44bb8
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-12 21:34:04 +0200

    Update transactions.ts

--- src/types/transactions.ts
+++ src/types/transactions.ts
@@ -14,20 +14,4 @@ export interface ITransaction {
 
 export interface ITransactions {
   transactions: ITransaction[];
-  // userBalance: number;
-}
-
-export interface NewTransaction extends ITransaction {
-  position: number;
-  updatedAt: string;
-}
-
-export interface RemoveTransaction extends ITransaction {
-  updatedAt: string;
-}
-
-export interface ChangedTransaction extends RemoveTransaction {}
-
-export interface IBalance {
-  userBalance: number;
 }
