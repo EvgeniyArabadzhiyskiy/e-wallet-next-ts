c50f8df6891951eee0a7a107885a2e0f4562bdeb
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-12 21:34:16 +0200

    Create categoryList.ts

---
+++ src/constants/categoryList.ts
@@ -0,0 +1,43 @@
+import { IColors } from "../types/statistics";
+
+export const categoryList: IColors = {
+  food: {
+    label: "Food",
+    value: "#f6a626",
+  },
+
+  main: {
+    label: "Main",
+    value: "#fde047",
+  },
+
+  house: {
+    label: "House",
+    value: "#a855f7",
+  },
+
+  auto: {
+    label: "Auto",
+    value: "#4bc0c0",
+  },
+
+  children: {
+    label: "Children",
+    value: "#4ade80",
+  },
+
+  development: {
+    label: "Development",
+    value: "#ff6385",
+  },
+
+  education: {
+    label: "Education",
+    value: "#3b82f6",
+  },
+
+  other: {
+    label: "Other expenses",
+    value: "#00ad31",
+  },
+};
