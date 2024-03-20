939b128ef8757806fb40de5cf5e3558f6ef29d5f
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-20 21:12:45 +0200

    Create TransactionError.styled.tsx

---
+++ src/components/Errors/TransactionError/TransactionError.styled.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import styled from "styled-components";
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 80%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 100px;
+`;
