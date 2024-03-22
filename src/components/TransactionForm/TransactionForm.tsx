d94bfc38c85d6530d0f96a57b244ecb32220bcf1
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-22 05:25:12 +0200

    Update TransactionForm.tsx

--- src/components/TransactionForm/TransactionForm.tsx
+++ src/components/TransactionForm/TransactionForm.tsx
@@ -1,92 +1,93 @@
-"use client";
+// "use client";
 
-import { FormikHelpers } from "formik";
-import { Dispatch, SetStateAction } from "react";
-import { TTransactionValues, transactionValidator } from "@/src/helpers/formValidation";
-import { getTypeOperation } from "@/src/helpers/getTypeOperation";
-import { useCreateTransaction, useEditTransaction } from "@/src/apiWallet";
+// import { FormikHelpers } from "formik";
+// import { Dispatch, SetStateAction } from "react";
+// import { TTransactionValues, transactionValidator } from "@/src/helpers/formValidation";
+// import { getTypeOperation } from "@/src/helpers/getTypeOperation";
+// import { useCreateTransaction, useEditTransaction } from "@/src/apiWallet";
 
-import { Title } from "./TransactionForm.styled";
-import FormContainer from "../FormContainer";
-import TransactionFormFields from "../TransactionFormFields";
-import { setTitleTransaction } from "@/src/helpers/setTitleTransaction";
-import CreateTransactionError from "../Errors/CreateTransactionError";
+// import { Title } from "./TransactionForm.styled";
+// import FormContainer from "../FormContainer";
+// import TransactionFormFields from "../TransactionFormFields";
+// import { setTitleTransaction } from "@/src/helpers/setTitleTransaction";
+// import TransactionError from "../Errors/TransactionError";
 
-interface IProps {
-  isIncome: boolean;
-  setIsIncome: Dispatch<SetStateAction<boolean>>;
-  modalKey: string;
-  editId: string;
-}
+// interface IProps {
+//   isIncome: boolean;
+//   setIsIncome: Dispatch<SetStateAction<boolean>>;
+//   modalKey: string;
+//   editId: string;
+// }
 
-function TransactionForm({ isIncome, setIsIncome, modalKey, editId }: IProps) {
-  const { mutate: editTransaction, error: editError, isError: isEditError, reset: editReset }
-   = useEditTransaction();
+// function TransactionForm({ isIncome, setIsIncome, modalKey, editId }: IProps) {
+//   const { mutate: editTransaction, isLoading: editLoading, error: editError, isError: isEditError, reset: editReset }
+//    = useEditTransaction();
    
-  const { mutate: createTransaction, error: createError, isError: isCreateError, reset: createReset }
-   = useCreateTransaction();
+//   const { mutate: createTransaction, isLoading: createLoading, error: createError, isError: isCreateError, reset: createReset }
+//    = useCreateTransaction();
   
-  const initialValues: TTransactionValues  = {
-    comment: "",
-    amount: 0,
-    category: "",
-    date: new Date().toString(),
-  };
+//   const initialValues: TTransactionValues  = {
+//     comment: "",
+//     amount: 0,
+//     category: "",
+//     date: new Date().toString(),
+//   };
 
-  const onFormSubmit = async (
-    values: TTransactionValues,
-    { resetForm }: FormikHelpers<TTransactionValues>
-  ) => {
-    const typeOperation = getTypeOperation(isIncome);
+//   const onFormSubmit = (
+//     values: TTransactionValues,
+//     { resetForm }: FormikHelpers<TTransactionValues>
+//   ) => {
+//     const typeOperation = getTypeOperation(isIncome);
 
-    const transaction = {
-      ...values,
-      amount: Number(values.amount),
-      typeOperation,
-    };
+//     const transaction = {
+//       ...values,
+//       amount: Number(values.amount),
+//       typeOperation,
+//     };
 
-    if (modalKey === "ADD") {
-      createTransaction(transaction);
-      resetForm();
-    }
+//     if (modalKey === "ADD") {
+//       createTransaction(transaction);
+//       resetForm();
+//     }
 
-    if (modalKey === "EDIT") {
-      editTransaction({...transaction, transactionID: editId});
-      resetForm();
-    }
-  };
+//     if (modalKey === "EDIT") {
+//       editTransaction({...transaction, transactionID: editId});
+//       resetForm();
+//     }
+//   };
 
-  if (isEditError) {
-    return (
-      <CreateTransactionError error={editError.message} resetError={editReset} />
-    );
-  }
+//   if (isEditError) {
+//     return (
+//       <TransactionError error={editError.message} resetError={editReset} />
+//     );
+//   }
 
-  if (isCreateError) {
-    return (
-      <CreateTransactionError error={createError.message} resetError={createReset} />
-    );
-  }
+//   if (isCreateError) {
+//     return (
+//       <TransactionError error={createError.message} resetError={createReset} />
+//     );
+//   }
 
-  return (
-    <>
-      <Title>{setTitleTransaction(modalKey)}</Title>
+//   return (
+//     <>
+//       <Title>{setTitleTransaction(modalKey)}</Title>
 
-      <FormContainer<TTransactionValues>
-        initialValues={initialValues}
-        validationSchema={transactionValidator}
-        onSubmit={onFormSubmit}
-        render={(formik) => (
-          <TransactionFormFields
-            formik={formik}
-            isIncome={isIncome}
-            setIsIncome={setIsIncome}
-            modalKey={modalKey}
-          />
-        )}
-      />
-    </>
-  );
-}
+//       <FormContainer<TTransactionValues>
+//         initialValues={initialValues}
+//         validationSchema={transactionValidator}
+//         onSubmit={onFormSubmit}
+//         render={(formik) => (
+//           <TransactionFormFields
+//             formik={formik}
+//             isIncome={isIncome}
+//             setIsIncome={setIsIncome}
+//             modalKey={modalKey}
+//             isLoading={createLoading || editLoading}
+//           />
+//         )}
+//       />
+//     </>
+//   );
+// }
 
-export default TransactionForm;
+// export default TransactionForm;
