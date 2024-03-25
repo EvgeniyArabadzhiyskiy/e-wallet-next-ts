a28ad4651ed1c108803cbbb2393923c277c821c8
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-03-25 22:00:08 +0200

    Create CreateTransactionForm.tsx

---
+++ src/components/TransactionForm/CreateTransactionForm.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { Dispatch, SetStateAction } from "react";
+import { useCreateTransaction } from "@/src/apiWallet";
+import {
+  TTransactionValues,
+  transactionValidator,
+} from "@/src/helpers/formValidation";
+import { FormikHelpers } from "formik";
+import { getTypeOperation } from "@/src/helpers/getTypeOperation";
+import TransactionError from "../Errors/TransactionError";
+import { Title } from "./TransactionForm.styled";
+import { setTitleTransaction } from "@/src/helpers/setTitleTransaction";
+import FormContainer from "../FormContainer";
+import TransactionFormFields from "../TransactionFormFields";
+
+interface IPropsCreateTransaction {
+  isIncome: boolean;
+  setIsIncome: Dispatch<SetStateAction<boolean>>;
+  modalKey: string;
+}
+
+function CreateTransactionForm({
+  isIncome,
+  setIsIncome,
+  modalKey,
+}: IPropsCreateTransaction) {
+  const { mutate, isLoading, error, isError, reset } = useCreateTransaction();
+
+  const initialValues: TTransactionValues = {
+    comment: "",
+    amount: 0,
+    category: "",
+    date: new Date().toString(),
+  };
+
+  const onFormSubmit = (
+    values: TTransactionValues,
+    { resetForm }: FormikHelpers<TTransactionValues>
+  ) => {
+    const typeOperation = getTypeOperation(isIncome);
+
+    const transaction = {
+      ...values,
+      amount: Number(values.amount),
+      typeOperation,
+    };
+
+    mutate(transaction);
+    resetForm();
+  };
+
+  if (isError) {
+    return <TransactionError error={error.message} resetError={reset} />;
+  }
+
+  return (
+    <>
+      <Title>{setTitleTransaction(modalKey)}</Title>
+
+      <FormContainer<TTransactionValues>
+        initialValues={initialValues}
+        validationSchema={transactionValidator}
+        onSubmit={onFormSubmit}
+        render={(formik) => (
+          <TransactionFormFields
+            formik={formik}
+            isIncome={isIncome}
+            setIsIncome={setIsIncome}
+            modalKey={modalKey}
+            isLoading={isLoading}
+          />
+        )}
+      />
+    </>
+  );
+}
+
+export default CreateTransactionForm;
