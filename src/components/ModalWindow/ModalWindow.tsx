af30d1435eae10842c5370508526813f1f676784
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-04-07 21:05:21 +0300

    Update ModalWindow.tsx

--- src/components/ModalWindow/ModalWindow.tsx
+++ src/components/ModalWindow/ModalWindow.tsx
@@ -2,7 +2,20 @@
 
 import { useEffect } from "react";
 import { createPortal } from "react-dom";
-import { Modal, Overlay } from "./ModalWindow.styled";
+import { Overlay } from "./ModalWindow.styled";
+import { motion } from "framer-motion";
+
+const pageAnimationType = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1 },
+};
+
+const pageAnimation = {
+  variants: pageAnimationType,
+  initial: "hidden",
+  animate: "show",
+  exit: "hidden",
+};
 
 interface IProps {
   modalName: string;
@@ -10,8 +23,18 @@ interface IProps {
   children: React.ReactNode;
 }
 
-function ModalWindow ({ children, modalName, setModalToggle }: IProps) {
-  const modalRoot = document.querySelector("#modal-root");
+// const variants = {
+//   active: {
+//       backgroundColor: "#f00"
+//   },
+//   inactive: {
+//     backgroundColor: "#fff",
+//     transition: { duration: 2 }
+//   }
+// }
+
+function ModalWindow({ children, modalName, setModalToggle }: IProps) {
+  const modalRoot: HTMLDivElement = document.querySelector("#modal-root")!;
 
   const onBackdropClick = (e: React.MouseEvent<HTMLDivElement>) => {
     if (e.target === e.currentTarget) {
@@ -27,22 +50,45 @@ function ModalWindow ({ children, modalName, setModalToggle }: IProps) {
 
   useEffect(() => {
     window.addEventListener("keydown", onEscPress);
-    document.body.style.overflow = "hidden";
+    // document.body.style.overflow = "hidden";
 
     return () => {
       window.removeEventListener("keydown", onEscPress);
-      document.body.style.overflow = "visible";
+      // document.body.style.overflow = "visible";
     };
   });
 
   return createPortal(
-    <Overlay onClick={onBackdropClick}>
-      <Modal>
+    <Overlay
+      // {...pageAnimation}
+      // transition={{ duration: 0.5 }}
+
+      // initial='hidden'
+      // animate="show"
+      // variants={{
+      //   hidden: { opacity: 0 },
+      //   show: {opacity: 1}
+      //   // inactive: {transition: {duration: 2}}
+      // }}
+      // exit='hidden'
+
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.3 }}
+      onClick={onBackdropClick}
+    >
+      <motion.div
+        initial={{ scale: 0.9 }}
+        animate={{ scale: 1 }}
+        exit={{ scale: 0.9 }}
+        transition={{ duration: 0.3 }}
+      >
         {children}
-      </Modal>
+      </motion.div>
     </Overlay>,
-    modalRoot as HTMLElement
+    modalRoot
   );
 }
 
-export default ModalWindow
+export default ModalWindow;
