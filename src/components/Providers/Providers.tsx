35eac8f2e9623a63d4e1b0d3ce3bf7ea61ef1657
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-04-10 21:43:50 +0300

    Update Providers.tsx

--- src/components/Providers/Providers.tsx
+++ src/components/Providers/Providers.tsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { walletTheme } from "@/src/styles/theme/theme";
 import { trpc } from "@/src/trpc/client";
 import { httpBatchLink } from "@trpc/client";
+import { Toaster } from "sonner";
 
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
@@ -53,6 +54,7 @@ function Providers({ children }: IProps) {
           </trpc.Provider>
         </ThemeProvider>
       </GoogleOAuthProvider>
+      <Toaster position="top-center" richColors toastOptions={{style: {fontSize: "20px"}}} />
     </>
   );
 }
