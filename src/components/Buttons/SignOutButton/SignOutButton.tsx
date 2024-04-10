41a511ab78415d08c2b8edb8adcd78da2739b63e
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-04-10 21:43:27 +0300

    Update SignOutButton.tsx

--- src/components/Buttons/SignOutButton/SignOutButton.tsx
+++ src/components/Buttons/SignOutButton/SignOutButton.tsx
@@ -5,11 +5,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import { useModalWindow } from "@/src/hooks/useModalWindow";
 
-function SignOutButton() {
+interface IProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+function SignOutButton({ ...allProps }: IProps) {
   const setModalToggle = useModalWindow((state) => state.setModalToggle);
 
   return (
-    <ButtonExit type="button" onClick={() => setModalToggle("logout")}>
+    <ButtonExit type="button" onClick={() => setModalToggle("logout")} {...allProps}>
       <FontAwesomeIcon icon={faArrowRightFromBracket} />
       <Text>Exit</Text>
     </ButtonExit>
