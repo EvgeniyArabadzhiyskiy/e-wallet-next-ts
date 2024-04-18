8277e8a5310559b47efc6f68008711993a17a4d6
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-04-18 22:23:24 +0300

    Update ModalBox.tsx

--- src/components/ModalWindow/ModalBox.tsx
+++ src/components/ModalWindow/ModalBox.tsx
@@ -2,7 +2,6 @@
 
 import ModalWindow from "./ModalWindow";
 import { useModalWindow } from "@/src/hooks/useModalWindow";
-import { AnimatePresence } from "framer-motion";
 
 interface IProps {
   children: React.ReactNode;
@@ -11,18 +10,10 @@ interface IProps {
 
 function ModalBox({ children, modalName }: IProps) {
   const isModalOpen = useModalWindow((state) => state.isModalOpen);
-  const setModalToggle = useModalWindow((state) => state.setModalToggle);
-
   const isOpen = !!isModalOpen[modalName];
 
   return (
-    <AnimatePresence>
-      {isOpen && (
-        <ModalWindow modalName={modalName} setModalToggle={setModalToggle}>
-          {children}
-        </ModalWindow>
-      )}
-    </AnimatePresence>
+    <>{isOpen && <ModalWindow modalName={modalName}>{children}</ModalWindow>}</>
   );
 }
 
