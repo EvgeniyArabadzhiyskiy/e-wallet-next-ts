2f220dd355d7c75f8d1ecdce31f8f7049148bb82
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-04-19 19:57:38 +0300

    Update ButtonAddTransactions.tsx

--- src/components/Buttons/ButtonAddTransactions/ButtonAddTransactions.tsx
+++ src/components/Buttons/ButtonAddTransactions/ButtonAddTransactions.tsx
@@ -4,16 +4,17 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button } from "./ButtonAddTransactions.styled";
 import { Dispatch, SetStateAction } from "react";
 import { useModalWindow } from "@/src/hooks/useModalWindow";
+import { TRANSACTION_KEY } from "@/src/constants/modalKey";
 
 interface IProps {
   setModalKey: Dispatch<SetStateAction<"ADD" | "EDIT">>;
 }
 
 function ButtonAddTransactions({ setModalKey }: IProps) {
-  const setModalToggle = useModalWindow((state) => state.setModalToggle);
+  const setModalOpen = useModalWindow((state) => state.setModalToggle);
 
   const onAddTransactions = () => {
-    setModalToggle("transaction");
+    setModalOpen(TRANSACTION_KEY);
     setModalKey("ADD")
   };
 
