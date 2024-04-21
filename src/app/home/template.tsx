3cebc4f959a38afe4239da85510ffdf51bfbed19
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-04-21 22:06:08 +0300

    Update template.tsx

--- src/app/home/template.tsx
+++ src/app/home/template.tsx
@@ -3,15 +3,29 @@
 import { useGSAP } from "@gsap/react";
 import { PropsWithChildren, useRef } from "react";
 import gsap from "gsap";
+import { usePageTransition } from "@/src/hooks/useTimeLine";
+import { useParams, usePathname, useSearchParams } from "next/navigation";
 
 export default function RootTemplate({ children }: PropsWithChildren) {
   const container = useRef<HTMLDivElement>(null);
+ 
+  
+
+  const setPageTimeline = usePageTransition(state => state.setPageTimeline);
+  // const tl = usePageTransition((state) => state.pageTimeline);
 
   useGSAP(() => {
-    gsap
-      .timeline()
-      .set("#transition-element", { translateX: "-100%", delay: 0 })
-      .to("#transition-element", { translateX: "0%", duration: 0.7 });
+    // gsap.timeline()
+    
+    console.log("Inner");
+    
+
+
+    const tl = gsap.timeline();
+    setPageTimeline(tl);
+
+    tl?.set("#transition-element", { translateX: "-100%", delay: 0 })
+      .to("#transition-element", { translateX: "0%", duration: 0.5 });
   });
 
   return <>{children}</>;
