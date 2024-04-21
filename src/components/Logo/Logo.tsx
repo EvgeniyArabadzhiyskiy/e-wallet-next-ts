b5128c1df0596e3b2d37cc9185a9ac52ce744f80
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-04-21 22:06:24 +0300

    Update Logo.tsx

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -2,15 +2,30 @@
 
 import Link from "next/link";
 import Image from "next/image";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import { LogoText, LogoWrap, ImgWrapper } from "./Logo.styled";
+import { usePageTransition } from "@/src/hooks/useTimeLine";
 
 function Logo() {
   const pathName = usePathname();
   const isLoginPage = pathName === "/login" || pathName === "/register";
 
+  const router = useRouter();
+  const tl = usePageTransition((state) => state.pageTimeline);
+
+  const handleClick = () => {
+    // gsap.timeline()
+    tl?.set("#transition-element", { translateX: "0%", delay: 0 })
+      .to("#transition-element", { translateX: "100%", duration: 0.5 })
+      // tl?.reversed(true)
+      .then(() => {
+        router.push("/");
+      });
+  };
+
   return (
-    <Link href="/">
+    // <Link href="/">
+    <button onClick={handleClick} style={{backgroundColor: "transparent"}}>
       <LogoWrap>
         <ImgWrapper>
           <Image src="/images/logo.png" alt="logo" width={40} height={40} />
@@ -18,7 +33,8 @@ function Logo() {
 
         <LogoText $isLoginPage={isLoginPage}>Wallet</LogoText>
       </LogoWrap>
-    </Link>
+    </button>
+    // </Link>
   );
 }
 
