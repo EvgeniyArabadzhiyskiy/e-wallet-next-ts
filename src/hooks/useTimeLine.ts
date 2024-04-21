d241684ab40a52e4d4f1bd025836a598d9a9129f
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-04-21 22:06:57 +0300

    Update useTimeLine.ts

--- src/hooks/useTimeLine.ts
+++ src/hooks/useTimeLine.ts
@@ -9,3 +9,13 @@ export const useTimeLine = create<ITimeLineStore>()((set) => ({
   timeline: undefined,
   setTimeLine: (timeline) => set((store) => ({ timeline: timeline })),
 }));
+
+interface IPageTransitionStore {
+  pageTimeline: gsap.core.Timeline | undefined;
+  setPageTimeline: (pageTimeline: gsap.core.Timeline) => void;
+}
+
+export const usePageTransition = create<IPageTransitionStore>()((set) => ({
+  pageTimeline: undefined,
+  setPageTimeline: (pageTimeline) => set((store) => ({ pageTimeline: pageTimeline })),
+}));
