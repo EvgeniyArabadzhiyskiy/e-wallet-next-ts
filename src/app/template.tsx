2398e99492be65b9e2ed5b51e4deb1cf603f8ff5
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-04-21 22:07:12 +0300

    Update template.tsx

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,34 +1,73 @@
 "use client";
 
+import { useGSAP } from "@gsap/react";
 import { AnimatePresence, motion } from "framer-motion";
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useRef } from "react";
+import gsap from "gsap";
+import { usePageTransition } from "../hooks/useTimeLine";
+import { usePathname } from "next/navigation";
 
-const variants = {
-  hidden: { opacity: 0, x: -200, y: 0 },
-  enter: { opacity: 1, x: 0, y: 0 },
-};
+// const variants = {
+//   hidden: { opacity: 0, x: -200, y: 0 },
+//   enter: { opacity: 1, x: 0, y: 0 },
+// };
 
 export default function RootTemplate({ children }: PropsWithChildren) {
+  const container = useRef<HTMLDivElement>(null);
+  const pathName = usePathname();
+
+  const setPageTimeline = usePageTransition(state => state.setPageTimeline);
+
+  useGSAP(() => {
+    // gsap.timeline()
+
+    if (pathName.startsWith("/home")) {
+      return
+    }
+    console.log("APP");
+    
+
+    const tl = gsap.timeline();
+    setPageTimeline(tl);
+
+    tl
+      .set("#transition-element", { translateX: "-100%", delay: 0 })
+      .to("#transition-element", { translateX: "0%", duration: 0.5 });
+  });
+
   return (
-    <>
-      <div
-        // variants={variants}
-        // initial="hidden"
-        // animate="enter"
+    // <div ref={container} className="transition-container">
+    //   <div
+    //   id="transition-element"
+    //   >
+    //     {children}
+    //   </div>
+    // </div>
 
-        // initial={{ translateX: "-100%" }}
-        // animate={{ translateX: "0%" }}
-
-        // initial={{ opacity: 0.5, x: -200, y: 0 }}
-        // animate={{ opacity: 1, x: 0, y: 0 }}
-        // exit={{ opacity: 0, x: -200, y: 0 }}
-        // transition={{ duration: 0.3 }}
-
-        // transition={{  type: "spring", stiffness: 200  }}
-        // transition={{duration: 0.1, type: "spring", stiffness: 200}}
-      >
-        {children}
-      </div>
-    </>
+    <>{children}</>
   );
 }
+
+// <AnimatePresence>
+// <motion.div
+// variants={variants}
+// initial="hidden"
+// animate="enter"
+
+// initial={{ translateX: "-100%" }}
+// animate={{ translateX: "0%" }}
+
+/* ===========================================
+// initial={{ opacity: 0.5, x: -200, y: 0 }}
+// animate={{ opacity: 1, x: 0, y: 0 }}
+// exit={{ opacity: 0, x: -200, y: 0 }}
+// transition={{ duration: 0.3 }}
+ =========================================== */
+
+// transition={{  type: "spring", stiffness: 200  }}
+// transition={{duration: 0.1, type: "spring", stiffness: 200}}
+// >
+
+// </motion.div>
+
+// </AnimatePresence>
