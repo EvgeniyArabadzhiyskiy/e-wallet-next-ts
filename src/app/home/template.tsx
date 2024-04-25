371c8501a38fd60bbbcb084befdcc18f3d63254f
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-04-25 22:05:40 +0300

    Update template.tsx

--- src/app/home/template.tsx
+++ src/app/home/template.tsx
@@ -5,6 +5,7 @@ import { PropsWithChildren, useRef } from "react";
 import gsap from "gsap";
 import { usePageTransition } from "@/src/hooks/useTimeLine";
 import { useParams, usePathname, useSearchParams } from "next/navigation";
+import { useLoading } from "@/src/hooks/useModalWindow";
 
 export default function RootTemplate({ children }: PropsWithChildren) {
   const container = useRef<HTMLDivElement>(null);
@@ -14,19 +15,25 @@ export default function RootTemplate({ children }: PropsWithChildren) {
   const setPageTimeline = usePageTransition(state => state.setPageTimeline);
   // const tl = usePageTransition((state) => state.pageTimeline);
 
-  useGSAP(() => {
-    // gsap.timeline()
+  const { setIsLoading } = useLoading();
+
+  // useGSAP(() => {
+  //   // gsap.timeline()
     
-    console.log("Inner");
+  //   console.log("Inner");
     
 
 
-    const tl = gsap.timeline();
-    setPageTimeline(tl);
+  //   const tl = gsap.timeline();
+  //   setPageTimeline(tl);
 
-    tl?.set("#transition-element", { translateX: "-100%", delay: 0 })
-      .to("#transition-element", { translateX: "0%", duration: 0.5 });
-  });
+  //   tl.set("#transition-element", { translateX: "-100%", delay: 0 })
+  //     .to("#transition-element", { translateX: "0%", duration: 0.5 })
+  //     .then(() => {
+  //       // setIsLoading();
+  //     });
+
+  // });
 
   return <>{children}</>;
 }
