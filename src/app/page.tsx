f9118d2d3eb708e9146bd080dc84367d485a3b44
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-04-25 22:05:50 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import { Suspense } from "react";
 import HeroSection from "../components/HeroSection";
 
 export default function Home() {
-  return <HeroSection />;
+  return (
+    // <Suspense
+    //   fallback={<h1 style={{ color: "white", fontSize: 50 }}>SUSPENSE</h1>}
+    // >
+      <HeroSection />
+    // </Suspense>
+  );
 }
