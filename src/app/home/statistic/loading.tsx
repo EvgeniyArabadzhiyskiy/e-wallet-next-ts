8063f0ae1470ef16aebba83bbcf7268b714499c1
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-04-26 22:17:02 +0300

    Create loading.tsx

---
+++ src/app/home/statistic/loading.tsx
@@ -0,0 +1,10 @@
+
+export default function Loading() {
+  // console.log("LOAADING");
+  
+  return (
+    <div>
+      <h1 style={{color: "yellow", fontSize: 46}}>Loading...</h1>
+    </div>
+  );
+}
