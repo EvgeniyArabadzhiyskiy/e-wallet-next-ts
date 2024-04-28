cb60b1dd9091eee47c19f34ab23e63e734fcb711
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-04-28 12:21:02 +0300

    Update Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,13 +5,14 @@ import {
   UserBox,
 } from "./Header.styled";
 
-
 import Logo from "../Logo";
 import Container from "../Container";
 import ModalBox from "../ModalWindow";
 import ModalLogOut from "../ModalLogOut";
 import SignOutButton from "../Buttons/SignOutButton";
 import { currentUser } from "@/src/apiWallet/user";
+
+// import stl from "./Header.module.scss";
 
 export default async function Header() {
   const user = await currentUser();
@@ -29,6 +30,18 @@ export default async function Header() {
           </HeaderWrapper>
         </Container>
       </StyledHeader>
+
+       {/* <header className={stl.header}>
+        <Container>
+          <div className={stl.header__wrapper}>
+            <Logo />
+            <div className={stl.user__box}>
+              {user && <div className={stl.user__name}>{user.firstName}</div>}
+              <SignOutButton />
+            </div>
+          </div>
+        </Container>
+      </header> */}
 
       <ModalBox modalName="logout">
         <ModalLogOut />
