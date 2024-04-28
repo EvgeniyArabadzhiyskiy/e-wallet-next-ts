752691371a1d35611791a2d9c4e3ca081ab7a239
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-04-28 12:20:48 +0300

    Update SignOutButton.tsx

--- src/components/Buttons/SignOutButton/SignOutButton.tsx
+++ src/components/Buttons/SignOutButton/SignOutButton.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import { ButtonExit, Text } from "./SignOutButton.styled";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import { useModalWindow } from "@/src/hooks/useModalWindow";
-import SettingsSvg from "../../SvgComponent/SettingsSvg";
+import ExitSvg from "../../SvgComponent/ExitSvg";
 
 interface IProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
@@ -12,9 +12,13 @@ function SignOutButton({ ...allProps }: IProps) {
   const setModalOpen = useModalWindow((state) => state.setModalToggle);
 
   return (
-    <ButtonExit type="button" onClick={() => setModalOpen("logout")} {...allProps}>
-      <FontAwesomeIcon icon={faArrowRightFromBracket} />
-      {/* <SettingsSvg width={14} height={14} /> */}
+    <ButtonExit
+      type="button"
+      onClick={() => setModalOpen("logout")}
+      {...allProps}
+    >
+      {/* <FontAwesomeIcon icon={faArrowRightFromBracket} /> */}
+      <ExitSvg width={18} height={18} />
       <Text>Exit</Text>
     </ButtonExit>
   );
