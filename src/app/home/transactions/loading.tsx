1395a819be419ffdb1b25a992610bcea4e6d5c3f
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-04-28 12:20:37 +0300

    Create loading.tsx

---
+++ src/app/home/transactions/loading.tsx
@@ -0,0 +1,11 @@
+import TransactionLoader from "@/src/components/TransactionLoader";
+
+export default function Loading() {
+  return (
+    <div>
+      <TransactionLoader />
+
+      {/* <h1 style={{ color: "yellow", fontSize: 46 }}>Loading...</h1> */}
+    </div>
+  );
+}
