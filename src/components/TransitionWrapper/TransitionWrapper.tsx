102c787bef49a62cea813a9ab21582ccd4a90b95
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2024-05-04 22:09:46 +0300

    Update TransitionWrapper.tsx

--- src/components/TransitionWrapper/TransitionWrapper.tsx
+++ src/components/TransitionWrapper/TransitionWrapper.tsx
@@ -2,22 +2,59 @@
 
 import { useGSAP } from "@gsap/react";
 
-import { PropsWithChildren, useRef } from "react";
+import {
+  PropsWithChildren,
+  ReactNode,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import gsap from "gsap";
+import { usePathname, useRouter } from "next/navigation";
+import { AnimatePresence, motion } from "framer-motion";
+import { LayoutRouterContext } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
-export default function TransitionWrapper({ children }: PropsWithChildren) {
-  const container = useRef<HTMLDivElement>(null);
-
-  useGSAP(() => {
-    gsap
-      .timeline()
-      .set("#transition-element", { translateX: "-100%", delay: 0 })
-      .to("#transition-element", { translateX: "0%", duration: 0.7 });
-  });
+function FrozenRouter(props: PropsWithChildren) {
+  const context = useContext(LayoutRouterContext);
+  const frozen = useRef(context).current;
 
   return (
-    <div ref={container} className="transition-container">
-      <div id="transition-element">{children}</div>
-    </div>
+    <LayoutRouterContext.Provider value={frozen}>
+      {props.children}
+    </LayoutRouterContext.Provider>
+  );
+}
+
+export default function TransitionWrapper(props: PropsWithChildren) {
+  const pathName = usePathname();
+
+  return (
+    <AnimatePresence
+      mode="wait"
+      initial={false}
+      // onExitComplete={() => {
+      //   console.log("Finish")
+      //   router.push(path)
+      // }}
+    >
+      <motion.div
+        key={pathName}
+        initial={{ translateX: "-100%" }}
+        animate={{ translateX: "0%"}}
+        exit={{ translateX: "100%",transition: { duration: 2 },}}
+        transition={{ duration: 2 }}
+
+        // onAnimationComplete={() => {
+        //   console.log("Finish");
+        // }}
+      >
+        <FrozenRouter>
+          {/* <button onClick={() => satPath(ddd)}>STATE</button>
+          <h1 style={{ color: "red" }}>{d}</h1> */}
+          {props.children}
+        </FrozenRouter>
+      </motion.div>
+    </AnimatePresence>
   );
 }
